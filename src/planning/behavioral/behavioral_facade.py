7210f9b8308622e42a1487a793a09155e1c438b9
raz <raz.nissim@gm.com>
2017-07-19 13:44:52 +0300

    fixing according to code review, not including DDS message comments

--- src/planning/behavioral/behavioral_facade.py
+++ src/planning/behavioral/behavioral_facade.py
@@ -1,14 +1,21 @@
 from src.planning.behavioral.behavioral_state import BehavioralState
+from src.planning.behavioral.policy import Policy
 from src.planning.messages.trajectory_parameters import TrajectoryParameters
 from src.state.enriched_state import State
 
 
 class BehavioralFacade:
-    def __init__(self, policy, behavioral_state: BehavioralState):
+    def __init__(self, policy: Policy, behavioral_state: BehavioralState):
         self._policy = policy
         self._behavioral_state = behavioral_state
 
     def update_state_and_plan(self):
+        """
+        The main function of the behavioral planner. It read the most up-to-date enriched state and navigation plan,
+         processes them into the behavioral state, and then performs behavioral planning. The results are then published
+          to the trajectory planner and as debug information to the visualizer.
+        :return: void
+        """
         state = self.__get_current_state()
         navigation_plan = self.__get_current_navigation_plan()
         self._behavioral_state.update_behavioral_state(state, navigation_plan)
@@ -16,18 +23,15 @@ class BehavioralFacade:
         self.__publish_results(trajectory_params)
         self.__publish_debug(trajectory_params)
 
-    # TODO: implement message passing, including type hints.
-    def __get_current_state(self)->State:
+    # TODO : implement message passing
+    def __get_current_state(self) -> State:
         pass
-
+    # TODO add navigation_plan type hint once available
     def __get_current_navigation_plan(self):
         pass
 
-    def __publish_results(self, results: TrajectoryParameters):
+    def __publish_results(self, results: TrajectoryParameters) -> None:
         pass
 
-    def __publish_debug(self, debug_data: TrajectoryParameters):
+    def __publish_debug(self, debug_data: TrajectoryParameters) -> None:
         pass
-
-
-
--- src/planning/behavioral/behavioral_state.py
+++ src/planning/behavioral/behavioral_state.py
@@ -1,15 +1,17 @@
-from src.state.enriched_state import State
+from src.state.enriched_state import State, EgoState
 
 
 class BehavioralState:
-    def __init__(self):
-        self._ego_state = None              # taken from the enriched state
-        self._margin_info = None
-        self._lane_object_information = []  # Array of LaneObjectInfo's
-        self._navigation_plan = None
+    # TODO add type hint for navigation plan, once implemented
+    def __init__(self, ego_state: EgoState = None, margin_info: MarginInfo = None,
+                 lane_object_information: list[LaneObjectInfo] = [], navigation_plan=None):
+        self._ego_state = ego_state  # taken from the enriched state
+        self._margin_info = margin_info
+        self._lane_object_information = lane_object_information  # Array of LaneObjectInfo's
+        self._navigation_plan = navigation_plan
 
     def update_behavioral_state(self, state: State, navigation_plan) -> None:
-        '''
+        """
         updating the behavioral state from the raw input state. This includes only direct processing without complex
         logic. This is implemented separately from initialization in order to potentially use differences for more
         efficient processing.
@@ -17,12 +19,12 @@ class BehavioralState:
         :param state: the enriched state coming as a message from perception via DDS.
         :param navigation_plan: will be used for processing the behavioral state, as well as for PolicyFeatures
         :return: void
-        '''
+        """
         pass
 
 
 class MarginInfo:
-    def __init__(self, right_width, right_clear, left_width, left_clear):
+    def __init__(self, right_width: float, right_clear: bool, left_width: float, left_clear: bool):
         self.right_width = right_width
         self.right_clear = right_clear
         self.left_width = left_width
@@ -30,7 +32,8 @@ class MarginInfo:
 
 
 class LaneObjectInfo:
-    def __init__(self, relative_velocity_of_closest_object, time_distance_of_closest_object, confidence=None):
+    def __init__(self, relative_velocity_of_closest_object: float, time_distance_of_closest_object: float,
+                 confidence: float = None):
         self.relative_velocity_of_closest_object = relative_velocity_of_closest_object
         self.time_distance_of_closest_object = time_distance_of_closest_object
         self.confidence = confidence
