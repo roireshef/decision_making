c7aae63a788dfe4eca4c26f2750fdfa30806e42b
raz <raz.nissim@gm.com>
2017-07-27 13:49:47 +0300

    navigation plan skeleton

--- src/planning/navigation/navigation_plan.py
+++ src/planning/navigation/navigation_plan.py
@@ -2,6 +2,6 @@ class NavigationPlan:
     """
     This class hold the navigation plan.
     """
+    def __init__(self, road_ids: list[int]):
+        self._road_ids = road_ids
 
-    # TODO: implement class
-    pass
