847fc9d6f13f14e4f7f8fb871eadfc9fb819c9e6
raz <raz.nissim@gm.com>
2017-07-27 13:55:31 +0300

    added comments

--- src/planning/navigation/navigation_plan.py
+++ src/planning/navigation/navigation_plan.py
@@ -3,5 +3,9 @@ class NavigationPlan:
     This class hold the navigation plan.
     """
     def __init__(self, road_ids: list[int]):
+        """
+        Initialization of the navigation plan. This is an initial implementation which contains only a list o road ids.
+        :param road_ids: list of road ids corresponding to the map.
+        """
         self._road_ids = road_ids
 
