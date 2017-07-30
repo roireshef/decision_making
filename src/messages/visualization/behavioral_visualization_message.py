cf030ce8a0a702e6051df0a5caaa6e9af0391650
raz <raz.nissim@gm.com>
2017-07-30 13:42:46 +0300

    conforming to ddsMessage underscore guidelines.

--- src/messages/visualization/behavioral_visualization_message.py
+++ src/messages/visualization/behavioral_visualization_message.py
@@ -4,13 +4,13 @@ from src.messages.dds_message import DDSMessage
 
 
 class BehavioralVisualizationMessage(DDSMessage):
-    def __init__(self, _reference_route: np.ndarray):
+    def __init__(self, ref_route: np.ndarray):
         """
         The struct used for communicating the behavioral plan to the visualizer.
         :param _reference_route: of type np.ndarray, with rows of [(x ,y, theta)] where x, y, theta are floats
         """
-        self._reference_route = _reference_route
+        self.ref_route = ref_route
 
     @property
-    def reference_route(self): return self._reference_route
+    def reference_route(self): return self.ref_route
 
