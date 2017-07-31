7b5ad573ab104b8689cfbfeb0860ffb334c96336
Ron Eyal <ron.eyal@gm.com>
2017-07-31 19:21:43 +0300

    start state and manager implementation

--- src/state/state_module.py
+++ src/state/state_module.py
@@ -1,5 +1,7 @@
-from src.infra.dm_module import DM_Module
+from decision_making.src.infra.dm_module import DM_Module
+from rte.python.periodic_timer.periodic_timer import PeriodicTimer
 
+import time
 class state_module(DM_Module):
 
     def __init__(self, DDS, logger):
@@ -8,12 +10,20 @@ class state_module(DM_Module):
     def start(self):
         self.logger.info("Starting state module")
         self.DDS.subscribe("StateSubscriber::DynamicObjectsReader", self.__dynamic_obj_callback)
+        self.timer = PeriodicTimer(2, self.__timer_callback)
+        self.timer.start()
 
     def stop(self):
         self.logger.info("Stopping state module")
         self.DDS.unsubscribe("StateSubscriber::DynamicObjectsReader")
+        self.timer.stop()
+
+    def __timer_callback(self):
+        self.logger.info("periodic timer")
 
     def __dynamic_obj_callback(self, objects: dict):
         self.logger.info("got dynamic objects %s", objects)
+
+
 
 
