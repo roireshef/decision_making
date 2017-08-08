f45d2c10080e0f3520144b8c0fac5bfd7a31f9b4
RoiGM <roi.reshef@gm.com>
2017-08-08 12:42:10 +0300

    moved logging to base dm_module class

--- test/messages/test_ddsMessage.py
+++ test/messages/test_ddsMessage.py
@@ -1,5 +1,7 @@
 import sys
 import numpy as np
+import py, pytest
+
 
 if sys.version_info > (3, 0):
     from decision_making.test.messages.typed_messages_fixture import *
@@ -18,3 +20,15 @@ def test_serialize_dummyMsg_successful():
     assert isinstance(w_new, Woo)
     assert isinstance(w_new.l, list)
     assert isinstance(w_new.l[0].y, np.ndarray)
+
+def test_serialize_dummyWrongFieldsMsg_throwsError():
+    f = Foo(2, 3)
+    v = Voo(f, np.array([[.1, .2, 3], [11, 22, 33]]))
+    w = Woo(list((f, v)))
+
+    try:
+        w_ser = w.serialize()
+        w_new = Woo.deserialize(w_ser)
+        pytest.fail("An exception was meant to be thrown but nothing was actually thrown")
+    except Exception:
+        assert True
