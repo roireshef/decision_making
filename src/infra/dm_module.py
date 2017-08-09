8ba262116e462336f23921434beac6d21fcda01b
kz430x <re22SHEF>
2017-08-09 13:36:54 +0300

    generalized logging at dm_module and inheritants

--- src/infra/dm_module.py
+++ src/infra/dm_module.py
@@ -2,22 +2,35 @@ from common_data.dds.python.Communication.ddspubsub import DdsPubSub
 from rte.python.logger.AV_logger import AV_Logger
 from abc import abstractmethod
 
-class DmModule():
 
-    def __init__(self, dds : DdsPubSub, logger):
+class DmModule:
+    def __init__(self, dds: DdsPubSub, logger: AV_Logger):
+        self.logger.info("initializing module: " + self.__class__.__name__)
         self.dds = dds
         self.logger = logger
 
     @abstractmethod
+    def _start_impl(self):
+        pass
+
     def start(self):
-        pass
+        self.logger.info("starting module: " + self.__class__.__name__)
+        self._start_impl()
 
     @abstractmethod
+    def _stop_impl(self):
+        pass
+
     def stop(self):
-        pass
+        self.logger.info("stopping module: " + self.__class__.__name__)
+        self._stop_impl()
 
     @abstractmethod
-    def periodic_action(self):
+    def _periodic_action_impl(self):
         pass
+
+    def periodic_action(self):
+        self.logger.info("executing periodic action at module: " + self.__class__.__name__)
+        self._stop_impl()
 
 
