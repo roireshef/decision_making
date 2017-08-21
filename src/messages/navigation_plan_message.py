f38ee769bf3a428f983f1d2dc2c397b28277fda7
Roi.GM <roi.reshef@gm.com>
2017-08-21 18:36:25 +0300

    added skeleton for navigation message, added naive navigtaion planner

--- src/messages/navigation_plan_message.py
+++ src/messages/navigation_plan_message.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import numpy as np
 
 from decision_making.src.messages.dds_typed_message import DDSTypedMsg
@@ -13,3 +15,27 @@ class NavigationPlanMsg(DDSTypedMsg):
         :param road_ids: list of road ids corresponding to the map.
         """
         self.road_ids = road_ids
+
+    def get_current_road_index(self, road_id: int) -> Union[None, int]:
+        """
+        Given a road_id, returns the index of this road_id in the navigation plan
+        :param road_id: a road_id as exists in MapAPI
+        :return: the index of the road_id in the navigation plan, None if road_id does not exist in the plan
+        """
+        pass
+
+    def get_next_road_id(self, road_id: int) -> Union[None, int]:
+        """
+        Given a road_id, returns the next road_id in the navigation plan
+        :param road_id: a road_id as exists in MapAPI
+        :return: the next road_id in the navigation plan, None if the road_id has no next or does not exist in the plan
+        """
+        pass
+
+    def get_previous_road_id(self, road_id: int) -> int:
+        """
+        Given a road_id, returns the previous road_id in the navigation plan
+        :param road_id: a road_id as exists in MapAPI
+        :return: the next road_id in the navigation plan, None if the road_id has no prev or does not exist in the plan
+        """
+        pass
--- src/planning/navigation/navigation_planner.py
+++ src/planning/navigation/navigation_planner.py
@@ -4,7 +4,10 @@ from decision_making.src.messages.navigation_plan_message import NavigationPlanM
 
 
 # TODO - must think about what the input to the navigation computation is, and where it comes from
+from decision_making.src.state.state import RoadLocalization
+
+
 class NavigationPlanner(metaclass=ABCMeta):
     @abstractmethod
-    def plan(self) -> NavigationPlanMsg:
+    def plan(self, init_localization: RoadLocalization, target_localization: RoadLocalization) -> NavigationPlanMsg:
         pass
