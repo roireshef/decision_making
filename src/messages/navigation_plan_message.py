2ce25aea3efa28c6ed93de3faf54ccb86538c714
Max Bluvstein <max.bluvstein@gm.com>
2017-08-22 11:05:05 +0300

    implemented navigation plan message methods

--- src/messages/navigation_plan_message.py
+++ src/messages/navigation_plan_message.py
@@ -8,7 +8,10 @@ from decision_making.src.messages.dds_typed_message import DDSTypedMsg
 class NavigationPlanMsg(DDSTypedMsg):
     """
         This class hold the navigation plan.
+        It also implements function (required by MapAPI) that iterate over the roads list in the navigation plan.
+        Important assumption: we assume that road_ids is a UNIQUE list, containing each value only once.
     """
+
     def __init__(self, road_ids: np.array):
         """
         Initialization of the navigation plan. This is an initial implementation which contains only a list o road ids.
@@ -16,26 +19,38 @@ class NavigationPlanMsg(DDSTypedMsg):
         """
         self.road_ids = road_ids
 
-    def get_current_road_index(self, road_id: int) -> Union[None, int]:
+    def get_road_index_in_plan(self, road_id: int) -> Union[None, int]:
         """
         Given a road_id, returns the index of this road_id in the navigation plan
-        :param road_id: a road_id as exists in MapAPI
-        :return: the index of the road_id in the navigation plan, None if road_id does not exist in the plan
+        :param road_id:
+        :return: index of road_id in the plan
         """
-        pass
+        search_result = np.where(self._road_ids == road_id)
+        if len(search_result[0]) == 0:  # target road_id not found in the navigation plan
+            return None
+        else:
+            return search_result[0][0]
 
-    def get_next_road_id(self, road_id: int) -> Union[None, int]:
+    def get_next_road(self, plan_index: int) -> Union[None, int]:
         """
         Given a road_id, returns the next road_id in the navigation plan
-        :param road_id: a road_id as exists in MapAPI
-        :return: the next road_id in the navigation plan, None if the road_id has no next or does not exist in the plan
+        :param plan_index: the index of the road in plan
+        :return: the next road_id in the navigation plan, None if the road has no next or does not exist in the plan
         """
-        pass
+        if plan_index < len(self.road_ids):
+            next_road_id = self.road_ids[plan_index + 1]
+            return next_road_id
+        else:
+            return None
 
-    def get_previous_road_id(self, road_id: int) -> int:
+    def get_previous_road(self, plan_index: int) -> int:
         """
         Given a road_id, returns the previous road_id in the navigation plan
-        :param road_id: a road_id as exists in MapAPI
+        :param plan_index: the index of the road in plan
         :return: the next road_id in the navigation plan, None if the road_id has no prev or does not exist in the plan
         """
-        pass
+        if plan_index > 0
+            prev_road_id = self.road_ids[plan_index - 1]
+            return prev_road_id
+        else:
+            return None
