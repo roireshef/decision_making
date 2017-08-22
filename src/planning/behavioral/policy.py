b3474dd62b1808cdcca7a53cdde5a514bfb3e413
Max Bluvstein <max.bluvstein@gm.com>
2017-08-22 09:29:19 +0300

    code cleanup

--- src/planning/behavioral/policy.py
+++ src/planning/behavioral/policy.py
@@ -8,16 +8,30 @@ from decision_making.src.planning.behavioral.behavioral_state import BehavioralS
 
 
 class PolicyConfig(DDSTypedMsg):
+    """
+    Parameters configuration class, loaded from parameter server
+    """
     def __init__(self):
         pass
 
 
 class Policy(metaclass=ABCMeta):
     def __init__(self, logger: Logger, policy_config: PolicyConfig):
+        """
+        Receives configuration and logger
+        :param logger: logger
+        :param policy_config: parameters configuration class, loaded from parameter server
+        """
         self._policy_config = policy_config
         self.logger = logger
 
     @abstractmethod
     def plan(self, behavioral_state: BehavioralState) -> (TrajectoryParameters, BehavioralVisualizationMsg):
+        """
+        Plan according to the behavioral state and return trajectory parametrs that
+        allow the Trajectory planner to evaluate & choose the best available trajectory
+        :param behavioral_state:
+        :return: TrajectoryParameters for behavioral planner, BehavioralVisualizationMsg for visualization purposes
+        """
         pass
 
