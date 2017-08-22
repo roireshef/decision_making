bc14e38a824a05be372be0c571f37b8e9f55ebcb
Vlad Goldner <vlad.goldner@gm.com>
2017-08-22 16:04:46 +0300

    correct comments in constant.py

--- src/map/constants.py
+++ src/map/constants.py
@@ -1,24 +1,40 @@
 
+# a small number
 EPSILON = 0.001
+# a large number
 LARGE_NUM = 1000000
 
-EARTH_RADIUS = 6371.  # in kilometers
+# [km] Earth radius
+EARTH_RADIUS = 6371.
 
+# [m] one-dimensional size of tile (cell) in the map xy2road_map
 ROADS_MAP_TILE_SIZE = 10.  # in meters; used by __render_road
-RENDER_ROAD_MARGIN = 3.  # in meters; roads are rendered with margins to roads_map
 
-NARROW_ROAD_WITHOUT_MARGINS = 2.6  # in meters; only wider roads have margins
-LAYER_HEIGHT = 7.  # in meters; altitude difference between two adjacent layers
+# [m] roads are rendered with margins to roads_map
+RENDER_ROAD_MARGIN = 3.
 
-LANE_STRIP_WIDTH = 0.15  # in meters; width of a strip between lanes
-LANE_STRIP_LENGTH = 3.  # in meters; length of a strip between lanes
-LANE_STRIP_CYCLE = 8.  # in meters; lane strip every 8 meters
+# [m] only wider roads have margins
+NARROW_ROAD_WITHOUT_MARGINS = 2.6
+# [m] altitude difference between two adjacent layers
+LAYER_HEIGHT = 7.
 
-CUSTOM_NUM_LANES = 2  # lanes number of custom roads
-CUSTOM_LANE_WIDTH = 3.  # in meters; lanes' width in custom roads
-CUSTOM_ROAD_ROUND_SQUARE_SIZE = 100.  # in meters; size of the edge of the square
-CUSTOM_ROAD_ROUND_SQUARE_STRAIGHT_SIZE = 60.  # in meters; size of the straight part of the edge
+# [m] width of a strip between lanes
+LANE_STRIP_WIDTH = 0.15
+# [m] length of a strip between lanes
+LANE_STRIP_LENGTH = 3.
+# [m] lane strip every 8 meters
+LANE_STRIP_CYCLE = 8.
 
+# lanes number of custom roads
+CUSTOM_NUM_LANES = 2
+# [m] lanes' width in custom roads
+CUSTOM_LANE_WIDTH = 3.
+# [m] size of the edge of the square
+CUSTOM_ROAD_ROUND_SQUARE_SIZE = 100.
+# [m] size of the straight part of the edge
+CUSTOM_ROAD_ROUND_SQUARE_STRAIGHT_SIZE = 60.
+
+# [m] Road_width for different road types in OSM file. These numbers were chosen arbitrarily
 OSM_FOOTWAY_ROAD_WIDTH = 1.0
 OSM_PEDESTRIAN_ROAD_WIDTH = 3.0
 OSM_MOTORWAY_ROAD_WIDTH = 3.6
