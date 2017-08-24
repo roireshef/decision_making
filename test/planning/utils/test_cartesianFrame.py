ea257d61fe9f36cfa09bc1c80f17e9a29fadf5d9
Roi.GM <roi.reshef@gm.com>
2017-08-24 17:07:42 +0300

    updated tests

--- test/planning/utils/test_cartesianFrame.py
+++ test/planning/utils/test_cartesianFrame.py
@@ -2,6 +2,7 @@ from decision_making.src.global_constants import DIVISION_FLOATING_ACCURACY
 from decision_making.src.planning.utils.geometry_utils import CartesianFrame
 import numpy as np
 
+ACCURACY_DECIMAL = np.log10(DIVISION_FLOATING_ACCURACY)
 
 def test_convertGlobalToRelativeFrame_onlyTranslation_successful():
     global_pos = np.array([2, 1, 0])
@@ -9,8 +10,8 @@ def test_convertGlobalToRelativeFrame_onlyTranslation_successful():
     ego_yaw = 0
 
     relative_pos = CartesianFrame.convert_global_to_relative_frame(global_pos, ego_pos, ego_yaw)
-
-    assert np.linalg.norm(relative_pos - np.array([1, 0, 0])) < DIVISION_FLOATING_ACCURACY
+    expected_relative_pos = np.array([1, 0, 0])
+    assert np.testing.assert_array_almost_equal(relative_pos, expected_relative_pos, decimal=ACCURACY_DECIMAL)
 
 
 def test_convertGlobalToRelativeFrame_withFortyFiveDegRotation_successful():
@@ -19,8 +20,8 @@ def test_convertGlobalToRelativeFrame_withFortyFiveDegRotation_successful():
     ego_yaw = np.pi / 4
 
     relative_pos = CartesianFrame.convert_global_to_relative_frame(global_pos, ego_pos, ego_yaw)
-
-    assert np.linalg.norm(relative_pos - np.array([1/np.sqrt(2), -1/np.sqrt(2), 0])) < DIVISION_FLOATING_ACCURACY
+    expected_relative_pos = np.array([1/np.sqrt(2), -1/np.sqrt(2), 0])
+    assert np.testing.assert_array_almost_equal(relative_pos, expected_relative_pos, decimal=ACCURACY_DECIMAL)
 
 
 def test_convertRelativeToGlobalFrame_onlyTranslation_successful():
@@ -29,8 +30,8 @@ def test_convertRelativeToGlobalFrame_onlyTranslation_successful():
     ego_yaw = 0
 
     relative_pos = CartesianFrame.convert_relative_to_global_frame(global_pos, ego_pos, ego_yaw)
-
-    assert np.linalg.norm(relative_pos - np.array([3, 2, 0])) < DIVISION_FLOATING_ACCURACY
+    expected_relative_pos = np.array([3, 2, 0])
+    assert np.testing.assert_array_almost_equal(relative_pos, expected_relative_pos, decimal=ACCURACY_DECIMAL)
 
 
 def test_convertRelativeToGlobalFrame_withFortyFiveDegRotation_successful():
@@ -39,5 +40,5 @@ def test_convertRelativeToGlobalFrame_withFortyFiveDegRotation_successful():
     ego_yaw = np.pi / 4
 
     relative_pos = CartesianFrame.convert_relative_to_global_frame(global_pos, ego_pos, ego_yaw)
-
-    assert np.linalg.norm(relative_pos - np.array([1 - 1/np.sqrt(2), 1 + 1/np.sqrt(2), 0])) < DIVISION_FLOATING_ACCURACY
+    expected_relative_pos = np.array([1 - 1/np.sqrt(2), 1 + 1/np.sqrt(2), 0])
+    assert np.testing.assert_array_almost_equal(relative_pos, expected_relative_pos, decimal=ACCURACY_DECIMAL)
