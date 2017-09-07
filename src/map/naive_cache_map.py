3508d599e001cf84d722ea06ab861dd1103ae8d7
Max Bluvstein <max.bluvstein@gm.com>
2017-09-07 11:19:39 +0300

    reverted naive map

--- src/map/naive_cache_map.py
+++ src/map/naive_cache_map.py
@@ -6,17 +6,11 @@ import pickle
 
 
 class NaiveCacheMap(MapAPI):
-    def __init__(self, map_model_filename, logger):
+    def __init__(self, map_model, logger):
         # type: (str, Logger) -> None
         """
         :param map_model: MapModel containing all relevant map data. To load .bin files (pickle), use:
         import pickle
         pickle.load(open(map_model_filename, "rb"))
         """
-
-        # TODO: A PATCH TOWARDS DEMO, FIX IT
-        map_model_from_file = pickle.load(open(map_model_filename, "rb"))
-        map_model = MapModel(map_model_from_file.roads_data, map_model_from_file.incoming_roads,
-                             map_model_from_file.outgoing_roads, map_model_from_file.xy2road_map,
-                             ROADS_MAP_TILE_SIZE)
         super().__init__(map_model, logger)
