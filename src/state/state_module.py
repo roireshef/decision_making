726bc47459c0a5c475fe0936387e2a04e8807fb8
Roi Reshef <Roi.Reshef@gm.com>
2017-09-07 21:54:52 +0300

    .

--- src/state/state_module.py
+++ src/state/state_module.py
@@ -149,7 +149,7 @@ class StateModule(DmModule):
         """
         closest_road_id, lon, lat, yaw, is_on_road = map_api.convert_global_to_road_coordinates(pos[0], pos[1], yaw)
         road_details = map_api.get_road(closest_road_id)
-        lane_width = road_details.width / road_details.lanes_num
+        lane_width = road_details.lane_width
         lane = np.math.floor(lat / lane_width)
         intra_lane_lat = lat - lane * lane_width
 
