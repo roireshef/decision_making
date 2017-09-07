392874d97ee7d8a1494204c3e0616fb983c85d59
Max Bluvstein <max.bluvstein@gm.com>
2017-09-07 16:51:25 +0300

    fixed some constants

--- src/global_constants.py
+++ src/global_constants.py
@@ -5,13 +5,13 @@ DIVISION_FLOATING_ACCURACY = 10 ** -10
 # Behavioral Planner
 
 # [m] high-level behavioral planner lookahead distance
-BEHAVIORAL_PLANNING_LOOKAHEAD_DIST = 50.0
+BEHAVIORAL_PLANNING_LOOKAHEAD_DIST = 60.0
 BEHAVIORAL_PLANNING_CONSTANT_DRIVE_VELOCITY = 10.0
 
 # Trajectory Planner #
 
 # [m] length of reference trajectory provided by behavioral plenner
-REFERENCE_TRAJECTORY_LENGTH = 25.0
+REFERENCE_TRAJECTORY_LENGTH = 30.0
 
 # [m] Resolution for the interpolation of the reference route
 TRAJECTORY_ARCLEN_RESOLUTION = 0.1
@@ -20,13 +20,13 @@ TRAJECTORY_ARCLEN_RESOLUTION = 0.1
 TRAJECTORY_CURVE_INTERP_TYPE = 'cubic'
 
 # [m] Do not consider obastacles that are distant than this thershold
-MAXIMAL_OBSTACLE_PROXIMITY = 25
+MAXIMAL_OBSTACLE_PROXIMITY = 25.0
 
 # [m] Cost function clips higher distances before exponentiation
-EXP_CLIP_TH = 50
+EXP_CLIP_TH = 50.0
 
 # Number of (best) trajectories to publish to visualization
-NUM_ALTERNATIVE_TRAJECTORIES = 100
+NUM_ALTERNATIVE_TRAJECTORIES = 10
 
 # Werling Planner #
 
--- src/planning/behavioral/constants.py
+++ src/planning/behavioral/constants.py
@@ -2,7 +2,8 @@
 LATERAL_SAFETY_MARGIN_FROM_OBJECT = 0.2
 
 # Define the grid of additive lateral offsets in [lanes] to current car's latitude on road
-POLICY_ACTION_SPACE_ADDITIVE_LATERAL_OFFSETS_IN_LANES = [-1, -0.5, -0.25, 0, 0.25, 0.5, 1]
+#POLICY_ACTION_SPACE_ADDITIVE_LATERAL_OFFSETS_IN_LANES = [-1, -0.5, -0.25, 0, 0.25, 0.5, 1]
+POLICY_ACTION_SPACE_ADDITIVE_LATERAL_OFFSETS_IN_LANES = [-2.0, -1.0, 0.0, 1.0, 2.0]
 
 # A lower and upper thresholds on the longitudinal offset between object and ego.
 # Any object out of this scope won't be accounted in the behavioral planning process
