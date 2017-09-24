d0d07005c10d4bdc0bead0258ba1587b52933963
raz <raz.nissim@gm.com>
2017-09-24 11:15:23 +0300

    minor change in type hints.

--- src/map/naive_cache_map.py
+++ src/map/naive_cache_map.py
@@ -1,13 +1,11 @@
-from decision_making.src.map.constants import ROADS_MAP_TILE_SIZE
 from decision_making.src.map.map_api import MapAPI
 from decision_making.src.map.map_model import MapModel
 from logging import Logger
-import pickle
 
 
 class NaiveCacheMap(MapAPI):
     def __init__(self, map_model, logger):
-        # type: (str, Logger) -> None
+        # type: (MapModel, Logger) -> None
         """
         :param map_model: MapModel containing all relevant map data.
         """
