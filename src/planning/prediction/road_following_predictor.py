975e8394bec254aa89e14ae4fba693cd942807ac
Max Bluvstein <max.bluvstein@gm.com>
2017-10-09 11:12:01 +0300

    completed implementation + test skeleton

--- src/planning/prediction/road_following_predictor.py
+++ src/planning/prediction/road_following_predictor.py
@@ -10,11 +10,24 @@ from mapping.src.transformations.geometry_utils import CartesianFrame
 
 class RoadFollowingPredictor(Predictor):
     """
-    See base class
+    See base class.
+
+    This predictor assumes that each dynamic object is moving in constant
+    velocity along the longitudinal axis of of the road. Meaning, there is no acceleration,
+    and the speed norm is directed towards advancing in the road while preserving the lateral offset.
     """
 
+
 def predict(dynamic_object: DynamicObject, predicted_timestamps: np.ndarray, map_api: MapAPI,
-            nav_plan:NavigationPlanMsg) -> np.ndarray:
+            nav_plan: NavigationPlanMsg) -> np.ndarray:
+    """
+    :param dynamic_object: in map coordinates
+    :param predicted_timestamps: np array of timestamps to predict for. In ascending order.
+    :param map_api: used in order to get the predicted trajectory and center lanes in map  coordinates
+    :param nav_plan: predicted navigation plan of the object
+    :return: predicted object's locations in global map coordinates np.array([x, y, theta, vel])
+    """
+
     # we assume the object is travelling exactly on a constant latitude. (i.e., lateral speed = 0)
     object_velocity = np.linalg.norm([dynamic_object.v_x, dynamic_object.v_y])
 
@@ -30,4 +43,11 @@ def predict(dynamic_object: DynamicObject, predicted_timestamps: np.ndarray, map
 
     # resample the route to predicted_timestamps
     predicted_distances_from_start = object_velocity * (predicted_timestamps - dynamic_object.timestamp)
-    CartesianFrame.resample_curve(curve=lookahead_route, arbitrary_curve_sampling_points=predicted_distances_from_start)
+    route_xy = CartesianFrame.resample_curve(curve=lookahead_route,
+                                             arbitrary_curve_sampling_points=predicted_distances_from_start)
+
+    # add yaw and velocity
+    velocity_column = np.ones(shape=[route_xy.shape[0], 1]) * object_velocity
+    route_x_y_theta_v = np.c_[CartesianFrame.add_yaw(route_xy), velocity_column]
+
+    return route_x_y_theta_v
---
+++ test/planning/prediction/test_RoadFollowingPredictor.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def test_predict_movingCarAlongRoad_precisePrediction():
+    pass
