715d6fe2a10f423a4637b30afadbc5c23ba38a22
raz <raz.nissim@gm.com>
2017-10-10 16:31:23 +0300

    added test of EgoState prediction API

--- src/planning/prediction/predictor.py
+++ src/planning/prediction/predictor.py
@@ -1,11 +1,11 @@
-from typing import List
+from typing import List, Type
 
 import copy
 import numpy as np
 
 from decision_making.src.messages.navigation_plan_message import NavigationPlanMsg
-from decision_making.src.planning.prediction.columns import *
-from decision_making.src.state.state import DynamicObject, EgoState, State, ObjectSize
+from decision_making.src.planning.prediction.columns import PREDICT_X, PREDICT_Y, PREDICT_YAW, PREDICT_VEL
+from decision_making.src.state.state import DynamicObject, EgoState, State, ObjectSize, RoadLocalization
 from mapping.src.model.map_api import MapAPI
 
 
@@ -15,7 +15,8 @@ class Predictor:
     """
 
     @classmethod
-    def predict_object_trajectory(cls, dynamic_object: DynamicObject, predicted_timestamps: np.ndarray, map_api: MapAPI,
+    def predict_object_trajectory(cls, dynamic_object: Type[DynamicObject], predicted_timestamps: np.ndarray,
+                                  map_api: MapAPI,
                                   nav_plan: NavigationPlanMsg) -> np.ndarray:
         """
         Method to compute future locations, yaw, and velocities for dynamic objects. Returns the np.array used by the
@@ -29,8 +30,9 @@ class Predictor:
         pass
 
     @classmethod
-    def predict_object_state(cls, dynamic_object: DynamicObject, predicted_timestamps: np.ndarray, map_api: MapAPI,
-                             nav_plan: NavigationPlanMsg) -> List[DynamicObject]:
+    def predict_object_state(cls, dynamic_object: Type[DynamicObject], predicted_timestamps: np.ndarray,
+                             map_api: MapAPI,
+                             nav_plan: NavigationPlanMsg) -> List[Type[DynamicObject]]:
         """
         Wrapper method that uses the predict_object_trajectory, and creates the dynamic object list.
         :param dynamic_object: in map coordinates
@@ -52,6 +54,7 @@ class Predictor:
             predicted_object_state.x = predicted_object_trajectory[t_ind, PREDICT_X]
             predicted_object_state.y = predicted_object_trajectory[t_ind, PREDICT_Y]
             predicted_object_state.yaw = predicted_object_trajectory[t_ind, PREDICT_YAW]
+            # TODO: check consistency between diff of (x,y) and v_x, v_y as calculated below:
             predicted_object_state.v_x = predicted_object_trajectory[t_ind, PREDICT_VEL] * np.cos(
                 predicted_object_trajectory[t_ind, PREDICT_YAW])
             predicted_object_state.v_y = predicted_object_trajectory[t_ind, PREDICT_VEL] * np.sin(
@@ -61,8 +64,9 @@ class Predictor:
 
     @classmethod
     def predict_ego_state(cls, ego_state: EgoState, predicted_timestamps: np.ndarray, map_api: MapAPI,
-                          nav_plan: NavigationPlanMsg) -> List[EgoState]:
-        pass
+                          nav_plan: NavigationPlanMsg) -> List[Type[DynamicObject]]:
+        # TODO: update EgoState attributes that are copied and are not part of DynamicObject (as steering_angle)
+        return cls.predict_object_state(ego_state, predicted_timestamps, map_api, nav_plan)
 
     @classmethod
     def predict_state(cls, state: State, predicted_timestamps: np.ndarray, map_api: MapAPI,
@@ -77,4 +81,4 @@ class Predictor:
         :param nav_plan: predicted navigation plan of the object
         :return: a list of predicted states.
         """
-        pass
+
