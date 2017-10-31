655f4806a9e2b5f0d08a2c4c8ba34be085991d2f
Max Bluvstein <max.bluvstein@gm.com>
2017-10-31 18:05:38 +0200

    naive implementation of _specify_action()

--- src/planning/behavioral/policies/november_demo_semantic_policy.py
+++ src/planning/behavioral/policies/november_demo_semantic_policy.py
@@ -1,8 +1,12 @@
+import numpy as np
 from logging import Logger
 from typing import List
 
+from decision_making.src.global_constants import BEHAVIORAL_PLANNING_DEFAULT_SPEED_LIMIT
+from decision_making.src.planning.behavioral.constants import BEHAVIORAL_PLANNING_TRAJECTORY_HORIZON
 from decision_making.src.planning.behavioral.semantic_actions_policy import SemanticActionsPolicy, \
-    SemanticBehavioralState, RoadSemanticOccupancyGrid
+    SemanticBehavioralState, RoadSemanticOccupancyGrid, SemanticAction, SemanticActionType, SemanticActionSpec, \
+    SEMANTIC_CELL_LANE
 from decision_making.src.state.state import DynamicObject, EgoState, State
 from mapping.src.model.map_api import MapAPI
 
@@ -14,6 +18,9 @@ SEMANTIC_OCCUPANCY_GRID_PARTITIONS_MARGIN_FROM_EGO = 1
 
 
 class NovDemoBehavioralState(SemanticBehavioralState):
+    def __init__(self, road_occupancy_grid: RoadSemanticOccupancyGrid, ego_state: EgoState):
+        super().__init__(road_occupancy_grid=road_occupancy_grid)
+        self.ego_state = ego_state
 
     @classmethod
     def create_from_state(cls, state: State, map_api: MapAPI, logger: Logger):
@@ -25,7 +32,7 @@ class NovDemoBehavioralState(SemanticBehavioralState):
                                                                         map_api=map_api,
                                                                         logger=logger)
 
-        return NovDemoBehavioralState(road_occupancy_grid=semantic_occupancy_grid)
+        return NovDemoBehavioralState(road_occupancy_grid=semantic_occupancy_grid, ego_state=state.ego_state)
 
     @staticmethod
     def _generate_semantic_occupancy_grid(ego_state: EgoState, dynamic_objects: List[
@@ -114,4 +121,94 @@ class NovDemoBehavioralState(SemanticBehavioralState):
 
 
 class NovDemoPolicy(SemanticActionsPolicy):
-    pass
+    def _enumerate_actions(self, behavioral_state: SemanticBehavioralState) -> List[SemanticAction]:
+        """
+        Enumerate the list of possible semantic actions to be generated.
+        :param behavioral_state:
+        :return:
+        """
+
+        semantic_actions: List[SemanticAction] = list()
+
+        # Generate actions towards each of the cells in front of ego
+        for relative_lane_key in [-1, 0, 1]:
+            for longitudinal_key in [SEMANTIC_GRID_FRONT]:
+                semantic_cell = (relative_lane_key, longitudinal_key)
+                if semantic_cell in behavioral_state.road_occupancy_grid:
+                    # Select first (closest) object in cell
+                    target_obj = behavioral_state.road_occupancy_grid[semantic_cell][0]
+                else:
+                    # There are no objects in cell
+                    target_obj = None
+
+                semantic_action = SemanticAction(cell=semantic_cell, target_obj=target_obj,
+                                                 action_type=SemanticActionType.FOLLOW)
+
+                semantic_actions.append(semantic_action)
+
+        return semantic_actions
+
+    def _specify_action(self, behavioral_state: NovDemoBehavioralState,
+                        semantic_action: SemanticAction) -> SemanticActionSpec:
+        """
+        For each semantic actions, generate a trajectory specifications that will be passed through to the TP
+        :param behavioral_state:
+        :param semantic_action:
+        :return: semantic action spec
+        """
+
+        if semantic_action.target_obj is None:
+            return self._specify_action_to_empty_cell(behavioral_state=behavioral_state,
+                                                      semantic_action=semantic_action)
+        else:
+            return self._specify_action_towards_object(behavioral_state=behavioral_state,
+                                                       semantic_action=semantic_action)
+
+    def _specify_action_to_empty_cell(self, behavioral_state: NovDemoBehavioralState,
+                                      semantic_action: SemanticAction) -> SemanticActionSpec:
+        """
+        Generate trajectory specification towards a target location in given cell considering ego speed, location.
+        :param behavioral_state:
+        :param semantic_action:
+        :return:
+        """
+
+        road_lane_latitudes = self._map_api.get_center_lanes_latitudes(
+            road_id=behavioral_state.ego_state.road_localization.road_id)
+        target_lane = behavioral_state.ego_state.road_localization.lane_num + semantic_action.cell[SEMANTIC_CELL_LANE]
+        target_lane_latitude = road_lane_latitudes[target_lane]
+
+        target_relative_s = target_lane_latitude - behavioral_state.ego_state.road_localization.full_lat
+        target_relative_d = BEHAVIORAL_PLANNING_DEFAULT_SPEED_LIMIT * BEHAVIORAL_PLANNING_TRAJECTORY_HORIZON
+
+        return SemanticActionSpec(t=BEHAVIORAL_PLANNING_TRAJECTORY_HORIZON, v=BEHAVIORAL_PLANNING_DEFAULT_SPEED_LIMIT,
+                                  s_rel=target_relative_s, d_rel=target_relative_d)
+
+    def _specify_action_towards_object(self, behavioral_state: NovDemoBehavioralState,
+                                       semantic_action: SemanticAction) -> SemanticActionSpec:
+
+        # Get object's location
+        default_navigation_plan = self.map_api.get_road_based_navigation_plan(
+            current_road_id=behavioral_state.ego_state.road_localization.road_id)
+
+        object_relative_road_localization = semantic_action.target_obj.get_relative_road_localization(
+            ego_road_localization=behavioral_state.ego_state.road_localization, ego_nav_plan=default_navigation_plan,
+            map_api=self._map_api, logger=self.logger)
+
+        road_lane_latitudes = self._map_api.get_center_lanes_latitudes(
+            road_id=behavioral_state.ego_state.road_localization.road_id)
+        target_lane = behavioral_state.ego_state.road_localization.lane_num + semantic_action.cell[SEMANTIC_CELL_LANE]
+        target_lane_latitude = road_lane_latitudes[target_lane]
+
+        target_relative_s = target_lane_latitude - behavioral_state.ego_state.road_localization.full_lat
+        target_relative_d = BEHAVIORAL_PLANNING_DEFAULT_SPEED_LIMIT * BEHAVIORAL_PLANNING_TRAJECTORY_HORIZON
+
+        # TODO: Define safe distance from object using ACDA
+        safe_distance = 10.0
+
+        # Clip target longitudinal location between 0.0 and the safe distance behind the car
+        target_relative_d = float(
+            np.clip(target_relative_d, 0.0, object_relative_road_localization.rel_lon - safe_distance)[0])
+
+        return SemanticActionSpec(t=BEHAVIORAL_PLANNING_TRAJECTORY_HORIZON, v=BEHAVIORAL_PLANNING_DEFAULT_SPEED_LIMIT,
+                                  s_rel=target_relative_s, d_rel=target_relative_d)
