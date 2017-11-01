aa70a7e28258abc0bde01648cfe2a570d0cdea9a
Vlad Goldner <vlad.goldner@gm.com>
2017-11-01 16:29:07 +0200

    consider time, s_rel, velocity

--- src/planning/behavioral/policies/november_demo_semantic_policy.py
+++ src/planning/behavioral/policies/november_demo_semantic_policy.py
@@ -1,9 +1,14 @@
 import numpy as np
 from typing import List
 
+from logging import Logger
+from decision_making.src.planning.behavioral.behavioral_state import BehavioralState
+from decision_making.src.planning.behavioral.policy import PolicyConfig
 from decision_making.src.planning.behavioral.semantic_actions_policy import SemanticActionsPolicy, SemanticAction, \
     SemanticActionSpec, SEMANTIC_CELL_LANE
+from decision_making.src.prediction.predictor import Predictor
 from decision_making.src.state.state import State
+from mapping.src.model.map_api import MapAPI
 
 SEMANTIC_GRID_FRONT, SEMANTIC_GRID_ASIDE, SEMANTIC_GRID_BEHIND = 1, 0, -1
 GRID_MID = 10
@@ -13,12 +18,14 @@ SEMANTIC_OCCUPANCY_GRID_PARTITIONS_MARGIN_FROM_EGO = 1
 
 
 MIN_OVERTAKE_VEL = 2  # [m/s]
-
+MIN_CHANGE_LANE_TIME = 4  # sec
+MAX_CHANGE_LANE_TIME = 6  # sec
 
 class NovDemoPolicy(SemanticActionsPolicy):
 
-    def _eval_actions(self, state: State, semantic_actions: List[SemanticAction],
-                      actions_spec: List[SemanticActionSpec]) -> np.ndarray:
+    @staticmethod
+    def _eval_actions(behavioral_state: NovDemoBehavioralState, semantic_actions: List[SemanticAction],
+                      actions_spec: List[SemanticActionSpec], max_velocity: float) -> np.ndarray:
         """
         Evaluate the generated actions using the full state.
         Gets a list of actions to evaluate so and returns a vector representing their costs.
@@ -31,34 +38,42 @@ class NovDemoPolicy(SemanticActionsPolicy):
         :return: numpy array of costs of semantic actions
         """
 
-        straight_lane_ind = self._get_action_ind_by_lane(semantic_actions, 0)
-        left_lane_ind = self._get_action_ind_by_lane(semantic_actions, 1)
-        right_lane_ind = self._get_action_ind_by_lane(semantic_actions, -1)
+        straight_lane_ind = NovDemoPolicy._get_action_ind_by_lane(semantic_actions, actions_spec, 0, state.ego.v_x)
+        left_lane_ind = NovDemoPolicy._get_action_ind_by_lane(semantic_actions, actions_spec, 1, state.ego.v_x)
+        right_lane_ind = NovDemoPolicy._get_action_ind_by_lane(semantic_actions, actions_spec, -1, state.ego.v_x)
 
-        right_is_fast = right_lane_ind is not None and \
-                        self._max_velocity - actions_spec[right_lane_ind].v < MIN_OVERTAKE_VEL
+        desired_vel = max_velocity
+        if state.ego.v_x < actions_spec[straight_lane_ind].v - MIN_OVERTAKE_VEL:
+            desired_vel = state.ego.v_x
+
+        right_is_fast = right_lane_ind is not None and desired_vel - actions_spec[right_lane_ind].v < MIN_OVERTAKE_VEL
+        right_is_far = right_lane_ind is not None and actions_spec[right_lane_ind].t > MAX_CHANGE_LANE_TIME
         straight_is_fast = straight_lane_ind is not None and \
-                           self._max_velocity - actions_spec[straight_lane_ind].v < MIN_OVERTAKE_VEL
-        left_is_faster = left_lane_ind is not None and \
-                         (straight_lane_ind is None or
+                           desired_vel - actions_spec[straight_lane_ind].v < MIN_OVERTAKE_VEL
+        straight_is_far = straight_lane_ind is not None and actions_spec[straight_lane_ind].t > MAX_CHANGE_LANE_TIME
+        left_is_faster = left_lane_ind is not None and (straight_lane_ind is None or
                             actions_spec[left_lane_ind].v - actions_spec[straight_lane_ind].v >= MIN_OVERTAKE_VEL)
+        left_is_far = left_lane_ind is not None and actions_spec[left_lane_ind].t > MAX_CHANGE_LANE_TIME
 
         costs = np.zeros(len(semantic_actions))
         # move right if both straight and right lanes are not slow
-        if right_is_fast and (straight_is_fast or straight_lane_ind is None):
+        if (right_is_fast or right_is_far) and (straight_is_fast or straight_is_far or straight_lane_ind is None):
             costs[right_lane_ind] = 1.
             return costs
         # move left if straight is slow and the left is faster than straight
-        if not straight_is_fast and (left_is_faster or straight_lane_ind is None):
+        if not (straight_is_fast or straight_is_far) and \
+                (left_is_faster or left_is_far or straight_lane_ind is None):
             costs[left_lane_ind] = 1.
             return costs
         costs[straight_lane_ind] = 1.
         return costs
 
     @staticmethod
-    def _get_action_ind_by_lane(semantic_actions: List[SemanticAction], cell_lane: int):
+    def _get_action_ind_by_lane(semantic_actions: List[SemanticAction], actions_spec: List[SemanticActionSpec],
+                                cell_lane: int, cur_vel: float):
         action_ind = [i for i, action in enumerate(semantic_actions) if action.cell[SEMANTIC_CELL_LANE] == cell_lane]
-        if len(action_ind) > 0:
+        if len(action_ind) > 0 and (semantic_actions[action_ind[0]].cell[SEMANTIC_CELL_LANE] == 0 or
+                                        actions_spec[action_ind[0]].t > MIN_CHANGE_LANE_TIME):
             return action_ind[0]
         else:
             return None
---
+++ test/planning/behavioral/policy/test_nov_demo_eval_actions.py
@@ -0,0 +1,67 @@
+from typing import Tuple, Dict, List
+
+from decision_making.src.planning.behavioral.policies.november_demo_semantic_policy import NovDemoPolicy
+from decision_making.src.planning.behavioral.policy import PolicyConfig
+from decision_making.src.planning.behavioral.behavioral_state import BehavioralState
+from decision_making.src.planning.behavioral.semantic_actions_policy import SemanticAction, SEMANTIC_CELL_LANE, \
+    SEMANTIC_CELL_LON, SemanticActionType, SemanticActionSpec
+from decision_making.src.prediction.predictor import Predictor
+from decision_making.src.state.state import State, EgoState, DynamicObject, ObjectSize, OccupancyState
+from decision_making.src.state.state_module import StateModule
+from mapping.test.model.testable_map_fixtures import *
+from logging import Logger
+
+
+def test_novDemoEvalSemanticActions(testable_map_api):
+    max_velocity = 12  # m/s
+
+    ego_v = 10
+    pos1 = np.array([7, -3])
+    pos2 = np.array([11, 0])
+    pos3 = np.array([8, 3])
+    vel1 = 12
+    vel2 = 6
+    vel3 = 9
+
+    road_localization1 = StateModule._compute_road_localization(pos1, 0, testable_map_api)
+    road_localization2 = StateModule._compute_road_localization(pos2, 0, testable_map_api)
+    road_localization3 = StateModule._compute_road_localization(pos3, 0, testable_map_api)
+    size = ObjectSize(1.5, 0.5, 0)
+    obs = list([
+        DynamicObject(obj_id=0, timestamp=0, x=pos1[0], y=pos1[1], z=0, yaw=0, size=size,
+                      road_localization=road_localization1, confidence=1.0, v_x=vel1, v_y=0, acceleration_lon=0.0,
+                      omega_yaw=0.0),
+        DynamicObject(obj_id=1, timestamp=0, x=pos2[0], y=pos2[1], z=0, yaw=0, size=size,
+                      road_localization=road_localization2, confidence=1.0, v_x=vel2, v_y=0, acceleration_lon=0.0,
+                      omega_yaw=0.0),
+        DynamicObject(obj_id=2, timestamp=0, x=pos3[0], y=pos3[1], z=0, yaw=0, size=size,
+                      road_localization=road_localization3, confidence=1.0, v_x=vel3, v_y=0, acceleration_lon=0.0,
+                      omega_yaw=0.0)
+    ])
+
+    ego = EgoState(obj_id=-1, timestamp=0, x=0, y=0, z=0, yaw=0, size=size,
+                   road_localization=StateModule._compute_road_localization(np.array([0, 0]), 0.0, testable_map_api),
+                   confidence=1.0, v_x=ego_v, v_y=0, steering_angle=0.0, acceleration_lon=0.0, omega_yaw=0.0)
+
+    state = State(occupancy_state=OccupancyState(0, np.array([]), np.array([])), dynamic_objects=obs, ego_state=ego)
+    # behav_state = BehavioralState()
+    # predictor = Predictor(testable_map_api)
+    # policy = NovDemoPolicy(Logger("NovDemoTest"), PolicyConfig(), behav_state, predictor, testable_map_api,
+    #                        max_velocity)
+
+    # grid = {}
+    # grid[(-1, 1)] = [obs[0]]
+    # grid[(0, 1)] = [obs[1]]
+    # grid[(1, 1)] = [obs[2]]
+
+    semantic_actions = []
+    semantic_actions.append(SemanticAction((-1, 1), obs[0], SemanticActionType.FOLLOW))
+    semantic_actions.append(SemanticAction((0, 1), obs[1], SemanticActionType.FOLLOW))
+    semantic_actions.append(SemanticAction((1, 1), obs[2], SemanticActionType.FOLLOW))
+    actions_spec = []
+    actions_spec.append(SemanticActionSpec(t=5, v=vel1, s_rel=obs[0].x-ego.x, d_rel=obs[0].y-ego.y))
+    actions_spec.append(SemanticActionSpec(t=2, v=vel2, s_rel=obs[1].x-ego.x, d_rel=obs[1].y-ego.y))
+    actions_spec.append(SemanticActionSpec(t=5, v=vel3, s_rel=obs[2].x-ego.x, d_rel=obs[2].y-ego.y))
+
+    costs = NovDemoPolicy._eval_actions(state, semantic_actions, actions_spec, max_velocity)
+    print(costs)
