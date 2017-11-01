bbb91da179ba8a7b8f83c92c84ddf26a63404d01
Max Bluvstein <max.bluvstein@gm.com>
2017-11-01 09:51:05 +0200

    implementation of semantic actions enumeration

--- src/planning/behavioral/policies/november_demo_semantic_policy.py
+++ src/planning/behavioral/policies/november_demo_semantic_policy.py
@@ -1,5 +1,7 @@
-from decision_making.src.planning.behavioral.semantic_actions_policy import SemanticActionsPolicy
+from typing import List
 
+from decision_making.src.planning.behavioral.semantic_actions_policy import SemanticActionsPolicy, SemanticAction, \
+    SemanticActionType, SemanticBehavioralState
 
 SEMANTIC_GRID_FRONT, SEMANTIC_GRID_ASIDE, SEMANTIC_GRID_BEHIND = 1, 0, -1
 GRID_MID = 10
@@ -9,5 +11,31 @@ SEMANTIC_OCCUPANCY_GRID_PARTITIONS_MARGIN_FROM_EGO = 1
 
 
 class NovDemoPolicy(SemanticActionsPolicy):
-    pass
+
+    def _enumerate_actions(self, behavioral_state: SemanticBehavioralState) -> List[SemanticAction]:
+        """
+        Enumerate the list of possible semantic actions to be generated.
+        :param behavioral_state:
+        :return:
+        """
+
+        semantic_actions: List[SemanticAction] = list()
+
+        # Generate actions towards each of the cells in front of ego
+        for relative_lane_key in [-1, 0, 1]:
+            for longitudinal_key in [SEMANTIC_GRID_FRONT]:
+                semantic_cell = (relative_lane_key, longitudinal_key)
+                if semantic_cell in behavioral_state.road_occupancy_grid:
+                    # Select first (closest) object in cell
+                    target_obj = behavioral_state.road_occupancy_grid[semantic_cell][0]
+                else:
+                    # There are no objects in cell
+                    target_obj = None
+
+                semantic_action = SemanticAction(cell=semantic_cell, target_obj=target_obj,
+                                                 action_type=SemanticActionType.FOLLOW)
+
+                semantic_actions.append(semantic_action)
+
+        return semantic_actions
 
