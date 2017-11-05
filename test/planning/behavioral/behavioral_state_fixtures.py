858be170dd5f20d82084501f4ea4d1330666b561
Max Bluvstein <max.bluvstein@gm.com>
2017-11-05 17:21:15 +0200

    merged with 1.5.2 - Added bugfix of actions enumeration filtering

--- test/planning/behavioral/behavioral_state_fixtures.py
+++ test/planning/behavioral/behavioral_state_fixtures.py
@@ -67,6 +67,62 @@ def state_with_sorrounding_objects(testable_map_api: MapAPI):
 
 
 @pytest.fixture(scope='function')
+def state_with_ego_on_right_lane(testable_map_api: MapAPI):
+    map_api = testable_map_api
+
+    # Stub of occupancy grid
+    occupancy_state = OccupancyState(0, np.array([]), np.array([]))
+
+    car_size = ObjectSize(length=2.5, width=1.5, height=1.0)
+
+    # Ego state
+    ego_road_id = 1
+    ego_road_lon = 15.0
+    ego_road_lat = 1.5
+
+    ego_pos, ego_yaw = map_api._convert_road_to_global_coordinates(road_id=ego_road_id, lon=ego_road_lon,
+                                                                   lat=ego_road_lat)
+
+    road_localization = DynamicObject.compute_road_localization(global_pos=ego_pos, global_yaw=ego_yaw,
+                                                                map_api=map_api)
+    ego_state = EgoState(obj_id=0, timestamp=0, x=ego_pos[0], y=ego_pos[1], z=ego_pos[2], yaw=ego_yaw,
+                         size=car_size, confidence=1.0, v_x=0.0, v_y=0.0, steering_angle=0.0,
+                         acceleration_lon=0.0, omega_yaw=0.0, road_localization=road_localization)
+
+    dynamic_objects = []
+
+    yield State(occupancy_state=occupancy_state, dynamic_objects=dynamic_objects, ego_state=ego_state)
+
+
+@pytest.fixture(scope='function')
+def state_with_ego_on_left_lane(testable_map_api: MapAPI):
+    map_api = testable_map_api
+
+    # Stub of occupancy grid
+    occupancy_state = OccupancyState(0, np.array([]), np.array([]))
+
+    car_size = ObjectSize(length=2.5, width=1.5, height=1.0)
+
+    # Ego state
+    ego_road_id = 1
+    ego_road_lon = 15.0
+    ego_road_lat = 7.5
+
+    ego_pos, ego_yaw = map_api._convert_road_to_global_coordinates(road_id=ego_road_id, lon=ego_road_lon,
+                                                                   lat=ego_road_lat)
+
+    road_localization = DynamicObject.compute_road_localization(global_pos=ego_pos, global_yaw=ego_yaw,
+                                                                map_api=map_api)
+    ego_state = EgoState(obj_id=0, timestamp=0, x=ego_pos[0], y=ego_pos[1], z=ego_pos[2], yaw=ego_yaw,
+                         size=car_size, confidence=1.0, v_x=0.0, v_y=0.0, steering_angle=0.0,
+                         acceleration_lon=0.0, omega_yaw=0.0, road_localization=road_localization)
+
+    dynamic_objects = []
+
+    yield State(occupancy_state=occupancy_state, dynamic_objects=dynamic_objects, ego_state=ego_state)
+
+
+@pytest.fixture(scope='function')
 def nov_demo_state():
     ego_state = EgoState(obj_id=0, timestamp=0, x=15.0, y=0.0, z=0.0, yaw=0.0,
                          size=ObjectSize(length=2.5, width=1.5, height=1.0), confidence=1.0, v_x=7.0, v_y=0.0,
