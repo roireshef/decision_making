ca6c68cde0f3b2acdbf3fa6f357673200fc161c3
kz430x <roi.reshef@gm.com>
2017-12-06 17:48:51 +0100

    implemented taylor expansion for s coordinate with the frenet-serret formulas

--- src/planning/utils/frenet_serret_frame.py
+++ src/planning/utils/frenet_serret_frame.py
@@ -15,10 +15,55 @@ class FrenetSerretFrame:
         # TODO: move this outside
         self.s_max = s_max
         self.ds = ds
+
+        self.O, _ = CartesianFrame.resample_curve(curve=points, step_size=ds,
+                                                  desired_curve_len=s_max, preserve_step_size=True)
+
+        # T, N, _, self.k, _ = frenet_serret(np.c_[self.O, np.zeros(len(self.O))])
+        # self.T = T[:, :(C_Y+1)]
+        # self.N = N[:, :(C_Y+1)]
+
+        self.T, self.N, self.k = FrenetSerretFrame.fit_frenet(self.O)
         self.s_cumm = np.linspace(0.0, s_max, len(self.O))
 
-        self.O, _ = CartesianFrame.resample_curve(curve=points, step_size=ds, desired_curve_len=s_max, preserve_step_size=True)
-        self.T, self.N, self.B, self.k, self.t = frenet_serret(self.O)
+    @staticmethod
+    def fit_frenet(xy: CartesianPath2D):
+        if xy.shape[0] == 0:
+            raise ValueError('xyz array cannot be empty')
+
+        dxy = np.gradient(xy)[0]
+        ddxy = np.gradient(dxy)[0]
+
+        # magintudes
+        dxy_norm = np.linalg.norm(dxy, axis=1)
+
+        # Tangent
+        T = np.divide(dxy, np.c_[dxy_norm])
+
+        # Derivative of Tangent
+        dT = np.gradient(T)[0]
+        dT_norm = np.linalg.norm(dT, axis=1)
+
+        # Normal - robust to zero-curvature
+        N = FrenetSerretFrame.row_wise_normal(T)
+
+        # Curvature
+        cross_norm = np.abs(np.sum(dxy * FrenetSerretFrame.row_wise_normal(ddxy), axis=1))
+        k = np.zeros(len(T))
+        k[dxy_norm > 0] = np.c_[cross_norm[dxy_norm > 0]] / (np.c_[dxy_norm[dxy_norm > 0]] ** 3)
+        return T, N, np.c_[k]
+
+    @staticmethod
+    def row_wise_normal(mat: np.ndarray):
+        return np.c_[-mat[:, 1], mat[:, 0]]
+
+    # @staticmethod
+    # def norm(vec: np.ndarray):
+    #     norm = np.linalg.norm(vec, axis=1)
+    #     is_zero = np.where(norm == 0)
+    #     norm[is_zero] = np.finfo(float).eps
+    #
+    #     return norm
 
     def taylor_interp(self, s: np.ndarray) -> (CartesianPoint2D, CartesianPoint3D, CartesianPoint3D, float):
         """Given arbitrary s in the range [0, self.s_max], this function uses taylor approximation to return
@@ -29,11 +74,11 @@ class FrenetSerretFrame:
         k(s) is the curvature (scalar) - assumed to be constant in the neighborhood of the points in self.O and thus
         taken from the nearest point in self.O
         """
-        assert 0 <= s <= self.s_max
+        assert np.all(np.bitwise_and(0 <= s, s <= self.s_max))
 
         progress_ds = s / self.ds
         O_idx = np.round(progress_ds).astype(np.int)
-        delta_s = progress_ds - O_idx * self.ds
+        delta_s = ((progress_ds - O_idx) * self.ds)[:, np.newaxis]
 
         a_s = self.O[O_idx] + \
               delta_s * self.T[O_idx] + \
@@ -64,7 +109,7 @@ class FrenetSerretFrame:
         # return exact_O + self.N[O_idx, :2] * dx
 
         a_s, _, N_s, _ = self.taylor_interp(fpoints[:, FP_SX])
-        return a_s + N_s * fpoints[:, FP_DX]
+        return a_s + N_s * fpoints[:, [FP_DX]]
 
     def cpoints_to_fpoints(self, cpoints: CartesianPoint2D) -> FrenetTrajectory:
         # O_idx, progress = Euclidean.project_on_piecewise_linear_curve(cpoints)
@@ -83,12 +128,12 @@ class FrenetSerretFrame:
         # TODO: replace this with GD for finding more accurate s
         s_exact = s_approx
 
-        a_s, _, _, _ = self.taylor_interp(s_exact)
+        a_s, _, N_s, _ = self.taylor_interp(s_exact)
 
-        d = np.linalg.norm(cpoints - a_s, axis=1)
+        # project cpoints on the normals at a_s
+        d = np.einsum('ij,ij->i', cpoints - a_s, N_s)
 
-        return np.dstack((s_exact, d))
-
+        return np.c_[s_exact, d]
 
     def ftrajectories_to_ctrajectories(self, ftrajectories: FrenetTrajectories) -> CartesianExtendedTrajectories:
         """
@@ -96,12 +141,4 @@ class FrenetSerretFrame:
         :param ftrajectories: Frenet-frame trajectories (tensor)
         :return: Cartesian-frame trajectories (tensor)
         """
-        num_t = ftrajectories.shape[0]
-        num_p = ftrajectories.shape[1]
-
-        s_x = ftrajectories[:, :, FS_SX]
-        s_v = ftrajectories[:, :, FS_SV]
-        s_a = ftrajectories[:, :, FS_SA]
-        d_x = ftrajectories[:, :, FS_DX]
-        d_v = ftrajectories[:, :, FS_DV]
-        d_a = ftrajectories[:, :, FS_DA]
+        pass
---
+++ test/planning/utils/test_frenetSerretFrame.py
@@ -0,0 +1,40 @@
+from decision_making.src.planning.utils.frenet_moving_frame import FrenetMovingFrame
+from decision_making.src.planning.utils.frenet_serret_frame import FrenetSerretFrame
+from decision_making.test.planning.trajectory.utils import RouteFixture
+from mapping.src.transformations.geometry_utils import *
+
+
+def test_frenetSerretFrame_pointTwoWayConversion_accurate():
+    ACCURACY_TH = 0.1
+
+    route_points = RouteFixture.get_route(lng=200, k=0.05, step=40, lat=100, offset=-50.0)
+    cpoints = np.array([[220.0, 0.0], [150.0, 0.0],
+                        [280.0, 40.0], [320.0, 60.0],
+                        [370.0, 0.0]
+                        ])
+
+    frenet = FrenetSerretFrame(route_points, np.sum(np.linalg.norm(np.diff(route_points, axis=0), axis=1)), ds=1)
+
+    fpoints = frenet.cpoints_to_fpoints(cpoints)
+    new_cpoints = frenet.fpoints_to_cpoints(fpoints)
+
+    errors = np.linalg.norm(cpoints - new_cpoints, axis=1)
+
+    for error in errors.__iter__():
+        assert error < ACCURACY_TH, 'FrenetMovingFrame point conversions aren\'t accurate enough'
+
+    import matplotlib.pyplot as plt
+
+    fig = plt.figure()
+    p1 = fig.add_subplot(111)
+    p1.plot(route_points[:, 0], route_points[:, 1], '-r')
+    p1.plot(frenet.O[:, 0], frenet.O[:, 1], '-k')
+
+    for i in range(len(cpoints)):
+        p1.plot(cpoints[i, 0], cpoints[i, 1], '*b')
+        p1.plot(new_cpoints[i, 0], new_cpoints[i, 1], '.r')
+
+    print(errors)
+
+    fig.show()
+    fig.clear()
