589858745ffe36f700b9d5e44fe1745125c03524
kz430x <roi.reshef@gm.com>
2017-12-07 18:55:43 +0100

    finalized ftrajectories_to_ctrajectories

--- src/planning/utils/frenet_serret_frame.py
+++ src/planning/utils/frenet_serret_frame.py
@@ -3,11 +3,11 @@ import numpy as np
 from decision_making.src.global_constants import TRAJECTORY_ARCLEN_RESOLUTION
 from decision_making.src.planning.types import FP_SX, FP_DX, CartesianPoint2D, \
     FrenetTrajectory, CartesianPath2D, FrenetTrajectories, CartesianExtendedTrajectories, CartesianPoint3D, FS_SX, \
-    FS_SV, FS_SA, FS_DX, FS_DV, FS_DA
+    FS_SV, FS_SA, FS_DX, FS_DV, FS_DA, C_Y, C_X, CartesianExtendedTrajectory
 from mapping.src.transformations.geometry_utils import CartesianFrame, Euclidean
 
 
-class FrenetSerretFrame:
+class FrenetSerret2DFrame:
     def __init__(self, points: CartesianPath2D, s_max: float, ds: float = TRAJECTORY_ARCLEN_RESOLUTION):
         # TODO: move this outside
         self.s_max = s_max
@@ -16,41 +16,107 @@ class FrenetSerretFrame:
         self.O, _ = CartesianFrame.resample_curve(curve=points, step_size=ds,
                                                   desired_curve_len=s_max, preserve_step_size=True)
 
-        self.T, self.N, self.k = FrenetSerretFrame.fit_frenet(self.O)
+        self.T, self.N, self.k = FrenetSerret2DFrame._fit_frenet(self.O)
         self.s_cumm = np.linspace(0.0, s_max, len(self.O))
 
-    @staticmethod
-    def fit_frenet(xy: CartesianPath2D):
-        if xy.shape[0] == 0:
-            raise ValueError('xyz array cannot be empty')
+    def ftrajectory_to_ctrajectory(self, ftrajectory: FrenetTrajectory) -> CartesianExtendedTrajectory:
+        """
+        Transforms Frenet-frame trajectory to cartesian-frame trajectory, using tensor operations
+        :param ftrajectory: a frenet-frame trajectory
+        :return: a cartesian-frame trajectory (given in the coordinate frame of self.points)
+        """
+        return self.ftrajectories_to_ctrajectories(np.array([ftrajectory]))[0]
 
-        dxy = np.gradient(xy)[0]
-        ddxy = np.gradient(dxy)[0]
+    def ctrajectory_to_ftrajectory(self, ctrajectory: CartesianExtendedTrajectory) -> FrenetTrajectory:
+        """
+        Transforms Cartesian-frame trajectory to Frenet-frame trajectory, using tensor operations
+        :param ctrajectory: a cartesian-frame trajectory (in the coordinate frame of self.points)
+        :return: a frenet-frame trajectory
+        """
+        return self.ctrajectories_to_ftrajectories(np.array([ctrajectory]))[0]
 
-        # magintudes
-        dxy_norm = np.linalg.norm(dxy, axis=1)
+    def fpoints_to_cpoints(self, fpoints: FrenetTrajectory) -> CartesianPath2D:
+        """
+        Transforms frenet-frame point to cartesian-frame point (using self.curve) \n
+        :param fpoint: numpy array of frenet-point [sx, dx]
+        :return: cartesian-frame point [x, y]
+        """
+        a_s, _, N_s, _, _ = self._taylor_interp(fpoints[:, FP_SX])
+        return a_s + N_s * fpoints[:, [FP_DX]]
 
-        # Tangent
-        T = np.divide(dxy, np.c_[dxy_norm])
+    def cpoints_to_fpoints(self, cpoints: CartesianPoint2D) -> FrenetTrajectory:
+        O_idx, delta_s = Euclidean.project_on_piecewise_linear_curve(cpoints, self.O)
+        s_approx = (O_idx + delta_s) * self.ds
 
-        # Derivative of Tangent
-        dT = np.gradient(T)[0]
-        dT_norm = np.linalg.norm(dT, axis=1)
+        # TODO: replace this with GD for finding more accurate s
+        s_exact = s_approx
 
-        # Normal - robust to zero-curvature
-        N = FrenetSerretFrame._row_wise_normal(T)
+        a_s, _, N_s, _, _ = self._taylor_interp(s_exact)
 
-        # Curvature
-        cross_norm = np.abs(np.sum(dxy * FrenetSerretFrame._row_wise_normal(ddxy), axis=1))
-        k = np.zeros(len(T))
-        k[dxy_norm > 0] = np.c_[cross_norm[dxy_norm > 0]] / (np.c_[dxy_norm[dxy_norm > 0]] ** 3)
-        return T, N, np.c_[k]
+        # project cpoints on the normals at a_s
+        d = np.einsum('ij,ij->i', cpoints - a_s, N_s)
 
-    @staticmethod
-    def _row_wise_normal(mat: np.ndarray):
-        return np.c_[-mat[:, 1], mat[:, 0]]
+        return np.c_[s_exact, d]
 
-    def taylor_interp(self, s: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarray, float):
+    def ftrajectories_to_ctrajectories(self, ftrajectories: FrenetTrajectories) -> CartesianExtendedTrajectories:
+        """
+        Transforms Frenet-frame trajectories to cartesian-frame trajectories, using tensor operations
+        :param ftrajectories: Frenet-frame trajectories (tensor)
+        :return: Cartesian-frame trajectories (tensor)
+        """
+        num_t = ftrajectories.shape[0]
+        num_p = ftrajectories.shape[1]
+
+        s_x = ftrajectories[:, :, FS_SX]
+        s_v = ftrajectories[:, :, FS_SV]
+        s_a = ftrajectories[:, :, FS_SA]
+        d_x = ftrajectories[:, :, FS_DX]
+        d_v = ftrajectories[:, :, FS_DV]
+        d_a = ftrajectories[:, :, FS_DA]
+
+        a_r, T_r, N_r, k_r, k_r_tag = FrenetSerret2DFrame._taylor_interp(ftrajectories[:, :, FS_SX])
+        theta_r = np.arctan2(T_r[C_Y], T_r[C_X])
+
+        cached_term = 1 - k_r * d_x  # pre-compute terms to use below
+        d_tag = d_v / s_v  # 1st derivative of d_x by distance
+        tan_delta_theta = d_tag / cached_term
+        delta_theta = np.arctan2(d_tag, cached_term)
+        cos_delta_theta = np.cos(delta_theta)
+
+        # compute v_x (velocity in the heading direction)
+        v_x = np.divide(s_v * cached_term, cos_delta_theta)
+
+        # compute k_x (curvature)
+        d_tagtag = (d_a - d_tag * s_a) / (s_v ** 2)  # 2nd derivative of d_x by distance
+        k_x = d_tagtag + (k_r_tag * k_r * d_tag) * tan_delta_theta * cos_delta_theta ** 3 / cached_term ** 2 + \
+              k_r * cos_delta_theta / cached_term
+
+        # compute a_x (curvature)
+        delta_theta_tag = k_x / cos_delta_theta - k_r
+        a_x = s_a * cached_term / cos_delta_theta + \
+              s_v ** 2 / cos_delta_theta * (cached_term * tan_delta_theta * delta_theta_tag - (k_r_tag * k_r * d_tag))
+
+        # compute position (cartesian)
+        pos_x = a_r + N_r * d_x
+
+        # compute theta_x
+        theta_x = theta_r + delta_theta
+
+        return np.concatenate((pos_x.reshape([num_t, num_p, 2]), theta_x.reshape([num_t, num_p, 1]),
+                               v_x.reshape([num_t, num_p, 1]), a_x.reshape([num_t, num_p, 1]),
+                               k_x.reshape([num_t, num_p, 1])), axis=2)
+
+    def ctrajectories_to_ftrajectories(self, ctrajectories: CartesianExtendedTrajectories) -> FrenetTrajectories:
+        """
+        Transforms Cartesian-frame trajectories to Frenet-frame trajectories, using tensor operations
+        :param ctrajectories: Cartesian-frame trajectories (tensor)
+        :return: Frenet-frame trajectories (tensor)
+        """
+        pass
+
+    ## UTILITIES ##
+
+    def _taylor_interp(self, s: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray):
         """Given arbitrary s tensor (of shape D) of values in the range [0, self.s_max], this function uses taylor
         approximation to return a(s), T(s), N(s), k(s), where:
         a(s) is the map to Cartesian-frame (a point on the curve. will have shape of Dx2),
@@ -58,6 +124,7 @@ class FrenetSerretFrame:
         N(s) is the normal unit vector (will have shape of Dx2)
         k(s) is the curvature (scalar) - assumed to be constant in the neighborhood of the points in self.O and thus
         taken from the nearest point in self.O (will have shape of D)
+        k'(s) is the derivative of the curvature (by distance d(s))
         """
         assert np.all(np.bitwise_and(0 <= s, s <= self.s_max))
 
@@ -78,51 +145,41 @@ class FrenetSerretFrame:
               delta_s * self.k[O_idx] * self.T[O_idx] - \
               delta_s ** 2 / 2 * self.k[O_idx] ** 2 * self.N[O_idx]
 
-        return a_s, T_s, N_s, self.k[O_idx]
+        k_s = self.k[O_idx] + \
+              delta_s * np.gradient(self.k)[O_idx] + \
+              delta_s ** 2 / 2 * np.gradient(np.gradient(self.k))[O_idx]
 
-    def fpoints_to_cpoints(self, fpoints: FrenetTrajectory) -> CartesianPath2D:
-        """
-        Transforms frenet-frame point to cartesian-frame point (using self.curve) \n
-        :param fpoint: numpy array of frenet-point [sx, dx]
-        :return: cartesian-frame point [x, y]
-        """
-        a_s, _, N_s, _ = self.taylor_interp(fpoints[:, FP_SX])
-        return a_s + N_s * fpoints[:, [FP_DX]]
+        k_s_tag = np.gradient(self.k)[O_idx] + delta_s * np.gradient(np.gradient(self.k))[O_idx]
 
-    def cpoints_to_fpoints(self, cpoints: CartesianPoint2D) -> FrenetTrajectory:
-        O_idx, delta_s = Euclidean.project_on_piecewise_linear_curve(cpoints, self.O)
-        s_approx = (O_idx + delta_s) * self.ds
+        return a_s, T_s, N_s, k_s, k_s_tag
 
-        # TODO: replace this with GD for finding more accurate s
-        s_exact = s_approx
+    @staticmethod
+    def _fit_frenet(xy: CartesianPath2D):
+        if xy.shape[0] == 0:
+            raise ValueError('xyz array cannot be empty')
 
-        a_s, _, N_s, _ = self.taylor_interp(s_exact)
+        dxy = np.gradient(xy)[0]
+        ddxy = np.gradient(dxy)[0]
 
-        # project cpoints on the normals at a_s
-        d = np.einsum('ij,ij->i', cpoints - a_s, N_s)
+        # magintudes
+        dxy_norm = np.linalg.norm(dxy, axis=1)
 
-        return np.c_[s_exact, d]
+        # Tangent
+        T = np.divide(dxy, np.c_[dxy_norm])
 
-    def ftrajectories_to_ctrajectories(self, ftrajectories: FrenetTrajectories) -> CartesianExtendedTrajectories:
-        """
-        Transforms Frenet-frame trajectories to cartesian-frame trajectories, using tensor operations
-        :param ftrajectories: Frenet-frame trajectories (tensor)
-        :return: Cartesian-frame trajectories (tensor)
-        """
-        s_x = ftrajectories[:, :, FS_SX]
-        s_v = ftrajectories[:, :, FS_SV]
-        s_a = ftrajectories[:, :, FS_SA]
-        d_x = ftrajectories[:, :, FS_DX]
-        d_v = ftrajectories[:, :, FS_DV]
-        d_a = ftrajectories[:, :, FS_DA]
+        # Derivative of Tangent
+        dT = np.gradient(T)[0]
+        dT_norm = np.linalg.norm(dT, axis=1)
 
-        pos_r, T_r, N_r, k_r = FrenetSerretFrame.taylor_interp(ftrajectories[:, :, FS_SX])
-        theta_r =
+        # Normal - robust to zero-curvature
+        N = FrenetSerret2DFrame._row_wise_normal(T)
 
-    def ctrajectories_to_ftrajectories(self, ctrajectories: CartesianExtendedTrajectories) -> FrenetTrajectories:
-        """
-        Transforms Cartesian-frame trajectories to Frenet-frame trajectories, using tensor operations
-        :param ctrajectories: Cartesian-frame trajectories (tensor)
-        :return: Frenet-frame trajectories (tensor)
-        """
-        pass
+        # Curvature
+        cross_norm = np.abs(np.sum(dxy * FrenetSerret2DFrame._row_wise_normal(ddxy), axis=1))
+        k = np.zeros(len(T))
+        k[dxy_norm > 0] = np.c_[cross_norm[dxy_norm > 0]] / (np.c_[dxy_norm[dxy_norm > 0]] ** 3)
+        return T, N, np.c_[k]
+
+    @staticmethod
+    def _row_wise_normal(mat: np.ndarray):
+        return np.c_[-mat[:, 1], mat[:, 0]]
