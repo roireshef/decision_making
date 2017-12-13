3d8f60aecab69aca42494afc5774cf73c4bd77ce
Raz <raz.nissim@gm.com>
2017-12-13 14:36:38 +0200

    finishing merge from LCM  development branch into BP cleanup.

--- test/planning/test_planning.py
+++ test/planning/test_planning.py
@@ -8,13 +8,10 @@ from decision_making.src.planning.behavioral.policies.semantic_actions_grid_poli
 from decision_making.src.planning.trajectory.optimal_control.werling_planner import WerlingPlanner
 from decision_making.src.planning.trajectory.trajectory_planning_facade import TrajectoryPlanningFacade
 from decision_making.src.planning.trajectory.trajectory_planning_strategy import TrajectoryPlanningStrategy
-from decision_making.src.prediction.predictor import Predictor
 from decision_making.src.prediction.road_following_predictor import RoadFollowingPredictor
 from common_data.lcm.config import pubsub_topics
 from decision_making.test.constants import MAP_SERVICE_ABSOLUTE_PATH
 from decision_making.test.planning.custom_fixtures import pubsub, behavioral_facade, state_module, navigation_facade
-from common_data.lcm.generatedFiles.gm_lcm.LcmTrajectoryParameters import LcmTrajectoryParameters
-from common_data.lcm.generatedFiles.gm_lcm.LcmTrajectoryData import LcmTrajectoryData
 
 from mapping.test.model.testable_map_fixtures import map_api_mock
 
