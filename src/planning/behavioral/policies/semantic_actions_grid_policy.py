ba607b41b8a069cbe08d2b0021774886a4be0f6b
Nitzan <nitzan.herzog@gm.com>
2017-12-20 18:08:08 +0200

    Changed speed limits to include small negative velocities. Fixed redundancy in resampling of road curve for generation of reference route.

--- src/planning/behavioral/policies/semantic_actions_grid_policy.py
+++ src/planning/behavioral/policies/semantic_actions_grid_policy.py
@@ -292,7 +292,7 @@ class SemanticActionsGridPolicy(SemanticActionsPolicy):
         dist_from_ref_sq_cost = 0.0
 
         # TODO: set velocity and acceleration limits properly
-        velocity_limits = np.array([0.0, 60.0])  # [m/s]. not a constant because it might be learned. TBD
+        velocity_limits = np.array([-1.0, 60.0])  # [m/s]. not a constant because it might be learned. TBD
         acceleration_limits = np.array([A_LON_MIN - A_LON_EPS, A_LON_MAX + A_LON_EPS])
         cost_params = TrajectoryCostParams(left_lane_cost=left_lane_cost,
                                            right_lane_cost=right_lane_cost,
@@ -493,10 +493,4 @@ class SemanticActionsGridPolicy(SemanticActionsPolicy):
             steps_num=int(np.ceil(lookahead_distance / TRAJECTORY_ARCLEN_RESOLUTION)),
             navigation_plan=navigation_plan)
 
-        # interpolate and create uniformly spaced path
-        reference_route_xy_resampled, _ = CartesianFrame.resample_curve(curve=lookahead_path,
-                                                                        step_size=TRAJECTORY_ARCLEN_RESOLUTION,
-                                                                        desired_curve_len=lookahead_distance,
-                                                                        preserve_step_size=False)
-
-        return reference_route_xy_resampled
+        return lookahead_path
