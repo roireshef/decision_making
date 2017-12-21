67b1558862c4f09882343767fb8dd302429e9d08
liat <liat.sless@gm.com>
2017-12-21 14:17:30 +0200

    Adding trajectory mock

---
+++ test/dm_main_test.py
@@ -0,0 +1,84 @@
+from os import getpid
+
+from common_data.lcm.config import config_defs
+from common_data.lcm.python.Communication.lcmpubsub import LcmPubSub
+from common_data.src.communication.pubsub.pubsub_factory import create_pubsub
+from decision_making.paths import Paths
+from decision_making.src.dm_main import DmInitialization
+from decision_making.src.global_constants import BEHAVIORAL_PLANNING_MODULE_PERIOD, TRAJECTORY_PLANNING_MODULE_PERIOD, \
+    DM_MANAGER_NAME_FOR_LOGGING, TRAJECTORY_PLANNING_NAME_FOR_LOGGING, TRAJECTORY_TIME_RESOLUTION
+from decision_making.src.manager.dm_manager import DmManager
+from decision_making.src.manager.dm_process import DmProcess
+from decision_making.src.manager.dm_trigger import DmTriggerType
+from decision_making.src.planning.trajectory.trajectory_planning_facade import TrajectoryPlanningFacade
+from decision_making.src.planning.trajectory.trajectory_planning_strategy import TrajectoryPlanningStrategy
+from decision_making.src.prediction.road_following_predictor import RoadFollowingPredictor
+from decision_making.test.planning.trajectory.fixed_trajectory_planner import FixedTrajectoryPlanner
+from decision_making.test.utils import Utils
+from mapping.src.service.map_service import MapService
+from rte.python.logger.AV_logger import AV_Logger
+from rte.python.os import catch_interrupt_signals
+
+import numpy as np
+
+class DmMockInitialization:
+    @staticmethod
+    def create_trajectory_planner_mock() -> TrajectoryPlanningFacade:
+        logger = AV_Logger.get_logger(TRAJECTORY_PLANNING_NAME_FOR_LOGGING)
+        pubsub = create_pubsub(config_defs.LCM_SOCKET_CONFIG, LcmPubSub)
+
+        # Init map
+        MapService.initialize()
+        predictor = RoadFollowingPredictor(logger)
+
+        # TODO: fill the strategy handlers
+        fixed_trajectory = Utils.read_trajectory(Paths.get_resource_absolute_path_filename(
+            'fixed_trajectory_files/lane_changes_from_north_50kmh_change_7sec_cruise_7sec.txt'))
+
+        step_size = TRAJECTORY_PLANNING_MODULE_PERIOD / TRAJECTORY_TIME_RESOLUTION
+        planner = FixedTrajectoryPlanner(logger, predictor, fixed_trajectory, step_size,
+                                         np.array([1114.69414522, 8.05544111]))
+
+        strategy_handlers = {TrajectoryPlanningStrategy.HIGHWAY: planner,
+                             TrajectoryPlanningStrategy.PARKING: planner,
+                             TrajectoryPlanningStrategy.TRAFFIC_JAM: planner}
+
+        trajectory_planning_module = TrajectoryPlanningFacade(pubsub=pubsub, logger=logger,
+                                                              strategy_handlers=strategy_handlers)
+        return trajectory_planning_module
+
+
+def main():
+    modules_list = \
+        [
+            DmProcess(DmInitialization.create_navigation_planner,
+                      trigger_type=DmTriggerType.DM_TRIGGER_PERIODIC,
+                      trigger_args={'period': BEHAVIORAL_PLANNING_MODULE_PERIOD}),
+
+            DmProcess(DmInitialization.create_state_module,
+                      trigger_type=DmTriggerType.DM_TRIGGER_NONE,
+                      trigger_args={}),
+
+            DmProcess(DmInitialization.create_behavioral_planner,
+                      trigger_type=DmTriggerType.DM_TRIGGER_PERIODIC,
+                      trigger_args={'period': BEHAVIORAL_PLANNING_MODULE_PERIOD}),
+
+            DmProcess(DmMockInitialization.create_trajectory_planner_mock,
+                      trigger_type=DmTriggerType.DM_TRIGGER_PERIODIC,
+                      trigger_args={'period': TRAJECTORY_PLANNING_MODULE_PERIOD})
+        ]
+    logger = AV_Logger.get_logger(DM_MANAGER_NAME_FOR_LOGGING)
+    logger.debug('%d: (main) registered signal handler', getpid())
+    catch_interrupt_signals()
+    manager = DmManager(logger, modules_list)
+    manager.start_modules()
+    try:
+        manager.wait_for_submodules()
+    except KeyboardInterrupt:
+        logger.info('%d: (main) Interrupted by signal!', getpid())
+        pass
+    finally:
+        manager.stop_modules()
+
+
+main()
---
+++ test/planning/trajectory/fixed_trajectory_planner.py
@@ -0,0 +1,86 @@
+from abc import abstractmethod
+from logging import Logger
+from typing import Tuple
+
+import numpy as np
+
+from decision_making.src.exceptions import NoValidTrajectoriesFound
+from decision_making.src.global_constants import NEGLIGIBLE_DISPOSITION_LON, NEGLIGIBLE_DISPOSITION_LAT
+from decision_making.src.messages.trajectory_parameters import TrajectoryCostParams
+from decision_making.src.planning.trajectory.trajectory_planner import TrajectoryPlanner, SamplableTrajectory
+from decision_making.src.planning.types import C_V, \
+    CartesianExtendedState, CartesianTrajectories, CartesianPath2D, CartesianExtendedTrajectory, CartesianPoint2D
+from decision_making.src.prediction.predictor import Predictor
+from decision_making.src.state.state import State
+
+
+# TODO: fill the units in timestamps
+class FixedSamplableTrajectory(SamplableTrajectory):
+
+    def __init__(self, fixed_trajectory: CartesianExtendedTrajectory):
+        super().__init__(0, np.inf)
+        self._fixed_trajectory = fixed_trajectory
+
+    def sample(self, time_points: np.ndarray) -> CartesianExtendedTrajectory:
+        """
+        This function takes an array of time stamps and returns an array of points <x, y, theta, v, acceleration,
+        curvature> along the trajectory. Note: this function ignores the time_points parameter and returns the
+        fixed trajectory which is already sampled.
+        :param time_points: 1D numpy array of time stamps *in seconds* (global self.timestamp)
+        :return: 2D numpy array with every row having the format of <x, y, yaw, velocity, a, k>
+        """
+        return self._fixed_trajectory
+
+
+class FixedTrajectoryPlanner(TrajectoryPlanner):
+    """
+            FixedTrajectoryPlanner purpose is once the ego reached the trigger position,
+            every time the trajectory planner is called, output a trajectory
+            that advances incrementally on fixed_trajectory by step size
+    """
+
+    def __init__(self, logger: Logger, predictor: Predictor, fixed_trajectory: CartesianExtendedTrajectory, step_size: int,
+                 trigger_pos: CartesianPoint2D):
+        """
+        :param logger:
+        :param fixed_trajectory: a fixed trajectory to advance on
+        :param step_size: the size by which to advance on the trajectory each time
+        :param trigger_pos: the position that triggers the first trajectory output
+        """
+        super().__init__(logger, predictor)
+        self._fixed_trajectory = fixed_trajectory
+        self._step_size = step_size
+        self._trajectory_advancing = 0
+        self._trigger_pos = trigger_pos
+        self._triggered = False
+
+    def plan(self, state: State, reference_route: CartesianPath2D, goal: CartesianExtendedState, goal_time: float,
+             cost_params: TrajectoryCostParams) -> Tuple[SamplableTrajectory, CartesianTrajectories, np.ndarray]:
+        """
+        Once the ego reached the trigger position, every time the trajectory planner is called, output a trajectory
+        that advances incrementally on fixed_trajectory by step size. Otherwise raise NoValidTrajectoriesFound
+        :param goal_time: ignored
+        :param state: environment & ego state object
+        :param reference_route: ignored
+        :param goal: ignored
+        :param cost_params: ignored
+        :return: a tuple of: (samplable represantation of the fixed trajectory, tensor of the fixed trajectory,
+         and numpy array of zero as the trajectory's cost)
+        """
+
+        current_pos = np.array([state.ego_state.x, state.ego_state.y])
+
+        if not self._triggered and np.all(np.abs(current_pos - self._trigger_pos) <
+                                          np.array([NEGLIGIBLE_DISPOSITION_LON, NEGLIGIBLE_DISPOSITION_LAT])):
+            self._triggered = True
+
+        if self._triggered:
+            current_trajectory = self._fixed_trajectory[self._trajectory_advancing:]
+
+            self._trajectory_advancing += self._step_size
+
+            # TODO: currently no one does anything with the cost, the array here is dummy
+            return FixedSamplableTrajectory(current_trajectory), \
+                   np.array([current_trajectory[:, :(C_V + 1)]]), np.array([0])
+        else:
+            raise NoValidTrajectoriesFound("Didn't reach trigger point yet.")
