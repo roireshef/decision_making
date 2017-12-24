1f93ea523922eeabf46944666356754cf20041d1
kz430x <roi.reshef@gm.com>
2017-12-24 14:42:19 +0100

    added BP mock

--- test/constants.py
+++ test/constants.py
@@ -1,3 +1,22 @@
+from decision_making.paths import Paths
+from decision_making.src.dm_main import DmInitialization
+from decision_making.test.dm_main_test import DmMockInitialization
+
 LCM_PUB_SUB_MOCK_NAME_FOR_LOGGING = "LCM_PUB_SUB_MOCK"
 
 MAP_SERVICE_ABSOLUTE_PATH = 'mapping.src.service.map_service.MapService.get_instance'
+
+# TP_INITIALIZER = DmInitialization
+TP_INITIALIZER = DmMockInitialization
+TP_MOCK_FIXED_TRAJECTORY_FILENAME = Paths.get_resource_absolute_path_filename(
+    'fixed_trajectory_files/trajectory_from_recording_2017_11_08_run2.txt')
+
+# BP_INITIALIZER = DmInitialization
+BP_INITIALIZER = DmMockInitialization
+BP_MOCK_FIXED_SPECS = {
+    'ROAD_ID': 20,
+    'LANE_NUM': 1,
+    'TARGET_LONGITUDE': 1000,  # [m] from road's start
+    'TARGET_VELOCITY': 20,  # [m/sec]
+    'PLANNING_TIME': 10  # [sec]
+}
--- test/dm_main_test.py
+++ test/dm_main_test.py
@@ -3,28 +3,38 @@ from os import getpid
 from common_data.lcm.config import config_defs
 from common_data.lcm.python.Communication.lcmpubsub import LcmPubSub
 from common_data.src.communication.pubsub.pubsub_factory import create_pubsub
-from decision_making.paths import Paths
 from decision_making.src import global_constants
 from decision_making.src.dm_main import DmInitialization
 from decision_making.src.global_constants import BEHAVIORAL_PLANNING_MODULE_PERIOD, TRAJECTORY_PLANNING_MODULE_PERIOD, \
-    DM_MANAGER_NAME_FOR_LOGGING, TRAJECTORY_PLANNING_NAME_FOR_LOGGING, TRAJECTORY_TIME_RESOLUTION
+    DM_MANAGER_NAME_FOR_LOGGING, TRAJECTORY_PLANNING_NAME_FOR_LOGGING, TRAJECTORY_TIME_RESOLUTION, \
+    BEHAVIORAL_PLANNING_NAME_FOR_LOGGING, EGO_LENGTH, EGO_WIDTH, EGO_HEIGHT
 from decision_making.src.manager.dm_manager import DmManager
 from decision_making.src.manager.dm_process import DmProcess
 from decision_making.src.manager.dm_trigger import DmTriggerType
+from decision_making.src.messages.navigation_plan_message import NavigationPlanMsg
+from decision_making.src.messages.trajectory_parameters import TrajectoryParams, TrajectoryCostParams
+from decision_making.src.messages.visualization.behavioral_visualization_message import BehavioralVisualizationMsg
+from decision_making.src.planning.behavioral.behavioral_facade import BehavioralFacade
+from decision_making.src.planning.behavioral.policies.semantic_actions_grid_policy import SemanticActionsGridPolicy
 from decision_making.src.planning.trajectory.trajectory_planning_facade import TrajectoryPlanningFacade
 from decision_making.src.planning.trajectory.trajectory_planning_strategy import TrajectoryPlanningStrategy
-from decision_making.src.planning.types import C_Y
+from decision_making.src.planning.types import C_Y, C_X
 from decision_making.src.prediction.road_following_predictor import RoadFollowingPredictor
+from decision_making.src.state.state import ObjectSize
+from decision_making.test.constants import TP_MOCK_FIXED_TRAJECTORY_FILENAME, BP_MOCK_FIXED_SPECS, TP_INITIALIZER, \
+    BP_INITIALIZER
+from decision_making.test.planning.behavioral.mock_behavioral_facade import BehavioralFacadeMock
 from decision_making.test.planning.trajectory.fixed_trajectory_planner import FixedTrajectoryPlanner
 from decision_making.test.utils import Utils
 from mapping.src.service.map_service import MapService
 from rte.python.logger.AV_logger import AV_Logger
 from rte.python.os import catch_interrupt_signals
+import numpy as np
 
 
 class DmMockInitialization:
     @staticmethod
-    def create_trajectory_planner_mock() -> TrajectoryPlanningFacade:
+    def create_trajectory_planner() -> TrajectoryPlanningFacade:
         logger = AV_Logger.get_logger(TRAJECTORY_PLANNING_NAME_FOR_LOGGING)
         pubsub = create_pubsub(config_defs.LCM_SOCKET_CONFIG, LcmPubSub)
 
@@ -32,8 +42,7 @@ class DmMockInitialization:
         MapService.initialize()
         predictor = RoadFollowingPredictor(logger)
 
-        fixed_trajectory = Utils.read_trajectory(Paths.get_resource_absolute_path_filename(
-            'fixed_trajectory_files/trajectory_from_recording_2017_11_08_run2.txt'))
+        fixed_trajectory = Utils.read_trajectory(TP_MOCK_FIXED_TRAJECTORY_FILENAME)
 
         step_size = TRAJECTORY_PLANNING_MODULE_PERIOD / TRAJECTORY_TIME_RESOLUTION
         planner = FixedTrajectoryPlanner(logger, predictor, fixed_trajectory, step_size,
@@ -47,6 +56,43 @@ class DmMockInitialization:
                                                               strategy_handlers=strategy_handlers)
         return trajectory_planning_module
 
+    @staticmethod
+    def create_behavioral_planner() -> BehavioralFacade:
+        logger = AV_Logger.get_logger(BEHAVIORAL_PLANNING_NAME_FOR_LOGGING)
+        pubsub = create_pubsub(config_defs.LCM_SOCKET_CONFIG, LcmPubSub)
+
+        # Init map
+        MapService.initialize()
+
+        road = MapService.get_instance().get_road(BP_MOCK_FIXED_SPECS['ROAD_ID'])
+        desired_lat = road.lane_width * (BP_MOCK_FIXED_SPECS['LANE_NUM'] + 0.5)
+        ref_route = MapService.get_instance().get_lookahead_points(
+            initial_road_id=BP_MOCK_FIXED_SPECS['ROAD_ID'], initial_lon=0,
+            lookahead_dist=BP_MOCK_FIXED_SPECS['TARGET_LONGITUDE'],
+            desired_lat=desired_lat,
+            navigation_plan=NavigationPlanMsg(road_ids=np.array([BP_MOCK_FIXED_SPECS['ROAD_ID']])))
+
+        target_pose, target_yaw = MapService.get_instance().convert_road_to_global_coordinates(
+            road_id=BP_MOCK_FIXED_SPECS['ROAD_ID'], lon=BP_MOCK_FIXED_SPECS['TARGET_LONGITUDE'], lat=desired_lat)
+        target_state = np.append(target_pose[[C_X, C_Y]], [target_yaw, BP_MOCK_FIXED_SPECS['TARGET_VELOCITY']])
+
+        cost_params = SemanticActionsGridPolicy._generate_cost_params(
+            road_id=BP_MOCK_FIXED_SPECS['ROAD_ID'], ego_size=ObjectSize(EGO_LENGTH, EGO_WIDTH, EGO_HEIGHT), 
+            target_path_latitude=desired_lat)
+
+        params = TrajectoryParams(strategy=TrajectoryPlanningStrategy.HIGHWAY,
+                                  reference_route=ref_route,
+                                  target_state=target_state,
+                                  cost_params=cost_params,
+                                  time=BP_MOCK_FIXED_SPECS['PLANNING_TIME'])
+
+        viz_msg = BehavioralVisualizationMsg(reference_route=ref_route)
+
+        behavioral_module = BehavioralFacadeMock(pubsub=pubsub, logger=logger,
+                                                 trajectory_params=params, visualization_msg=viz_msg)
+        return behavioral_module
+
+
 def main():
     modules_list = \
         [
@@ -58,11 +104,11 @@ def main():
                       trigger_type=DmTriggerType.DM_TRIGGER_NONE,
                       trigger_args={}),
 
-            DmProcess(DmInitialization.create_behavioral_planner,
+            DmProcess(BP_INITIALIZER.create_behavioral_planner,
                       trigger_type=DmTriggerType.DM_TRIGGER_PERIODIC,
                       trigger_args={'period': BEHAVIORAL_PLANNING_MODULE_PERIOD}),
 
-            DmProcess(DmMockInitialization.create_trajectory_planner_mock,
+            DmProcess(TP_INITIALIZER.create_trajectory_planner,
                       trigger_type=DmTriggerType.DM_TRIGGER_PERIODIC,
                       trigger_args={'period': TRAJECTORY_PLANNING_MODULE_PERIOD})
         ]
