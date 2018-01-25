ddebefff7b2f487832fc48fdd1d2819356aaba4d
RoiReshef <roi.reshef@gm.com>
2018-01-25 12:30:47 +0100

    started to modify tests for frenet conversion

--- test/planning/utils/test_frenetSerretFrame.py
+++ test/planning/utils/test_frenetSerretFrame.py
@@ -5,7 +5,7 @@ from decision_making.src.planning.types import CartesianPoint2D
 import numpy as np
 
 def test_cpointsToFpointsToCpoints_pointTwoWayConversion_accurate():
-    ACCURACY_TH = 10 ** -3
+    ACCURACY_TH = 1e-3  # up to 1 [mm] error in euclidean distance
 
     route_points = RouteFixture.get_route(lng=200, k=0.05, step=40, lat=100, offset=-50.0)
     cpoints = np.array([[220.0, 0.0], [150.0, 0.0],
@@ -13,36 +13,35 @@ def test_cpointsToFpointsToCpoints_pointTwoWayConversion_accurate():
                         [370.0, 0.0]
                         ])
 
-    frenet = FrenetSerret2DFrame(route_points, ds=1)
+    frenet = FrenetSerret2DFrame(route_points)
 
     fpoints = frenet.cpoints_to_fpoints(cpoints)
     new_cpoints = frenet.fpoints_to_cpoints(fpoints)
 
     errors = np.linalg.norm(cpoints - new_cpoints, axis=1)
 
-    for error in errors.__iter__():
-        assert error < ACCURACY_TH, 'FrenetMovingFrame point conversions aren\'t accurate enough'
+    np.testing.assert_array_less(errors, ACCURACY_TH, 'FrenetMovingFrame point conversions aren\'t accurate enough')
 
-    ## FOR DEBUG PURPOSES
-    # import matplotlib.pyplot as plt
-    #
-    # fig = plt.figure()
-    # p1 = fig.add_subplot(111)
-    # p1.plot(route_points[:, 0], route_points[:, 1], '-r')
-    # p1.plot(frenet.O[:, 0], frenet.O[:, 1], '-k')
-    #
-    # for i in range(len(cpoints)):
-    #     p1.plot(cpoints[i, 0], cpoints[i, 1], '*b')
-    #     p1.plot(new_cpoints[i, 0], new_cpoints[i, 1], '.r')
-    #
-    # print(errors)
-    #
-    # fig.show()
-    # fig.clear()
+    # FOR DEBUG PURPOSES
+    import matplotlib.pyplot as plt
+
+    fig = plt.figure()
+    p1 = fig.add_subplot(111)
+    p1.plot(route_points[:, 0], route_points[:, 1], '-r')
+    p1.plot(frenet.O[:, 0], frenet.O[:, 1], '-k')
+
+    for i in range(len(cpoints)):
+        p1.plot(cpoints[i, 0], cpoints[i, 1], '*b')
+        p1.plot(new_cpoints[i, 0], new_cpoints[i, 1], '.r')
+
+    print(errors)
+
+    fig.show()
+    fig.clear()
 
 
 def test_ctrajectoryToFtrajectoryToCtrajectory_pointTwoWayConversion_accuratePoseAndVelocity():
-    ACCURACY_TH = 10 ** -3
+    ACCURACY_TH = 1e-5
 
     route_points = RouteFixture.get_route(lng=200, k=0.05, step=40, lat=100, offset=-50.0)
     cpoints = np.array([[150.0, 0.0, -np.pi/8, 10.0, 1.0, 1e-2], [250.0, 0.0, np.pi/6, 20.0, 1.1, -1e-2],
@@ -50,16 +49,16 @@ def test_ctrajectoryToFtrajectoryToCtrajectory_pointTwoWayConversion_accuratePos
                         [370.0, 0.0, np.pi/9, 2.5, -2.0, 0.0]
                         ])
 
-    frenet = FrenetSerret2DFrame(route_points, ds=1)
+    frenet = FrenetSerret2DFrame(route_points)
 
     fpoints = frenet.ctrajectory_to_ftrajectory(cpoints)
     new_cpoints = frenet.ftrajectory_to_ctrajectory(fpoints)
 
     # currently there is no guarantee on the accuracy of acceleration and curvature
-    errors = np.linalg.norm((cpoints - new_cpoints)[:, :C_A], axis=1)
+    errors = np.abs(cpoints - new_cpoints)
 
-    for error in errors.__iter__():
-        assert error < ACCURACY_TH, 'FrenetMovingFrame point conversions aren\'t accurate enough'
+    np.testing.assert_array_less(errors, ACCURACY_TH,
+                                 err_msg='FrenetMovingFrame point conversions aren\'t accurate enough')
 
     ## FOR DEBUG PURPOSES
     # import matplotlib.pyplot as plt
@@ -85,7 +84,7 @@ def test_projectCartesianPoint_fivePointsProjection_accurate():
                         [280.0, 40.0], [320.0, 60.0],
                         [370.0, 0.0]
                         ])
-    frenet = FrenetSerret2DFrame(route_points, ds=1)
+    frenet = FrenetSerret2DFrame(route_points)
 
     correct_s = np.array([225.92931598, 149.93270474, 334.6431725, 368.37501329, 390.22811868])
     correct_a_s = np.array([[226.09287019, -48.59049741], [149.93758273, -49.97189866],
