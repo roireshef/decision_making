fix division by zero in _project_cartesian_points (FrenetSeretFrame.py) in werling_planner, sx_range starts not before the average between start s and goal s

src/planning/trajectory/optimal_control/werling_planner.py
@@ -85,7 +85,8 @@ class WerlingPlanner(TrajectoryPlanner):
         goal_frenet_state: FrenetState = frenet.cstate_to_fstate(goal)
 
         # TODO: Determine desired final state search grid - this should be fixed with introducing different T_s, T_d
-        sx_range = np.linspace(np.max((SX_OFFSET_MIN + goal_frenet_state[FS_SX], 0)),
+        sx_range = np.linspace(np.max((SX_OFFSET_MIN + goal_frenet_state[FS_SX],
+                                       (goal_frenet_state[FS_SX] + ego_frenet_state[FS_SX]) / 2)),
                                np.min((SX_OFFSET_MAX + goal_frenet_state[FS_SX], (len(frenet.O) - 1) * frenet.ds)),
                                SX_STEPS)
         # sx_range = np.linspace(goal_frenet_state[FS_SX]  / 2, goal_frenet_state[FS_SX], SX_STEPS)

src/planning/utils/frenet_serret_frame.py
@@ -166,7 +166,7 @@ class FrenetSerret2DFrame:
         v_x = ctrajectories[:, :, C_V]
         a_x = ctrajectories[:, :, C_A]
 
-        s_x, a_r, T_r, N_r, k_r, k_r_tag = self._project_cartesian_points(ctrajectories[:, :, [C_X, C_Y]])
+        s_x, a_r, T_r, N_r, k_r, k_r_tag = self._project_cartesian_points(pos_x)
 
         d_x = np.einsum('tpi,tpi->tp', pos_x - a_r, N_r)
 
@@ -214,6 +214,9 @@ class FrenetSerret2DFrame:
 
         is_curvature_big_enough = np.greater(np.abs(k_s), TINY_CURVATURE)
 
+        # don't enable zero curvature to prevent numerical problems with infinite radius
+        k_s[np.logical_not(is_curvature_big_enough)] = TINY_CURVATURE
+
         # signed circle radius according to the curvature
         signed_radius = np.divide(1, k_s)
 
@@ -227,10 +230,11 @@ class FrenetSerret2DFrame:
         cos = np.abs(np.einsum('...ik,...ik->...i', N_s, center_to_point) / np.linalg.norm(center_to_point, axis=-1))
 
         # prevent illegal (greater than 1) argument for arccos()
-        cos[cos > 1.0] = 1.0
+        # don't enable zero curvature to prevent numerical problems with infinite radius
+        cos[np.logical_or(np.logical_not(is_curvature_big_enough), cos > 1.0)] = 1.0
 
         # arc length from a_s to the new guess point
-        step = step_sign * np.apply_along_axis(np.math.acos, 1, cos[:, np.newaxis]) * np.abs(signed_radius)
+        step = step_sign * np.arccos(cos) * np.abs(signed_radius)
         s_approx[is_curvature_big_enough] += step[is_curvature_big_enough]  # next s_approx of the current point
 
         a_s, T_s, N_s, k_s, k_s_tag = self._taylor_interp(s_approx)