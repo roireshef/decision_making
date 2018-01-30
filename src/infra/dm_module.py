a6610b810715c558781f655c40e800979a7997a1
Gabi <gabi.guetta@gm.com>
2018-01-30 18:08:19 +0200

    Convert DmModule to 2.7

--- src/infra/dm_module.py
+++ src/infra/dm_module.py
@@ -1,3 +1,6 @@
+##################################################
+### This code has to be written in python 2.7! ###
+##################################################
 from abc import abstractmethod, ABCMeta
 from logging import Logger
 
@@ -10,7 +13,8 @@ class DmModule:
     """
     Abstract class which is implemented in functional DM modules and facades.
     """
-    def __init__(self, pubsub: PubSub, logger: Logger) -> None:
+    def __init__(self, pubsub, logger):
+        # type: (PubSub, Logger) -> None
         self.pubsub = pubsub
         """
         :param dds: Inter-process communication interface.
@@ -20,35 +24,41 @@ class DmModule:
         self.logger.info("initializing module: " + self.__class__.__name__)
 
     @abstractmethod
-    def _start_impl(self) -> None:
+    def _start_impl(self):
+        # type: () -> None
         """
         Implementation specific start script.
         """
         pass
 
-    def start(self) -> None:
+    def start(self):
+        # type: () -> None
         self.logger.info("starting module: " + self.__class__.__name__)
         self._start_impl()
 
     @abstractmethod
-    def _stop_impl(self) -> None:
+    def _stop_impl(self):
+        # type: () -> None
         """
         Implementation specific stop script.
         """
         pass
 
-    def stop(self) -> None:
+    def stop(self):
+        # type: () -> None
         self.logger.info("stopping module: " + self.__class__.__name__)
         self._stop_impl()
 
     @abstractmethod
-    def _periodic_action_impl(self) -> None:
+    def _periodic_action_impl(self):
+        # type: () -> None
         """
         Implementation specific script for execution upon event.
         """
         pass
 
-    def periodic_action(self) -> None:
+    def periodic_action(self):
+        # type: () -> None
         """
         Perform triggered action and write logging messages.
         """
