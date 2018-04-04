4bc9cf5058659c528bc73c657eff05cbdcffa72e
RoiReshef <roi.reshef@gm.com>
2018-04-04 11:28:48 +0200

    changed safety distance to 2.5sec

--- src/global_constants.py
+++ src/global_constants.py
@@ -94,7 +94,7 @@ LAT_ACC_LIMITS = np.array([-4.0, 4.0])
 
 # Assumed response delay on road [sec]
 # Used to compute safe distance from other agents on road
-SAFE_DIST_TIME_DELAY = 2.0
+SAFE_DIST_TIME_DELAY = 2.5
 
 # Semantic Grid indices
 SEMANTIC_CELL_LON_FRONT, SEMANTIC_CELL_LON_SAME, SEMANTIC_CELL_LON_REAR = 1, 0, -1
