3b8f5c78a6fad9db2883b5a41797714c176f8134
mz8cj6 <vlad.goldner@gm.com>
2018-04-10 09:54:30 +0300

    cosmetic: rename variables

--- src/planning/performance_metrics/plan_cost_functions.py
+++ src/planning/performance_metrics/plan_cost_functions.py
@@ -71,99 +71,98 @@ class PlanEfficiencyMetric:
         return VelocityProfile(init_vel, acc_time, mid_vel, mid_time, dec_time, target_vel)
 
     @staticmethod
-    def _calc_velocity_profile_follow_car(init_vel: float, acc: float, max_vel: float, tot_dist: float, end_vel: float,
-                                          end_acc: float) -> \
-            [float, float, float, float]:
+    def _calc_velocity_profile_follow_car(v_init: float, a: float, v_max: float, dist: float, v_tar: float,
+                                          a_tar: float) -> [float, float, float, float]:
         """
         Given start & end velocities and distance to the followed car, calculate velocity profile:
-            1. acceleration to a velocity mid_vel <= max_vel for t1 time,
-            2. moving by max_vel for t2 time (t2 = 0 if mid_vel < max_vel),
+            1. acceleration to a velocity v_mid <= v_max for t1 time,
+            2. moving by v_max for t2 time (t2 = 0 if v_mid < v_max),
             3. deceleration to end_vel for t3 time.
         If this profile is infeasible, then try an opposite order of accelerations: 1. deceleration, 3. acceleration.
         In the case of opposite order, the constant velocity segment is missing.
         In each velocity segment the acceleration is constant.
-        :param init_vel: start ego velocity
-        :param acc: absolute ego acceleration in the first and the last profile segments
-        :param max_vel: maximal desired velocity of ego
-        :param tot_dist: initial distance to the safe location from the target
-        :param end_vel: target object velocity
-        :param end_acc: target object acceleration
-        return: t1, mid_vel, t2, t3 (time periods of the 3 profile segments and maximal achieved velocity)
+        :param v_init: start ego velocity
+        :param a: absolute ego acceleration in the first and the last profile segments
+        :param v_max: maximal desired velocity of ego
+        :param dist: initial distance to the safe location from the target
+        :param v_tar: target object velocity
+        :param a_tar: target object acceleration
+        return: t1, v_mid, t2, t3 (time periods of the 3 profile segments and maximal achieved velocity)
         """
-        print('CALC PROFILE: init_vel=%f tot_dist=%f' % (init_vel, tot_dist))
+        print('CALC PROFILE: v_init=%f dist=%f' % (v_init, dist))
 
         ILLEGAL_VEL = -1000  # illegal velocity creates "infinite" cost
-        init_vel_rel = init_vel - end_vel  # relative velocity; may be negative
-        max_vel_rel = max(max_vel - end_vel, max(init_vel_rel, 1.))  # let max_vel > end_vel to enable reaching the car
-        if np.math.isclose(acc, end_acc, rel_tol=1e-03) or np.math.isclose(acc, -end_acc, rel_tol=1e-03):
-            acc -= 0.1  # slightly change the acceleration to prevent instability in calculations
+        v_init_rel = v_init - v_tar  # relative velocity; may be negative
+        v_max_rel = max(v_max - v_tar, max(v_init_rel, 1.))  # let max_vel > end_vel to enable reaching the car
+        if np.math.isclose(a, a_tar, rel_tol=1e-02) or np.math.isclose(a, -a_tar, rel_tol=1e-02):
+            a -= 0.1  # slightly change the acceleration to prevent instability in calculations
 
-        # here we use formula (mv^2 - v^2)/2(a-a1) + mv^2/2(a+a1) = tot_dist
-        mid_vel_rel_sqr = (init_vel_rel * init_vel_rel / 2 + tot_dist * (acc - end_acc)) * (acc + end_acc) / acc
+        # here we use formula (vm^2 - v^2)/2(a-a_tar) + vm^2/2(a+a_tar) = dist
+        v_mid_rel_sqr = (v_init_rel * v_init_rel / 2 + dist * (a - a_tar)) * (a + a_tar) / a
         try_opposite_order = False
         t1 = t3 = 0
-        mid_vel_rel = init_vel_rel
-        if mid_vel_rel_sqr >= 0:
-            mid_vel_rel = np.sqrt(mid_vel_rel_sqr)
-            t1 = (mid_vel_rel - init_vel_rel) / (acc - end_acc)  # acceleration time
-            t3 = mid_vel_rel / (acc + end_acc)                   # deceleration time
+        v_mid_rel = v_init_rel
+        if v_mid_rel_sqr >= 0:
+            v_mid_rel = np.sqrt(v_mid_rel_sqr)
+            t1 = (v_mid_rel - v_init_rel) / (a - a_tar)  # acceleration time
+            t3 = v_mid_rel / (a + a_tar)                 # deceleration time
             if t1 < 0 or t3 < 0:  # illegal time, try the opposite order of accelerations
                 try_opposite_order = True
         else:  # the target is unreachable, try the opposite order of accelerations
             try_opposite_order = True
 
         # try_opposite_order: first deceleration, then acceleration
-        # here we use formula (v^2 - mv^2)/2(a+a1) - mv^2/2(a-a1) = tot_dist
+        # here we use formula (v^2 - vm^2)/2(a+a_tar) - vm^2/2(a-a_tar) = dist
         if try_opposite_order:  # first deceleration, then acceleration
-            mid_vel_rel_sqr = (init_vel_rel * init_vel_rel / 2 - tot_dist * (acc + end_acc)) * (acc - end_acc) / acc
-            if mid_vel_rel_sqr < 0:  # the target is unreachable
-                print('BAD: negative mid_vel_rel_sqr=%f tot_dist=%f' % (mid_vel_rel_sqr, tot_dist))
+            v_mid_rel_sqr = (v_init_rel * v_init_rel / 2 - dist * (a + a_tar)) * (a - a_tar) / a
+            if v_mid_rel_sqr < 0:  # the target is unreachable
+                print('BAD: negative v_mid_rel_sqr=%f tot_dist=%f' % (v_mid_rel_sqr, dist))
                 return 0, ILLEGAL_VEL, 1, 0  # illegal action
-            mid_vel_rel = -np.sqrt(mid_vel_rel_sqr)
-            t1 = (init_vel_rel - mid_vel_rel) / (acc - end_acc)   # deceleration time
-            t3 = -mid_vel_rel / (acc + end_acc)                   # acceleration time
+            v_mid_rel = -np.sqrt(v_mid_rel_sqr)
+            t1 = (v_init_rel - v_mid_rel) / (a - a_tar)   # deceleration time
+            t3 = -v_mid_rel / (a + a_tar)                 # acceleration time
             if t1 < 0 or t3 < 0:
-                print('BAD: negative t1 or t3 or mid_vel: mid_vel_rel=%f t1=%f t3=%f' % (mid_vel_rel, t1, t3))
+                print('BAD: negative t1 or t3 or v_mid: v_mid_rel=%f t1=%f t3=%f' % (v_mid_rel, t1, t3))
                 return 0, ILLEGAL_VEL, 1, 0  # illegal action
 
-        if init_vel + t1 * acc <= max_vel or tot_dist < 0:  # ego does not reach max_vel, then t2 = 0
-            if t3 < 0 or mid_vel_rel + end_vel < 0:  # negative t3 or negative mid_vel
-                print('BAD: negative t3 or mid_vel: t3=%f mid_vel_rel=%f' % (t3, mid_vel_rel))
+        if v_init + t1 * a <= v_max or dist < 0:  # ego does not reach max_vel, then t2 = 0
+            if t3 < 0 or v_mid_rel + v_tar < 0:  # negative t3 or negative v_mid
+                print('BAD: negative t3 or v_mid: t3=%f v_mid_rel=%f' % (t3, v_mid_rel))
                 return 0, ILLEGAL_VEL, 1, 0  # illegal action
-            return t1, mid_vel_rel + end_vel, 0, t3
+            return t1, v_mid_rel + v_tar, 0, t3
 
         # from now: ego reaches max_vel, such that t2 > 0
 
-        t1 = (max_vel - init_vel) / acc  # acceleration time
-        if end_acc == 0:  # a simple case: the followed car has constant velocity
-            t3 = max_vel_rel / acc  # deceleration time
-            mid_dist = tot_dist - (2 * max_vel_rel * max_vel_rel - init_vel_rel * init_vel_rel) / (2 * acc)
-            t2 = max(0., mid_dist / max_vel_rel)  # constant velocity (max_vel) time
-            return t1, max_vel, t2, t3
+        t1 = (v_max - v_init) / a  # acceleration time
+        if a_tar == 0:  # a simple case: the followed car has constant velocity
+            t3 = v_max_rel / a  # deceleration time
+            dist_mid = dist - (2 * v_max_rel * v_max_rel - v_init_rel * v_init_rel) / (2 * a)
+            t2 = max(0., dist_mid / v_max_rel)  # constant velocity (max_vel) time
+            return t1, v_max, t2, t3
 
         # from now the most general case: t2 > 0 and the followed car has non-zero acceleration
 
         # Notations:
         #   v is initial relative velocity
-        #   a > 0 is ego acceleration, a1 target object acceleration
+        #   a > 0 is ego acceleration, a_tar target object acceleration
         #   vm1 is relative velocity of ego immediately after acceleration
-        #   vm2 is relative velocity of ego immediately before the deceleration, vm2 = vm1 - a1*t2
+        #   vm2 is relative velocity of ego immediately before the deceleration, i.e. vm2 = vm1 - a_tar*t2
         # Quadratic equation: tot_dist is the sum of 3 distances:
         #   acceleration distance     max_vel distance     deceleration distance
-        #   (vm1^2 - v^2)/2(a-a1)  +  (vm1+vm2)/2 * t2  +  vm2^2/2(a+a1) = tot_dist
-        (v, a, a1) = (init_vel_rel, acc, end_acc)
-        vm1 = v + (a - a1) * t1
+        #   (vm1^2 - v^2)/2(a-a_tar)  +  (vm1+vm2)/2 * t2  +  vm2^2/2(a+a_tar)   =   dist
+        v = v_init_rel
+        vm1 = v + (a - a_tar) * t1
         # after substitution of vm2 = vm1 - a1*t and simplification, solve quadratic equation on t2:
-        # a*a1*t^2 - 2*vm1*a*t2 - (vm1^2 + 2*(a+a1)*((vm1^2 - v^2) / 2*(a-a1) - tot_dist)) = 0
-        c = vm1*vm1 + 2*(a+a1) * ((vm1*vm1 - v*v)/(2*(a-a1)) - tot_dist)  # free coefficient
-        discriminant = vm1*vm1*a*a + a*a1*c  # discriminant of the quadratic equation
+        # a*a_tar * t^2 - 2*vm1*a * t2 - (vm1^2 + 2(a+a_tar) * ((vm1^2 - v^2)/2(a-a_tar) - dist)) = 0
+        c = vm1*vm1 + 2*(a + a_tar) * ((vm1 * vm1 - v * v) / (2 * (a - a_tar)) - dist)  # free coefficient
+        discriminant = vm1*vm1 * a * a + a * a_tar * c  # discriminant of the quadratic equation
         if discriminant < 0:
             print('BAD: general case: det < 0')
             return 0, ILLEGAL_VEL, 1, 0  # illegal action
-        t2 = (vm1*a + np.sqrt(discriminant)) / (a*a1)
-        vm2 = vm1 - a1*t2
-        t3 = vm2 / (a + a1)
-        return t1, max_vel, t2, t3
+        t2 = (vm1 * a + np.sqrt(discriminant)) / (a * a_tar)
+        vm2 = vm1 - a_tar*t2
+        t3 = vm2 / (a + a_tar)
+        return t1, v_max, t2, t3
 
 
 class PlanComfortMetric:
