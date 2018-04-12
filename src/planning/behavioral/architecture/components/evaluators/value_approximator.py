0d553dc28ccae56abe2dfaf38b83d3a805cb341f
RoiReshef <roi.reshef@gm.com>
2018-04-12 11:58:32 +0300

    removed action_validator, backward compatibility of rule_based_evaluator, splitted evaluators into 2 separated classes

--- src/planning/behavioral/architecture/components/evaluators/value_approximator.py
+++ src/planning/behavioral/architecture/components/evaluators/value_approximator.py
@@ -7,6 +7,4 @@ class ValueApproximator:
         self.logger = logger
 
     def evaluate_state(self, behavioral_state: SemanticActionsGridState) -> float:
-        state_val = 0.0
-        self.logger.debug("state %s value is: %d", str(behavioral_state), state_val)
-        return state_val
+        pass
