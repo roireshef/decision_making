ae77a22f0ff7e5311f0b412bf3267eebcb9d62e3
mz8cj6 <vlad.goldner@gm.com>
2018-05-09 12:16:51 +0300

    chose actions only from valid (unmasked-out) actions

--- src/planning/behavioral/planner/single_step_behavioral_planner.py
+++ src/planning/behavioral/planner/single_step_behavioral_planner.py
@@ -59,10 +59,10 @@ class SingleStepBehavioralPlanner(CostBasedBehavioralPlanner):
         action_specs_mask = self.action_spec_validator.filter_action_specs(action_specs, behavioral_state)
 
         # State-Action Evaluation
-        action_costs = self.action_spec_evaluator.evaluate(behavioral_state, action_recipes, action_specs,
-                                                           action_specs_mask)
+        action_costs = self.recipe_evaluator.evaluate(behavioral_state, action_recipes, action_specs_mask)
 
-        selected_action_index = int(np.argmin(action_costs))
+        valid_idx = np.where(action_specs_mask)[0]
+        selected_action_index = valid_idx[action_costs[valid_idx].argmin()]
         self.logger.debug('Selected recipe: ', action_recipes[selected_action_index].__dict__)
         selected_action_spec = action_specs[selected_action_index]
 
