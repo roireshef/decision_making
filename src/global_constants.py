1. don't enable profiles longer than 20 sec (None instead of cut_profile) 2. fix bug in safety: t + td may overflow to the next segment 3. fast safety check for profile 4. single_step_planner: move recipe_evaluator.evaluate to run before specify 5. change cost weights: right_lane & lane_deviation 6. add test for specification

src/global_constants.py
@@ -3,6 +3,7 @@ import numpy as np
 from decision_making.src.messages.str_serializable import StrSerializable
 
 # General constants
+from decision_making.src.planning.types import LIMIT_MAX
 
 UNKNOWN_DEFAULT_VAL = 0.0
 EPS = np.finfo(np.float32).eps
@@ -69,7 +70,7 @@ LON_JERK_COST_WEIGHT = 1.0                         # cost of longitudinal jerk
 LAT_JERK_COST_WEIGHT = 1.0                         # cost of lateral jerk
 
 EFFICIENCY_COST_WEIGHT = 0.6                # cost of zero velocity on one trajectory point
-EFFICIENCY_COST_DERIV_ZERO_DESIRED_RATIO = 3  # ratio UNDER_C'(0) / UNDER_C'(v_desired)
+EFFICIENCY_COST_DERIV_ZERO_DESIRED_RATIO = 2  # ratio UNDER_C'(0) / UNDER_C'(v_desired)
 RIGHT_LANE_COST_WEIGHT = 0.005              # cost of using non-right lane on one trajectory point
 
 # [m/sec] speed to plan towards by default in BP
@@ -101,7 +102,7 @@ LAT_ACC_LIMITS = np.array([-4.0, 4.0])
 SAFE_DIST_TIME_DELAY = 2.0
 
 # [m/sec] Minimal difference of velocities to justify an overtake
-MIN_OVERTAKE_VEL = 3.5
+MIN_OVERTAKE_VEL = 3
 
 # [m] The margin that we take from the front/read of the vehicle to define the front/rear partitions
 LON_MARGIN_FROM_EGO = 1
@@ -115,17 +116,24 @@ LON_ACC_TO_JERK_FACTOR = 0.2
 LAT_ACC_TO_JERK_FACTOR = 0.2
 
 # [sec] performance metrics for BP minimal plan time
-BP_METRICS_TIME_HORIZON = 20
+BP_METRICS_TIME_HORIZON = BP_ACTION_T_LIMITS[LIMIT_MAX]
+
+BP_EFFICIENCY_COST_WEIGHT = 6.
 
 # performance metrics for BP: lane deviation cost weight
-BP_METRICS_LANE_DEVIATION_COST_WEIGHT = 0.15
+BP_METRICS_LANE_DEVIATION_COST_WEIGHT = 1.0
+
+# performance metrics for BP: non-right lane cost weight
+BP_RIGHT_LANE_COST_WEIGHT = 0.15              # cost of using non-right lane on one trajectory point
 
 # performance metrics for BP: missing goal cost
-BP_MISSING_GOAL_COST = 100
+BP_MISSING_GOAL_COST = 100.
 
 # [m/sec] when target velocity of vel_profile is greater than the desired velocity,
 # ego moves a bit faster (by this delta) than the target in order to reach it.
 BP_MAX_VELOCITY_TOLERANCE = 1.
+
+
 
 # Trajectory Planner #
 

src/planning/behavioral/evaluators/cost_functions.py
@@ -2,7 +2,8 @@ import numpy as np
 from decision_making.src.planning.behavioral.data_objects import AggressivenessLevel
 
 from decision_making.src.global_constants import WERLING_TIME_RESOLUTION, LON_ACC_TO_JERK_FACTOR, \
-    LAT_ACC_TO_JERK_FACTOR, RIGHT_LANE_COST_WEIGHT, EFFICIENCY_COST_WEIGHT, LAT_JERK_COST_WEIGHT, LON_JERK_COST_WEIGHT, \
+    LAT_ACC_TO_JERK_FACTOR, BP_RIGHT_LANE_COST_WEIGHT, BP_EFFICIENCY_COST_WEIGHT, \
+    LAT_JERK_COST_WEIGHT, LON_JERK_COST_WEIGHT, \
     BEHAVIORAL_PLANNING_DEFAULT_DESIRED_SPEED, BP_METRICS_LANE_DEVIATION_COST_WEIGHT, \
     EFFICIENCY_COST_DERIV_ZERO_DESIRED_RATIO, AGGRESSIVENESS_TO_LAT_ACC
 from decision_making.src.planning.behavioral.evaluators.velocity_profile import VelocityProfile
@@ -33,7 +34,7 @@ class PlanEfficiencyMetric:
         #        vel_profile.v_mid, target_vel))
 
         efficiency_cost = PlanEfficiencyMetric.calc_pointwise_cost_for_velocities(np.array([avg_vel]))[0]
-        return EFFICIENCY_COST_WEIGHT * efficiency_cost * profile_time / WERLING_TIME_RESOLUTION
+        return BP_EFFICIENCY_COST_WEIGHT * efficiency_cost * profile_time
 
     @staticmethod
     def calc_pointwise_cost_for_velocities(vel: np.array) -> np.array:
@@ -100,10 +101,10 @@ class PlanComfortMetric:
 class PlanRightLaneMetric:
     @staticmethod
     def calc_cost(time_period: float, lane_idx: int) -> float:
-        return RIGHT_LANE_COST_WEIGHT * lane_idx * time_period / WERLING_TIME_RESOLUTION
+        return BP_RIGHT_LANE_COST_WEIGHT * lane_idx * time_period
 
 
 class PlanLaneDeviationMetric:
     @staticmethod
     def calc_cost(lat_dev: float) -> float:
-        return BP_METRICS_LANE_DEVIATION_COST_WEIGHT * lat_dev*lat_dev / WERLING_TIME_RESOLUTION
+        return BP_METRICS_LANE_DEVIATION_COST_WEIGHT * lat_dev * lat_dev

src/planning/behavioral/evaluators/heuristic_state_action_recipe_evaluator.py
@@ -2,9 +2,10 @@ from logging import Logger
 from typing import List, Optional
 
 import numpy as np
+import sys
 
 from decision_making.src.global_constants import BP_METRICS_TIME_HORIZON, \
-    EFFICIENCY_COST_WEIGHT, WERLING_TIME_RESOLUTION, RIGHT_LANE_COST_WEIGHT, \
+    BP_EFFICIENCY_COST_WEIGHT, WERLING_TIME_RESOLUTION, BP_RIGHT_LANE_COST_WEIGHT, \
     BP_METRICS_LANE_DEVIATION_COST_WEIGHT, BP_MISSING_GOAL_COST, BP_MAX_VELOCITY_TOLERANCE, SAFE_DIST_TIME_DELAY, \
     BEHAVIORAL_PLANNING_DEFAULT_DESIRED_SPEED
 from decision_making.src.planning.behavioral.behavioral_grid_state import BehavioralGridState, \
@@ -50,8 +51,8 @@ class HeuristicStateActionRecipeEvaluator(ActionRecipeEvaluator):
         # get front dynamic objects' velocities from the occupancy grid
         front_objects = self._get_front_objects(behavioral_state)
 
-        print('time=%.1f ego_v=%.2f ego_lat=%.2f ego_dv=%.2f' % (ego.timestamp_in_sec, ego.v_x,
-                                                                 ego.road_localization.intra_road_lat, ego_lat_vel))
+        print('time=%.1f ego_v=%.2f ego_lat=%.2f ego_dv=%.2f grid_size=%d' % (ego.timestamp_in_sec, ego.v_x,
+                ego.road_localization.intra_road_lat, ego_lat_vel, len(behavioral_state.road_occupancy_grid)))
 
         action_costs = np.full(len(action_recipes), np.inf)
 
@@ -67,7 +68,7 @@ class HeuristicStateActionRecipeEvaluator(ActionRecipeEvaluator):
 
             # create velocity profile, whose length is at least as the lateral time
             vel_profile = self._calc_velocity_profile(behavioral_state, action_recipe, T_d, front_objects)
-            # print('version is: %s' % sys.version)
+            #print('version is: %s' % sys.version)
             if vel_profile is None:  # infeasible action
                 continue
 
@@ -127,8 +128,7 @@ class HeuristicStateActionRecipeEvaluator(ActionRecipeEvaluator):
         target_acc = cars_size_margin = obj_lon = None
 
         if action_recipe.action_type == ActionType.FOLLOW_VEHICLE or action_recipe.action_type == ActionType.OVER_TAKE_VEHICLE:
-            target_obj = \
-            behavioral_state.road_occupancy_grid[(action_recipe.relative_lane, action_recipe.relative_lon)][0].dynamic_object
+            target_obj = behavioral_state.road_occupancy_grid[(action_recipe.relative_lane, action_recipe.relative_lon)][0].dynamic_object
             target_vel = target_obj.v_x
             target_acc = target_obj.acceleration_lon
             obj_lon = target_obj.road_localization.road_lon
@@ -152,8 +152,9 @@ class HeuristicStateActionRecipeEvaluator(ActionRecipeEvaluator):
             action_recipe.action_type, ego_fpoint[FP_SX], ego.v_x, obj_lon, target_vel, target_acc,
             action_recipe.aggressiveness, cars_size_margin, min_time=T_d)
 
-        if vel_profile is not None:
-            vel_profile = vel_profile.cut_by_time(BP_METRICS_TIME_HORIZON)
+        #if vel_profile is not None and vel_profile.total_time() > BP_METRICS_TIME_HORIZON:
+        #    return None
+        #vel_profile = vel_profile.cut_by_time(BP_METRICS_TIME_HORIZON)
         return vel_profile
 
     def _calc_largest_safe_time(self, behavioral_state: BehavioralGridState, action: ActionRecipe,
@@ -277,12 +278,12 @@ class HeuristicStateActionRecipeEvaluator(ActionRecipeEvaluator):
         efficiency_cost = right_lane_cost = future_lane_deviation_cost = future_comfort_cost = goal_cost = 0
         if time_period > 0:
             efficiency_cost = PlanEfficiencyMetric.calc_pointwise_cost_for_velocities(np.array([v_tar]))[0] * \
-                              EFFICIENCY_COST_WEIGHT * time_period / WERLING_TIME_RESOLUTION
-            right_lane_cost = ego_lane * time_period * RIGHT_LANE_COST_WEIGHT / WERLING_TIME_RESOLUTION
+                              BP_EFFICIENCY_COST_WEIGHT * time_period
+            right_lane_cost = ego_lane * time_period * BP_RIGHT_LANE_COST_WEIGHT
 
         if goal is None or ego_road_id != goal.road_id:  # no relevant goal
             if ego_lane > 0:  # distance in lanes from the rightest lane
-                future_lane_deviation_cost = ego_lane * BP_METRICS_LANE_DEVIATION_COST_WEIGHT / WERLING_TIME_RESOLUTION
+                future_lane_deviation_cost = ego_lane * BP_METRICS_LANE_DEVIATION_COST_WEIGHT
                 future_comfort_cost = ego_lane * PlanComfortMetric.calc_cost(empty_vel_profile, T_d_full, T_d_full,
                                                                              AggressivenessLevel.CALM)
         elif ego_lane > goal.to_lane or ego_lane < goal.from_lane:  # outside of the lanes range of the goal

src/planning/behavioral/evaluators/velocity_profile.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 
 from decision_making.src.global_constants import AGGRESSIVENESS_TO_LON_ACC, BEHAVIORAL_PLANNING_DEFAULT_DESIRED_SPEED, \
     SAFE_DIST_TIME_DELAY, LON_ACC_LIMITS, AGGRESSIVENESS_TO_LAT_ACC, BP_MAX_VELOCITY_TOLERANCE
@@ -74,10 +75,42 @@ class VelocityProfile:
             return d1 + d2 + t3 * (self.v_mid + 0.5 * a3 * t3), self.v_mid + a3 * t3
         return d1 + d2 + 0.5 * (self.v_mid + self.v_tar) * self.t3, self.v_tar
 
+    def when_decel_velocity_is_equal_to(self, v: float) -> float:
+        """
+        Search only during deceleration
+        :param v: velocity to search
+        :return: time during deceleration when the velocity is equal to v
+        """
+        if v > self.v_mid or v < self.v_tar:
+            return None
+        if v == self.v_mid:
+            return self.t1 + self.t2
+        t = None
+        if v >= self.v_tar:  # v_tar <= v < v_mid, then self.t3 > 0
+            a3 = (self.v_tar - self.v_mid) / self.t3
+            t = (v - self.v_mid) / a3
+        return self.t1 + self.t2 + t
+
+    def when_accel_velocity_is_equal_to(self, v: float) -> float:
+        """
+        Search only during acceleration
+        :param v: velocity to search
+        :return: time during acceleration when the velocity is equal to v
+        """
+        if v > self.v_mid or v < self.v_init:
+            return None
+        if v == self.v_mid:
+            return self.t1
+        t = None
+        if v >= self.v_init:  # v_init <= v < v_mid, then self.t1 > 0
+            a1 = (self.v_mid - self.v_init) / self.t1
+            t = (v - self.v_init) / a1
+        return t
+
     def cut_by_time(self, max_time: float):
         tot_time = self.total_time()
         if tot_time <= max_time:
-            return self
+            return copy.copy(self)
         if self.t1 + self.t2 <= max_time:
             a = (self.v_tar - self.v_mid) / self.t3
             t3 = max_time - self.t1 - self.t2
@@ -393,6 +426,60 @@ class VelocityProfile:
 class ProfileSafety:
 
     @staticmethod
+    def is_safe(v_front: float, v_back: float, dist: float, time_delay: float, margin: float):
+        max_brake = -LON_ACC_LIMITS[0]
+        return (v_back**2 - v_front**2) / (2*max_brake) + v_back*time_delay + margin < dist
+
+    @staticmethod
+    def check_safety(ego_lon: float, ego_half_size: float, vel_profile: VelocityProfile,
+                            dyn_obj: DynamicObject, T_max: float, time_delay: float) -> bool:
+        """
+        Given ego velocity profile and dynamic object, calculate the last time, when the safety complies.
+        Here we assume the object has a constant velocity.
+        :param ego_lon: ego initial longitude
+        :param ego_half_size: [m] half length of ego
+        :param vel_profile: ego velocity profile
+        :param dyn_obj: the dynamic object, for which the safety is tested
+        :param T_max: maximal time to check the safety (usually T_d for safety w.r.t. F and LB)
+        :param time_delay: reaction time of the back car
+        :return: the latest safe time
+        """
+        if T_max <= 0:
+            return True
+        (init_s_ego, init_s_obj, v_obj) = (ego_lon, dyn_obj.road_localization.road_lon, dyn_obj.v_x)
+        margin = ego_half_size + dyn_obj.size.length / 2
+        cut_profile = vel_profile.cut_by_time(T_max)
+
+        if init_s_ego < init_s_obj:  # the object is in front of ego
+            t = cut_profile.when_decel_velocity_is_equal_to(v_obj)
+            if t is None:  # cut_profile does not contain velocity v_obj
+                if cut_profile.v_init < v_obj:  # ego is always slower
+                    return ProfileSafety.is_safe(v_obj, cut_profile.v_init, init_s_obj - init_s_ego, time_delay, margin)
+                else:  # ego is always faster, check only at the end
+                    tot_time = cut_profile.total_time()
+                    tot_dist = cut_profile.total_dist()
+                    return ProfileSafety.is_safe(v_obj, cut_profile.v_tar,
+                                                 (init_s_obj + v_obj*tot_time) - (init_s_ego + tot_dist),
+                                                 time_delay, margin)
+            else:  # t was found
+                s, _ = cut_profile.sample_at(t)  # v_ego(t2) = v_obj
+                return init_s_obj + v_obj*t - (init_s_ego + s) > v_obj * time_delay + margin
+        else:  # the object is behind ego
+            t = cut_profile.when_accel_velocity_is_equal_to(v_obj)
+            if t is None:  # cut_profile does not contain velocity v_obj
+                if cut_profile.v_init > v_obj:  # ego is always faster
+                    return ProfileSafety.is_safe(cut_profile.v_init, v_obj, init_s_ego - init_s_obj, time_delay, margin)
+                else:  # ego is always slower, check only at the end
+                    tot_time = cut_profile.total_time()
+                    tot_dist = cut_profile.total_dist()
+                    return ProfileSafety.is_safe(cut_profile.v_tar, v_obj,
+                                                 (init_s_ego + tot_dist) - (init_s_obj + v_obj*tot_time),
+                                                 time_delay, margin)
+            else:  # t was found
+                s, _ = cut_profile.sample_at(t)  # v_ego(t2) = v_obj
+                return (init_s_ego + s) - (init_s_obj + v_obj*t) > v_obj * time_delay + margin
+
+    @staticmethod
     def calc_last_safe_time(ego_lon: float, ego_half_size: float, vel_profile: VelocityProfile,
                             dyn_obj: DynamicObject, T_max: float, time_delay: float) -> float:
         """
@@ -405,12 +492,26 @@ class ProfileSafety:
         :param time_delay: reaction time of the back car
         :return: the latest safe time
         """
-        # check safety until completing the lane change
         if T_max <= 0:
             return np.inf
+
+        margin = ego_half_size + dyn_obj.size.length / 2
         # initialization of motion parameters
         (init_s_ego, init_v_obj, a_obj) = (ego_lon, dyn_obj.v_x, dyn_obj.acceleration_lon)
         init_s_obj = dyn_obj.road_localization.road_lon
+
+        if a_obj == 0:  # the object has constant velocity, then use fast safety test
+            dist = abs(init_v_obj - init_s_ego)
+            if init_s_ego < init_s_obj:
+                v_front = init_v_obj
+                v_back = vel_profile.v_init
+            else:
+                v_back = init_v_obj
+                v_front = vel_profile.v_init
+            if not ProfileSafety.is_safe(v_front, v_back, dist, time_delay, margin):
+                return -1
+            if ProfileSafety.check_safety(ego_lon, ego_half_size, vel_profile, dyn_obj, T_max, time_delay):
+                return np.inf
 
         t, t_cum, s_ego, v_ego, a_ego = vel_profile.get_details(T_max)
         s_ego += init_s_ego
@@ -425,13 +526,31 @@ class ProfileSafety:
 
         # calculate last_safe_time
         last_safe_time = 0
+        a_max = -LON_ACC_LIMITS[LIMIT_MIN]
+
         for seg in range(t.shape[0]):
             if t[seg] == 0:
                 continue
             last_safe_time += ProfileSafety._calc_largest_time_for_segment(
                 s[seg, front], v[seg, front], a[seg, front], s[seg, back], v[seg, back], a[seg, back], t[seg],
-                ego_half_size + dyn_obj.size.length / 2, time_delay)
+                margin, time_delay)
             if last_safe_time < t_cum[seg + 1]:  # becomes unsafe inside this segment
+                # check if delayed last_safe_time (t+td) overflowed to the next segment
+                T = last_safe_time + time_delay
+                if T > t_cum[seg + 1]:  # then check safety on delayed point of vel_segment
+                    if init_s_ego < init_s_obj:
+                        s_back, v_back = vel_profile.sample_at(T)
+                        s_back += init_s_ego
+                        s_front = init_s_obj + init_v_obj * T + 0.5 * a_obj * T * T
+                        v_front = init_v_obj + a_obj * T
+                    else:
+                        s_front, v_front = vel_profile.sample_at(T)
+                        s_front += init_s_ego
+                        s_back = init_s_obj + init_v_obj * T + 0.5 * a_obj * T * T
+                        v_back = init_v_obj + a_obj * T
+                    if (v_back**2 - v_front**2)/(2*a_max) + margin <= s_front - s_back:
+                        last_safe_time = t_cum[seg + 1]
+                        continue  # this segment is safe
                 return last_safe_time
         return np.inf  # always safe
 

src/planning/behavioral/planner/single_step_behavioral_planner.py
@@ -54,6 +54,9 @@ class SingleStepBehavioralPlanner(CostBasedBehavioralPlanner):
         self.logger.debug('Number of actions originally: %d, valid: %d, filter processing time: %f',
                           self.action_space.action_space_size, np.sum(recipes_mask), time.time()-time_before_filters)
 
+        # State-Action Evaluation
+        action_costs = self.recipe_evaluator.evaluate(behavioral_state, action_recipes, recipes_mask)
+
         # TODO: FOR DEBUG PURPOSES!
         time_before_specify = time.time()
 
@@ -69,7 +72,7 @@ class SingleStepBehavioralPlanner(CostBasedBehavioralPlanner):
         action_specs_mask = self.action_spec_validator.filter_action_specs(action_specs, behavioral_state)
 
         # State-Action Evaluation
-        action_costs = self.recipe_evaluator.evaluate(behavioral_state, action_recipes, action_specs_mask)
+        #action_costs = self.action_spec_evaluator.evaluate(behavioral_state, action_recipes, action_specs_mask)
 
         valid_idx = np.where(action_specs_mask)[0]
         selected_action_index = valid_idx[action_costs[valid_idx].argmin()]

test/planning/behavioral/evaluators/test_bp_metrics.py
@@ -4,20 +4,15 @@ from unittest.mock import patch
 
 import numpy as np
 import pytest
-from decision_making.src.planning.behavioral.architecture.components.action_space.action_space import \
-    ActionSpaceContainer
-from decision_making.src.planning.behavioral.architecture.components.action_space.dynamic_action_space import \
-    DynamicActionSpace
-from decision_making.src.planning.behavioral.architecture.components.action_space.static_action_space import \
-    StaticActionSpace
-from decision_making.src.planning.behavioral.architecture.data_objects import ActionType, AggressivenessLevel, \
+from decision_making.src.planning.behavioral.action_space.action_space import ActionSpaceContainer
+from decision_making.src.planning.behavioral.action_space.dynamic_action_space import DynamicActionSpace
+from decision_making.src.planning.behavioral.action_space.static_action_space import StaticActionSpace
+from decision_making.src.planning.behavioral.data_objects import ActionType, AggressivenessLevel, \
     ActionRecipe, RelativeLane, DynamicActionRecipe, RelativeLongitudinalPosition, StaticActionRecipe
-from decision_making.src.planning.behavioral.architecture.semantic_behavioral_grid_state import \
-    SemanticBehavioralGridState
+from decision_making.src.planning.behavioral.behavioral_grid_state import BehavioralGridState
 
 from decision_making.src.global_constants import BEHAVIORAL_PLANNING_DEFAULT_DESIRED_SPEED, \
-    BEHAVIORAL_PLANNING_NAME_FOR_LOGGING, SEMANTIC_CELL_LAT_SAME, SEMANTIC_CELL_LON_FRONT, SEMANTIC_CELL_LAT_LEFT, \
-    BP_MAX_VELOCITY_TOLERANCE
+    BEHAVIORAL_PLANNING_NAME_FOR_LOGGING, BP_MAX_VELOCITY_TOLERANCE
 from decision_making.src.planning.behavioral.evaluators.heuristic_state_action_recipe_evaluator import \
     HeuristicStateActionRecipeEvaluator
 from decision_making.src.prediction.road_following_predictor import RoadFollowingPredictor
@@ -43,7 +38,6 @@ def calc_collision_time(v_init: float, v_max: float, acc: float, v_tar: float, d
             return acceleration_time
     else:  # v_init_rel > v_max_rel
         return dist / v_init_rel
-
 
 
 def test_behavioralScenarios_moveToLeft_differentDistFrom_F_and_initVel():
@@ -97,12 +91,12 @@ def test_behavioralScenarios_moveToLeft_differentDistFrom_F_and_initVel():
                                   (F_vel, dist_from_F_in_sec, LB_vel, dist_from_LB_in_sec, ego_lon-LB_lon))
 
                             state = State(None, objects, ego)
-                            behavioral_state = SemanticBehavioralGridState.create_from_state(state, logger)
+                            behavioral_state = BehavioralGridState.create_from_state(state, logger)
 
                             actions = []
                             # create actions based on the occupancy grid
                             # add dynamic and static actions going to the same lane
-                            F_exists = (SEMANTIC_CELL_LAT_SAME, SEMANTIC_CELL_LON_FRONT) in behavioral_state.road_occupancy_grid
+                            F_exists = (RelativeLane.SAME_LANE, RelativeLongitudinalPosition.FRONT) in behavioral_state.road_occupancy_grid
                             if F_exists:
                                 right_action = DynamicActionRecipe(RelativeLane.SAME_LANE, RelativeLongitudinalPosition.FRONT,
                                                                    ActionType.FOLLOW_VEHICLE, AggressivenessLevel.CALM)
@@ -114,13 +108,13 @@ def test_behavioralScenarios_moveToLeft_differentDistFrom_F_and_initVel():
                                 actions.append(StaticActionRecipe(RelativeLane.SAME_LANE, des_vel, AggressivenessLevel.CALM))
 
                             # add dynamic and static actions to the left lane
-                            if (SEMANTIC_CELL_LAT_LEFT, SEMANTIC_CELL_LON_FRONT) in behavioral_state.road_occupancy_grid:
+                            if (RelativeLane.LEFT_LANE, RelativeLongitudinalPosition.FRONT) in behavioral_state.road_occupancy_grid:
                                 left_action = DynamicActionRecipe(RelativeLane.LEFT_LANE, RelativeLongitudinalPosition.FRONT,
                                                                   ActionType.FOLLOW_VEHICLE, AggressivenessLevel.CALM)
                                 actions.append(left_action)
                             actions.append(StaticActionRecipe(RelativeLane.LEFT_LANE, des_vel, AggressivenessLevel.CALM))
 
-                            costs = evaluator.evaluate_recipes(behavioral_state, actions, [True] * len(actions), None)
+                            costs = evaluator.evaluate(behavioral_state, actions, [True] * len(actions))
 
                             best_idx = np.argmin(costs)
                             # if F is far or too close
@@ -175,8 +169,8 @@ def test_behavioralScenarios_moveToLeft_aggressivenessBySafety():
             actions = [right_action, left_action1, left_action2, left_action3]
 
             state = State(None, objects, ego)
-            behavioral_state = SemanticBehavioralGridState.create_from_state(state, logger)
-            costs = evaluator.evaluate_recipes(behavioral_state, actions, [True]*len(actions), None)
+            behavioral_state = BehavioralGridState.create_from_state(state, logger)
+            costs = evaluator.evaluate(behavioral_state, actions, [True]*len(actions))
             best_idx = np.argmin(costs)
 
             if dist_from_F_in_sec > 3:  # F is far
@@ -217,8 +211,8 @@ def test_behavioralScenarios_moveToToRight_differentDistFromRF():
 
     lane_width = MapService.get_instance().get_road(ego.road_localization.road_id).lane_width
     state = State(None, [], ego)
-    behavioral_state = SemanticBehavioralGridState.create_from_state(state, logger)
-    costs = evaluator.evaluate_recipes(behavioral_state, actions, [True]*len(actions), None)
+    behavioral_state = BehavioralGridState.create_from_state(state, logger)
+    costs = evaluator.evaluate(behavioral_state, actions, [True]*len(actions))
     assert costs[0] < costs[1]  # right is better
 
     for dist_from_RF_in_sec in [7, 4]:
@@ -231,9 +225,9 @@ def test_behavioralScenarios_moveToToRight_differentDistFromRF():
         RF = DynamicObject(1, 0, RF_cpoint[0], RF_cpoint[1], RF_cpoint[2], RF_yaw, size, 0, RF_vel, 0, 0, 0)
 
         state = State(None, [RF], ego)
-        behavioral_state = SemanticBehavioralGridState.create_from_state(state, logger)
+        behavioral_state = BehavioralGridState.create_from_state(state, logger)
 
-        costs = evaluator.evaluate_recipes(behavioral_state, actions, [True]*len(actions), None)
+        costs = evaluator.evaluate(behavioral_state, actions, [True]*len(actions))
 
         best_idx = np.argmin(costs)
         if dist_from_RF_in_sec > 6:
@@ -293,7 +287,7 @@ def test_speedProfiling():
     objects = [F, LF, L, LB, RF, R, RB, B]
 
     state = State(None, objects, ego)
-    behavioral_state = SemanticBehavioralGridState.create_from_state(state, logger)
+    behavioral_state = BehavioralGridState.create_from_state(state, logger)
 
     logger = AV_Logger.get_logger(BEHAVIORAL_PLANNING_NAME_FOR_LOGGING)
     predictor = RoadFollowingPredictor(logger)
@@ -304,7 +298,7 @@ def test_speedProfiling():
 
     start = time.time()
 
-    costs = evaluator.evaluate_recipes(behavioral_state, action_recipes, recipes_mask, None)
+    costs = evaluator.evaluate(behavioral_state, action_recipes, recipes_mask)
 
     end = time.time()
 
@@ -352,20 +346,20 @@ def test_behavioralScenarios_moveToLeft_findThresholds():
 
                 objects = [F]
                 state = State(None, objects, ego)
-                behavioral_state = SemanticBehavioralGridState.create_from_state(state, logger)
+                behavioral_state = BehavioralGridState.create_from_state(state, logger)
 
                 actions = []
                 # create actions based on the occupancy grid
-                if (SEMANTIC_CELL_LAT_SAME, SEMANTIC_CELL_LON_FRONT) in behavioral_state.road_occupancy_grid:
+                if (RelativeLane.SAME_LANE, RelativeLongitudinalPosition.FRONT) in behavioral_state.road_occupancy_grid:
                     right_action = DynamicActionRecipe(RelativeLane.SAME_LANE, RelativeLongitudinalPosition.FRONT,
-                                                           ActionType.FOLLOW_VEHICLE, AggressivenessLevel.CALM)
+                                                       ActionType.FOLLOW_VEHICLE, AggressivenessLevel.CALM)
                     actions.append(right_action)
                 right_action_stat = StaticActionRecipe(RelativeLane.SAME_LANE, des_vel, AggressivenessLevel.CALM)
                 actions.append(right_action_stat)
                 left_action = StaticActionRecipe(RelativeLane.LEFT_LANE, des_vel, AggressivenessLevel.CALM)
                 actions.append(left_action)
 
-                costs = evaluator.evaluate_recipes(behavioral_state, actions, [True] * len(actions), None)
+                costs = evaluator.evaluate(behavioral_state, actions, [True] * len(actions))
 
                 best_idx = np.argmin(costs)
 