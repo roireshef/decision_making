ff6e42e0e6011e27e29f1c4a5a215162de1283ce
RoiReshef <roi.reshef@gm.com>
2018-05-15 20:11:19 +0300

    fixed close-to-target-speed specification - needs testing

--- src/planning/behavioral/action_space/static_action_space.py
+++ src/planning/behavioral/action_space/static_action_space.py
@@ -3,8 +3,7 @@ from typing import Optional, List, Type
 import numpy as np
 from sklearn.utils.extmath import cartesian
 
-from decision_making.src.global_constants import BP_ACTION_T_LIMITS, EPS, BP_JERK_S_JERK_D_TIME_WEIGHTS, \
-    BP_ACTION_T_RES, LON_ACC_LIMITS, LAT_ACC_LIMITS
+from decision_making.src.global_constants import BP_ACTION_T_LIMITS, BP_JERK_S_JERK_D_TIME_WEIGHTS
 from decision_making.src.planning.behavioral.action_space.action_space import ActionSpace
 from decision_making.src.planning.behavioral.behavioral_grid_state import BehavioralGridState
 from decision_making.src.planning.behavioral.constants import VELOCITY_STEP, MAX_VELOCITY, MIN_VELOCITY
@@ -55,8 +54,13 @@ class StaticActionSpace(ActionSpace):
         # T_s <- find minimal non-complex local optima within the BP_ACTION_T_LIMITS bounds, otherwise <np.nan>
         cost_coeffs_s = QuarticPoly1D.time_cost_function_derivative_coefs(
             w_T=weights[:, 2], w_J=weights[:, 0], a_0=ego_init_fstate[FS_SA], v_0=ego_init_fstate[FS_SV], v_T=v_T)
-        roots_s = ActionSpace.find_roots(cost_coeffs_s, [0, 20.0])
+        roots_s = ActionSpace.find_real_roots(cost_coeffs_s, np.array([0, BP_ACTION_T_LIMITS[LIMIT_MAX]]))
         T_s = np.fmin.reduce(roots_s, axis=-1)
+
+        # voids (setting <np.nan>) all non-Calm actions with T_s < (minimal allowed T_s)
+        # this still leaves some values of T_s which are smaller than (minimal allowed T_s) and will be replaced later
+        # when setting T
+        T_s[(T_s < BP_ACTION_T_LIMITS[LIMIT_MIN]) & (aggressiveness > AggressivenessLevel.CALM.value)] = np.nan
 
         # latitudinal difference to target
         latitudinal_difference = desired_center_lane_latitude - ego_init_fstate[FS_DX]
@@ -64,15 +68,16 @@ class StaticActionSpace(ActionSpace):
         # T_d <- find minimal non-complex local optima within the BP_ACTION_T_LIMITS bounds, otherwise <np.nan>
         cost_coeffs_d = QuinticPoly1D.time_cost_function_derivative_coefs(
             w_T=weights[:, 2], w_J=weights[:, 1], a_0=ego_init_fstate[FS_DA], v_0=ego_init_fstate[FS_DV], v_T=0,
-            ds_0=latitudinal_difference, T_m=0)
-        roots_d = ActionSpace.find_roots(cost_coeffs_d, [0, 20.0])
+            ds=latitudinal_difference, T_m=0)
+        roots_d = ActionSpace.find_real_roots(cost_coeffs_d, np.array([0, BP_ACTION_T_LIMITS[LIMIT_MAX]]))
         T_d = np.fmin.reduce(roots_d, axis=-1)
 
         # if both T_d[i] and T_s[i] are defined for i, then take maximum. otherwise leave it nan.
-        T = np.maximum(T_d, T_s)
+        T = np.maximum(np.maximum(T_d, T_s), BP_ACTION_T_LIMITS[LIMIT_MIN])
 
         # Calculate resulting distance from sampling the state at time T from the Quartic polynomial solution
-        distance_s = QuarticPoly1D.distance_profile_function(a_0=ego_init_fstate[FS_SA], v_0=ego_init_fstate[FS_SV], v_T=v_T, T=T)(T)
+        distance_s = QuarticPoly1D.distance_profile_function(a_0=ego_init_fstate[FS_SA], v_0=ego_init_fstate[FS_SV],
+                                                             v_T=v_T, T=T)(T)
         target_s = distance_s + ego_init_fstate[FS_SX]
 
         action_specs = [ActionSpec(t, v_T[i], target_s[i], desired_center_lane_latitude[i])
@@ -80,58 +85,4 @@ class StaticActionSpace(ActionSpace):
                         for i, t in enumerate(T)]
 
         return action_specs
-
-    def specify_goal(self, action_recipe: StaticActionRecipe, behavioral_state: BehavioralGridState) -> \
-            Optional[ActionSpec]:
-
-        ego = behavioral_state.ego_state
-        road_frenet = MapUtils.get_road_rhs_frenet(ego)
-
-        # project ego vehicle onto the road
-        ego_init_fstate = MapUtils.get_ego_road_localization(ego, road_frenet)
-
-        road_id = ego.road_localization.road_id
-        road_lane_latitudes = MapService.get_instance().get_center_lanes_latitudes(road_id)
-        desired_lane = ego.road_localization.lane_num + action_recipe.relative_lane.value
-        desired_center_lane_latitude = road_lane_latitudes[desired_lane]
-
-        T_s_vals = np.arange(BP_ACTION_T_LIMITS[LIMIT_MIN], BP_ACTION_T_LIMITS[LIMIT_MAX] + EPS, BP_ACTION_T_RES)
-
-        w_Js, w_Jd, w_T = BP_JERK_S_JERK_D_TIME_WEIGHTS[action_recipe.aggressiveness.value]
-        v_0, a_0 = ego_init_fstate[FS_SV], ego_init_fstate[FS_SA]
-        v_T = action_recipe.velocity
-
-        lon_time_cost_func_der = QuarticPoly1D.time_cost_function_derivative(w_T, w_Js, a_0, v_0, v_T)
-
-        T_s = ActionSpace.find_roots(lon_time_cost_func_der, T_s_vals)
-        # If roots were found out of the desired region, this action won't be specified
-        if len(T_s) == 0:
-            return None
-
-        # TODO: Do the same as above for lateral movement
-        # TODO: check if lateral trajectory is feasible(?)
-
-        latitudinal_difference = desired_center_lane_latitude - ego_init_fstate[FS_DX]
-        lat_time_cost_func_der = QuinticPoly1D.time_cost_function_derivative(w_T, w_Jd,
-                                                                             ego_init_fstate[FS_DA],
-                                                                             ego_init_fstate[FS_DV], 0,
-                                                                             latitudinal_difference,
-                                                                             T_m=0)
-
-        T_d_vals = np.arange(0.1, BP_ACTION_T_LIMITS[LIMIT_MAX] + EPS, BP_ACTION_T_RES)
-        T_d = ActionSpace.find_roots(lat_time_cost_func_der, T_d_vals)
-        # If roots were found out of the desired region, this action won't be specified
-        if len(T_d) == 0:
-            return None
-
-        # This stems from the assumption we've made about independency between d and s
-        planning_time = float(max(T_s[0], T_d[0]))
-
-        distance_func = QuarticPoly1D.distance_profile_function(a_0, v_0, v_T, planning_time)
-        target_s = distance_func(planning_time) + ego_init_fstate[FS_SX]
-
-        return ActionSpec(t=planning_time, v=v_T,
-                          s=target_s,
-                          d=desired_center_lane_latitude)
-
 
