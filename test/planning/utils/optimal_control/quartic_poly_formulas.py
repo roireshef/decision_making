-fixed quartic test and set filter grid bounds -fixed roots calculation

test/planning/utils/optimal_control/quartic_poly_formulas.py
@@ -1,13 +1,12 @@
-import sympy as sp
-import numpy as np
-from sympy import symbols
-from sympy.matrices import *
 import time
+import numpy as np
 
 from decision_making.paths import Paths
 from decision_making.src.global_constants import LON_ACC_LIMITS, BP_JERK_S_JERK_D_TIME_WEIGHTS, VELOCITY_LIMITS, \
     BP_ACTION_T_LIMITS, EPS
+from decision_making.src.planning.behavioral.data_objects import ActionType
 from decision_making.src.planning.utils.file_utils import BinaryReadWrite
+from decision_making.src.planning.utils.math import Math
 from decision_making.src.planning.utils.optimal_control.poly1d import QuarticPoly1D
 
 
@@ -17,6 +16,8 @@ def create_quartic_motion_funcs(a_0, v_0, v_T, T):
            QuarticPoly1D.acceleration_profile_function(a_0, v_0, v_T, T)
 
 
+action_type = ActionType.FOLLOW_LANE
+
 a_0_grid = np.arange(LON_ACC_LIMITS[0], LON_ACC_LIMITS[1]+EPS, 0.5)
 v_0_grid = np.arange(VELOCITY_LIMITS[0], VELOCITY_LIMITS[1]+EPS, 0.5)
 v_T_grid = np.arange(VELOCITY_LIMITS[0], VELOCITY_LIMITS[1]+EPS, 0.5)
@@ -24,47 +25,47 @@ predicate = np.full(shape=[v_0_grid.shape[0], a_0_grid.shape[0], v_T_grid.shape[
 
 if __name__ == "__main__":
 
-    T = symbols('T')
-    t = symbols('t')
-    Tm = symbols('T_m')  # safety margin in seconds
-
-    s0, v0, a0, vT, aT = symbols('s_0 v_0 a_0 v_T a_T')
-
-    A = Matrix([
-        [0, 0, 0, 0, 1],
-        [0, 0, 0, 1, 0],
-        [0, 0, 2, 0, 0],
-        [4*T**3, 3*T**2, 2*T, 1, 0],
-        [12*T**2, 6*T, 2, 0, 0]
-    ])
-
-    # solve to get solution
-    # this assumes a0=aT==0 (constant velocity)
-    [c4, c3, c2, c1, c0] = A.inv() * Matrix([s0, v0, a0, vT, aT])
-
-    x_t = (c4 * t**4 + c3 * t**3 + c2 * t**2 + c1 * t + c0).simplify()
-    v_t = sp.diff(x_t, t).simplify()
-    a_t = sp.diff(v_t, t).simplify()
-    j_t = sp.diff(a_t, t).simplify()
-
-    J = sp.integrate(j_t ** 2, (t, 0, T)).simplify()
-
-    wJ, wT = symbols('w_J w_T')
-
-    cost = (wJ * J + wT * T).simplify()
-
-    cost = cost.subs(s0, 0).subs(aT, 0).simplify()
-    cost_diff = sp.diff(cost, T).simplify()
-
-    temp_x_t = x_t.subs(s0, 0).subs(aT, 0).simplify()
-    temp_v_t = v_t.subs(s0, 0).subs(aT, 0).simplify()
-    temp_a_t = sp.diff(temp_v_t, t).simplify()
-
-    cost_desmos = cost.subs(a0, 0).simplify()
-    cost_diff_desmos = cost_diff.subs(a0, 0).simplify()
-    x_t_desmos = temp_x_t.subs(a0, 0).simplify()
-    v_t_desmos = temp_v_t.subs(a0, 0).simplify()
-    a_t_desmos = temp_a_t.subs(a0, 0).simplify()
+    # T = symbols('T')
+    # t = symbols('t')
+    # Tm = symbols('T_m')  # safety margin in seconds
+    #
+    # s0, v0, a0, vT, aT = symbols('s_0 v_0 a_0 v_T a_T')
+    #
+    # A = Matrix([
+    #     [0, 0, 0, 0, 1],
+    #     [0, 0, 0, 1, 0],
+    #     [0, 0, 2, 0, 0],
+    #     [4*T**3, 3*T**2, 2*T, 1, 0],
+    #     [12*T**2, 6*T, 2, 0, 0]
+    # ])
+    #
+    # # solve to get solution
+    # # this assumes a0=aT==0 (constant velocity)
+    # [c4, c3, c2, c1, c0] = A.inv() * Matrix([s0, v0, a0, vT, aT])
+    #
+    # x_t = (c4 * t**4 + c3 * t**3 + c2 * t**2 + c1 * t + c0).simplify()
+    # v_t = sp.diff(x_t, t).simplify()
+    # a_t = sp.diff(v_t, t).simplify()
+    # j_t = sp.diff(a_t, t).simplify()
+    #
+    # J = sp.integrate(j_t ** 2, (t, 0, T)).simplify()
+    #
+    # wJ, wT = symbols('w_J w_T')
+    #
+    # cost = (wJ * J + wT * T).simplify()
+    #
+    # cost = cost.subs(s0, 0).subs(aT, 0).simplify()
+    # cost_diff = sp.diff(cost, T).simplify()
+    #
+    # temp_x_t = x_t.subs(s0, 0).subs(aT, 0).simplify()
+    # temp_v_t = v_t.subs(s0, 0).subs(aT, 0).simplify()
+    # temp_a_t = sp.diff(temp_v_t, t).simplify()
+    #
+    # cost_desmos = cost.subs(a0, 0).simplify()
+    # cost_diff_desmos = cost_diff.subs(a0, 0).simplify()
+    # x_t_desmos = temp_x_t.subs(a0, 0).simplify()
+    # v_t_desmos = temp_v_t.subs(a0, 0).simplify()
+    # a_t_desmos = temp_a_t.subs(a0, 0).simplify()
 
     start_time = time.time()
     for weight in BP_JERK_S_JERK_D_TIME_WEIGHTS:
@@ -75,27 +76,29 @@ if __name__ == "__main__":
             for m, a_0 in enumerate(a_0_grid):
                 for j, v_T in enumerate(v_T_grid):
                     # start_time = time.time()
-                    lambda_func = QuarticPoly1D.time_cost_function_derivative(w_T, w_J, a_0, v_0, v_T)
-                    t = np.arange(BP_ACTION_T_LIMITS[0], BP_ACTION_T_LIMITS[1], 0.001)
-                    der = lambda_func(t)
-                    ind = np.argwhere(np.abs(der) < 0.01)
-                    if len(ind) == 0:
+                    time_cost_poly_coefs = \
+                    QuarticPoly1D.time_cost_function_derivative_coefs(np.array([w_T]), np.array([w_J]),
+                                                                      np.array([a_0]), np.array([v_0]), np.array([v_T]))[0]
+                    cost_roots_reals = Math.find_real_roots_in_limits(time_cost_poly_coefs,
+                                                                      np.array([EPS, BP_ACTION_T_LIMITS[1]]))
+                    extremum_T = cost_roots_reals[np.isfinite(cost_roots_reals)]
+                    if len(extremum_T) == 0:
                         predicate[k, m, j] = False
                         continue
-                    T = t[ind[0]]  # First extrema is our local (and sometimes global) minimum
+                    T = extremum_T.min()  # First extrema is our local (and sometimes global) minimum
 
                     v_t_func, a_t_func = create_quartic_motion_funcs(a_0, v_0, v_T, T)
                     t = np.arange(0, T, 0.01)
                     min_v, max_v = min(v_t_func(t)), max(v_t_func(t))
                     min_a, max_a = min(a_t_func(t)), max(a_t_func(t))
 
-                    is_T_in_range = (T >= BP_ACTION_T_LIMITS[0]) and (T <= BP_ACTION_T_LIMITS[1]+EPS)
+                    is_T_in_range = (T >= EPS) and (T <= BP_ACTION_T_LIMITS[1]+EPS)
                     is_vel_in_range = (min_v >= VELOCITY_LIMITS[0]) and (max_v <= VELOCITY_LIMITS[1]+EPS)
                     is_acc_in_range = (min_a >= LON_ACC_LIMITS[0]) and (max_a <= LON_ACC_LIMITS[1]+EPS)
 
                     predicate[k, m, j] = (is_T_in_range and is_vel_in_range and is_acc_in_range)
 
-        output_predicate_file_name = 'static_predicate_wT_%.2f_wJ_%.2f.bin' % (w_T, w_J)
+        output_predicate_file_name = '%s_predicate_wT_%.2f_wJ_%.2f.bin' % (action_type.name.lower(), w_T, w_J)
         output_predicate_file_path = Paths.get_resource_absolute_path_filename('predicates/%s' % output_predicate_file_name)
         BinaryReadWrite.save(array=predicate, file_path=output_predicate_file_path)
 

test/planning/utils/optimal_control/quintic_poly_formulas.py
@@ -1,8 +1,6 @@
-import sympy as sp
-import numpy as np
-from sympy import symbols
-from sympy.matrices import *
 import time
+
+import numpy as np
 
 from decision_making.paths import Paths
 from decision_making.src.global_constants import LON_ACC_LIMITS, BP_JERK_S_JERK_D_TIME_WEIGHTS, VELOCITY_LIMITS, \
@@ -23,59 +21,59 @@ def create_quintic_motion_funcs(a_0, v_0, v_T, s_T, T, T_m):
 
 action_type = ActionType.FOLLOW_VEHICLE
 T_m = 2
-st_limits = [0, 100]
+st_limits = [0, 110]
 
 a_0_grid = np.arange(LON_ACC_LIMITS[0], LON_ACC_LIMITS[1]+EPS, 0.5)
-v_0_grid = np.arange(VELOCITY_LIMITS[0], 20+EPS, 0.5)
+v_0_grid = np.arange(VELOCITY_LIMITS[0], VELOCITY_LIMITS[1]+EPS, 0.5)
+v_T_grid = np.arange(VELOCITY_LIMITS[0], VELOCITY_LIMITS[1]+EPS, 0.5)
 s_T_grid = np.arange(st_limits[0], st_limits[1]+EPS, 1)
-v_T_grid = np.arange(VELOCITY_LIMITS[0], 20+EPS, 0.5)
 predicate = np.full(shape=[v_0_grid.shape[0], a_0_grid.shape[0], s_T_grid.shape[0], v_T_grid.shape[0]], fill_value=False)
 
 if __name__ == "__main__":
 
-    T = symbols('T')
-    t = symbols('t')
-    Tm = symbols('T_m')  # safety margin in seconds
-
-    s0, v0, a0, sT, vT, aT = symbols('s_0 v_0 a_0 s_T v_T a_T')
-
-    A = Matrix([
-        [0, 0, 0, 0, 0, 1],
-        [0, 0, 0, 0, 1, 0],
-        [0, 0, 0, 2, 0, 0],
-        [T ** 5, T ** 4, T ** 3, T ** 2, T, 1],
-        [5 * T ** 4, 4 * T ** 3, 3 * T ** 2, 2 * T, 1, 0],
-        [20 * T ** 3, 12 * T ** 2, 6 * T, 2, 0, 0]]
-    )
-
-    # solve to get solution
-    # this assumes a0=aT==0 (constant velocity)
-    [c5, c4, c3, c2, c1, c0] = A.inv() * Matrix([s0, v0, a0, sT + vT * (T - Tm), vT, aT])
-
-    x_t = (c5 * t ** 5 + c4 * t ** 4 + c3 * t ** 3 + c2 * t ** 2 + c1 * t + c0).simplify()
-    v_t = sp.diff(x_t, t).simplify()
-    a_t = sp.diff(v_t, t).simplify()
-    j_t = sp.diff(a_t, t).simplify()
-
-    J = sp.integrate(j_t ** 2, (t, 0, T)).simplify()
-
-    wJ, wT = symbols('w_J w_T')
-
-    cost = (wJ * J + wT * T).simplify()
-
-    cost = cost.subs(s0, 0).subs(aT, 0).simplify()
-    cost_diff = sp.diff(cost, T).simplify()
-
-    temp_v_t = v_t.subs(s0, 0).subs(aT, 0).simplify()
-    temp_delta_s_t = (sT + vT * t - x_t.subs(s0, 0).subs(aT, 0)).simplify()
-    temp_distance_from_target_deriv = sp.diff(temp_delta_s_t, t).simplify()
-    temp_a_t = sp.diff(temp_v_t, t).simplify()
-
-    cost_desmos = cost.subs(a0, 0).simplify()
-    cost_diff_desmos = cost_diff.subs(a0, 0).simplify()
-    delta_s_t_desmos = temp_delta_s_t.subs(a0, 0).simplify()
-    v_t_desmos = temp_v_t.subs(a0, 0).simplify()
-    a_t_desmos = temp_a_t.subs(a0, 0).simplify()
+    # T = symbols('T')
+    # t = symbols('t')
+    # Tm = symbols('T_m')  # safety margin in seconds
+    #
+    # s0, v0, a0, sT, vT, aT = symbols('s_0 v_0 a_0 s_T v_T a_T')
+    #
+    # A = Matrix([
+    #     [0, 0, 0, 0, 0, 1],
+    #     [0, 0, 0, 0, 1, 0],
+    #     [0, 0, 0, 2, 0, 0],
+    #     [T ** 5, T ** 4, T ** 3, T ** 2, T, 1],
+    #     [5 * T ** 4, 4 * T ** 3, 3 * T ** 2, 2 * T, 1, 0],
+    #     [20 * T ** 3, 12 * T ** 2, 6 * T, 2, 0, 0]]
+    # )
+    #
+    # # solve to get solution
+    # # this assumes a0=aT==0 (constant velocity)
+    # [c5, c4, c3, c2, c1, c0] = A.inv() * Matrix([s0, v0, a0, sT + vT * (T - Tm), vT, aT])
+    #
+    # x_t = (c5 * t ** 5 + c4 * t ** 4 + c3 * t ** 3 + c2 * t ** 2 + c1 * t + c0).simplify()
+    # v_t = sp.diff(x_t, t).simplify()
+    # a_t = sp.diff(v_t, t).simplify()
+    # j_t = sp.diff(a_t, t).simplify()
+    #
+    # J = sp.integrate(j_t ** 2, (t, 0, T)).simplify()
+    #
+    # wJ, wT = symbols('w_J w_T')
+    #
+    # cost = (wJ * J + wT * T).simplify()
+    #
+    # cost = cost.subs(s0, 0).subs(aT, 0).simplify()
+    # cost_diff = sp.diff(cost, T).simplify()
+    #
+    # temp_v_t = v_t.subs(s0, 0).subs(aT, 0).simplify()
+    # temp_delta_s_t = (sT + vT * t - x_t.subs(s0, 0).subs(aT, 0)).simplify()
+    # temp_distance_from_target_deriv = sp.diff(temp_delta_s_t, t).simplify()
+    # temp_a_t = sp.diff(temp_v_t, t).simplify()
+    #
+    # cost_desmos = cost.subs(a0, 0).simplify()
+    # cost_diff_desmos = cost_diff.subs(a0, 0).simplify()
+    # delta_s_t_desmos = temp_delta_s_t.subs(a0, 0).simplify()
+    # v_t_desmos = temp_v_t.subs(a0, 0).simplify()
+    # a_t_desmos = temp_a_t.subs(a0, 0).simplify()
 
     start_time = time.time()
     for action_type in [ActionType.FOLLOW_VEHICLE, ActionType.OVER_TAKE_VEHICLE]:
@@ -92,7 +90,7 @@ if __name__ == "__main__":
                         for j, v_T in enumerate(v_T_grid):
                             time_cost_poly_coefs = QuinticPoly1D.time_cost_function_derivative_coefs(np.array([w_T]), np.array([w_J]),
                                         np.array([a_0]), np.array([v_0]), np.array([v_T]), np.array([s_T]), np.array([T_m]))[0]
-                            cost_roots_reals = Math.find_real_roots_in_limits(time_cost_poly_coefs, np.array([BP_ACTION_T_LIMITS[0], BP_ACTION_T_LIMITS[1]]))
+                            cost_roots_reals = Math.find_real_roots_in_limits(time_cost_poly_coefs, np.array([EPS, BP_ACTION_T_LIMITS[1]]))
                             extremum_T = cost_roots_reals[np.isfinite(cost_roots_reals)]
                             if len(extremum_T) == 0:
                                 predicate[k, m, i, j] = False
@@ -101,14 +99,14 @@ if __name__ == "__main__":
 
                             delta_s_t_func, coefs_s_der, v_t_func, a_t_func = create_quintic_motion_funcs(a_0, v_0, v_T, s_T, T, T_m=T_m)
 
-                            roots_s_reals = Math.find_real_roots_in_limits(coefs_s_der, np.array([BP_ACTION_T_LIMITS[0], BP_ACTION_T_LIMITS[1]]))
+                            s_roots_reals = Math.find_real_roots_in_limits(coefs_s_der, np.array([EPS, T]))
 
                             t = np.arange(0, T, 0.01)
-                            extremum_delta_s_val = delta_s_t_func(roots_s_reals[np.isfinite(roots_s_reals)])
+                            extremum_delta_s_val = delta_s_t_func(s_roots_reals[np.isfinite(s_roots_reals)])
                             min_v, max_v = min(v_t_func(t)), max(v_t_func(t))
                             min_a, max_a = min(a_t_func(t)), max(a_t_func(t))
 
-                            is_T_in_range = (T >= BP_ACTION_T_LIMITS[0]) and (T <= BP_ACTION_T_LIMITS[1]+EPS)
+                            is_T_in_range = (T >= EPS) and (T <= BP_ACTION_T_LIMITS[1]+EPS)
                             is_vel_in_range = (min_v >= VELOCITY_LIMITS[0]) and (max_v <= VELOCITY_LIMITS[1]+EPS)
                             is_acc_in_range = (min_a >= LON_ACC_LIMITS[0]) and (max_a <= LON_ACC_LIMITS[1]+EPS)
                             if action_type == ActionType.FOLLOW_VEHICLE: