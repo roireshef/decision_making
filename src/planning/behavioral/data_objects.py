3cb0e1cc46fbd9f89669444bcbc6f0aab6e71410
mz8cj6 <vlad.goldner@gm.com>
2018-05-21 16:26:33 +0300

    define NavigationGoal class and add it as a parameter to the function ValueApproximator.approximate

--- src/planning/behavioral/data_objects.py
+++ src/planning/behavioral/data_objects.py
@@ -75,3 +75,17 @@ class ActionSpec:
 
     def __str__(self):
         return str({k: str(v) for (k, v) in self.__dict__.items()})
+
+
+class NavigationGoal:
+    def __init__(self, road_id: int, lon: float, lanes: List[int]):
+        """
+        Holds parameters of a navigation goal: road id, longitude, list of lanes.
+        Used by value function approximation.
+        :param road_id: road id from the map
+        :param lon: [m] longitude of the goal relatively to the road's beginning
+        :param lanes: list of lane indices of the goal
+        """
+        self.road_id = road_id
+        self.lon = lon
+        self.lanes_list = lanes
--- src/planning/behavioral/evaluators/value_approximator.py
+++ src/planning/behavioral/evaluators/value_approximator.py
@@ -4,6 +4,7 @@ from logging import Logger
 import six
 
 from decision_making.src.planning.behavioral.behavioral_grid_state import BehavioralGridState
+from decision_making.src.planning.behavioral.data_objects import NavigationGoal
 
 
 @six.add_metaclass(ABCMeta)
@@ -12,5 +13,5 @@ class ValueApproximator:
         self.logger = logger
 
     @abstractmethod
-    def approximate(self, behavioral_state: BehavioralGridState) -> float:
+    def approximate(self, behavioral_state: BehavioralGridState, goal: NavigationGoal) -> float:
         pass
