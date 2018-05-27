cf8c27f8aa4ff4003bb4115a8e5327ac50ed1d59
RoiReshef <roi.reshef@gm.com>
2018-05-27 16:16:55 +0300

    changes to conform new naming in mapping repo

--- src/planning/utils/map_utils.py
+++ src/planning/utils/map_utils.py
@@ -1,8 +1,7 @@
 from decision_making.src.planning.types import CartesianExtendedState, C_X, C_Y
 from decision_making.src.planning.types import FrenetState2D
 from decision_making.src.state.state import MapState
-from mapping.src.localization.localization import GlobalLocalization, SegmentLocalization, MapLocalization, \
-    LaneLocalization, RoadLocalization
+from mapping.src.localization.localization import GlobalLocalization
 from mapping.src.service.map_service import MapService
 
 
@@ -15,33 +14,24 @@ class MapUtils:
 
         x, y = cartesian_state[C_X], cartesian_state[C_Y]
         global_localization = GlobalLocalization(x, y, 0, None)
-        map_localization = map_api.global_to_map_localization(global_localization)
+        lane = map_api.resolve_location_on_global(global_localization)
+        lane_fstate = lane.frame.center_frenet_frame.cstate_to_fstate(cartesian_state)
 
-        lane = map_api.resolve_location_on_map(map_localization)
-        lane_fstate = lane.get_as_frame().get_center_frame().cstate_to_fstate(cartesian_state)
+        segment = lane.segment
+        road = lane.segment.road
 
-        segment = lane.parent
-
-        road = lane.parent.parent
-        road_fstate = road.get_as_frame().get_center_frame().cstate_to_fstate(cartesian_state)
-
-        return MapState(lane_fstate, road_fstate, road.id, segment.id, lane.id)
+        return MapState(lane_fstate, road.id, segment.id, lane.id)
 
     @staticmethod
     def convert_map_to_cartesian_state(map_state):
         # type: (MapState) -> FrenetState2D
         map_api = MapService.get_instance()
 
-        road_localization = RoadLocalization(map_state.road_id, 0)
-        segment_localization = SegmentLocalization(map_state.segment_id, 0, 0)
-        lane_localization = LaneLocalization(map_state.lane_id, 0, 0, None)
-        map_localization = MapLocalization(road_localization, segment_localization, lane_localization)
+        lane = map_api.get_lane(map_state.lane_id)
 
-        lane = map_api.resolve_location_on_map(map_localization)
+        cartesian_state = lane.frame.frenet_to_cartesian(map_state.lane_state)
 
-        frenet = lane.get_as_frame().get_center_frame()
-
-        return frenet.cstate_to_fstate(map_state.lane_state)
+        return cartesian_state
 
     @staticmethod
     def convert_lane_to_road_localization(lane_state, road_id, segment_id, lane_id):
@@ -52,10 +42,10 @@ class MapUtils:
         segment = road.get_segment(segment_id)
         lane = segment.get_lane(lane_id)
 
-        road_frenet = road.get_as_frame().get_center_frame()
-        lane_frenet = lane.get_as_frame().get_center_frame()
+        road_frenet = road.frame
+        lane_frenet = lane.frame
 
-        return road_frenet.cstate_to_fstate(lane_frenet.fstate_to_cstate(lane_state))
+        return road_frenet.cartesian_to_frenet(lane_frenet.frenet_to_cartesian(lane_state))
 
     @staticmethod
     def convert_road_to_lane_localization(lane_state, road_id, segment_id, lane_id):
@@ -66,8 +56,8 @@ class MapUtils:
         segment = road.get_segment(segment_id)
         lane = segment.get_lane(lane_id)
 
-        road_frenet = road.get_as_frame().get_center_frame()
-        lane_frenet = lane.get_as_frame().get_center_frame()
+        road_frenet = road.frame.center_frenet_frame
+        lane_frenet = lane.frame.center_frenet_frame
 
         return lane_frenet.cstate_to_fstate(road_frenet.fstate_to_cstate(lane_state))
 
@@ -80,8 +70,8 @@ class MapUtils:
         segment = road.get_segment(segment_id)
         lane = segment.get_lane(lane_id)
 
-        segment_frenet = segment.get_as_frame().get_center_frame()
-        lane_frenet = lane.get_as_frame().get_center_frame()
+        segment_frenet = segment.frame.center_frenet_frame
+        lane_frenet = lane.frame.center_frenet_frame
 
         return segment_frenet.cstate_to_fstate(lane_frenet.fstate_to_cstate(lane_state))
 
@@ -94,8 +84,8 @@ class MapUtils:
         segment = road.get_segment(segment_id)
         lane = segment.get_lane(lane_id)
 
-        segment_frenet = segment.get_as_frame().get_center_frame()
-        lane_frenet = lane.get_as_frame().get_center_frame()
+        segment_frenet = segment.frame.center_frenet_frame
+        lane_frenet = lane.frame.center_frenet_frame
 
         return lane_frenet.cstate_to_fstate(segment_frenet.fstate_to_cstate(lane_state))
 
@@ -107,8 +97,8 @@ class MapUtils:
         road = map_api._map_model._map.get_road(road_id)
         segment = road.get_segment(segment_id)
 
-        road_frenet = road.get_as_frame().get_center_frame()
-        segment_frenet = segment.get_as_frame().get_center_frame()
+        road_frenet = road.frame.center_frenet_frame
+        segment_frenet = segment.frame.center_frenet_frame
 
         return segment_frenet.cstate_to_fstate(road_frenet.fstate_to_cstate(lane_state))
 
@@ -120,7 +110,7 @@ class MapUtils:
         road = map_api._map_model._map.get_road(road_id)
         segment = road.get_segment(segment_id)
 
-        road_frenet = road.get_as_frame().get_center_frame()
-        segment_frenet = segment.get_as_frame().get_center_frame()
+        road_frenet = road.frame.center_frenet_frame
+        segment_frenet = segment.frame.center_frenet_frame
 
         return road_frenet.cstate_to_fstate(segment_frenet.fstate_to_cstate(lane_state))
