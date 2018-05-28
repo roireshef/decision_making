dfa214698130c3b62f8a110605da7ed2251e3588
mz8cj6 <vlad.goldner@gm.com>
2018-05-28 10:27:01 +0300

    add coverage tests to static & dynamic specify

--- test/planning/behavioral/action_space/test_dynamicActionSpace.py
+++ test/planning/behavioral/action_space/test_dynamicActionSpace.py
@@ -4,12 +4,16 @@ from typing import List
 import numpy as np
 
 from decision_making.src.global_constants import SAFE_DIST_TIME_DELAY, LONGITUDINAL_SAFETY_MARGIN_FROM_OBJECT
+from decision_making.src.planning.behavioral.action_space.action_space import ActionSpaceContainer
 from decision_making.src.planning.behavioral.action_space.dynamic_action_space import DynamicActionSpace
 from decision_making.src.planning.behavioral.behavioral_grid_state import BehavioralGridState, RelativeLane, \
     RelativeLongitudinalPosition
 from decision_making.src.planning.behavioral.data_objects import DynamicActionRecipe, AggressivenessLevel
+from decision_making.src.planning.behavioral.filtering.action_spec_filter_bank import FilterIfNone
+from decision_making.src.planning.behavioral.filtering.action_spec_filtering import ActionSpecFiltering
 from decision_making.src.planning.behavioral.filtering.recipe_filtering import RecipeFiltering
 from decision_making.src.planning.types import FS_SX, FS_SV
+from decision_making.src.planning.utils.map_utils import MapUtils
 from decision_making.src.prediction.road_following_predictor import RoadFollowingPredictor
 from decision_making.src.state.state import ObjectSize, EgoState, State
 from decision_making.test.planning.behavioral.behavioral_state_fixtures import behavioral_grid_state, \
@@ -85,3 +89,44 @@ def test_specifyGoal_slightlyUnsafeState_shouldSucceed():
 
     # verify that there is at least one valid recipe for dynamic actions
     assert len(front_recipies) > 0
+
+
+def test_specifyGoal_atLeastOneActionShouldBeFound():
+    """
+    Object and ego have the same velocity 14 m/s (~50 km/h), the initial distance 49 m.
+    At least one dynamic action should be created.
+    :return:
+    """
+    logger = Logger("test_specifyGoal_dynamic")
+    road_id = 20
+    ego_lon = 400.
+    lane_width = MapService.get_instance().get_road(road_id).lane_width
+    length = 4
+    size = ObjectSize(length, 2, 1)
+
+    predictor = RoadFollowingPredictor(logger)
+    action_space = ActionSpaceContainer(logger, [DynamicActionSpace(logger, predictor, DEFAULT_DYNAMIC_RECIPE_FILTERING)])
+    action_spec_validator = ActionSpecFiltering([FilterIfNone()], logger)
+    road_frenet = MapUtils.get_road_rhs_frenet_by_road_id(road_id)
+
+    ego_vel = 14
+    F_vel = 14
+    dist = 45
+
+    ego = MapUtils.create_canonic_ego(0, ego_lon, lane_width / 2, ego_vel, size, road_frenet)
+    F_lon = ego_lon + dist
+    F = MapUtils.create_canonic_object(1, 0, F_lon, lane_width / 2, F_vel, size, road_frenet)
+
+    state = State(None, [F], ego)
+    behavioral_state = BehavioralGridState.create_from_state(state, logger)
+    recipes = action_space.recipes
+    recipes_mask = action_space.filter_recipes(recipes, behavioral_state)
+
+    # Action specification
+    specs = np.full(recipes.__len__(), None)
+    valid_action_recipes = [recipe for i, recipe in enumerate(recipes) if recipes_mask[i]]
+    specs[recipes_mask] = action_space.specify_goals(valid_action_recipes, behavioral_state)
+
+    specs_mask = action_spec_validator.filter_action_specs(specs, behavioral_state)
+    valid_idxs = np.where(specs_mask)[0]
+    assert len(valid_idxs) > 0
--- test/planning/behavioral/action_space/test_staticActionSpace.py
+++ test/planning/behavioral/action_space/test_staticActionSpace.py
@@ -3,12 +3,24 @@ from logging import Logger
 import numpy as np
 
 from decision_making.src.global_constants import LON_ACC_LIMITS
+from decision_making.src.planning.behavioral.action_space.action_space import ActionSpaceContainer
+from decision_making.src.planning.behavioral.action_space.dynamic_action_space import DynamicActionSpace
 from decision_making.src.planning.behavioral.action_space.static_action_space import StaticActionSpace
 from decision_making.src.planning.behavioral.behavioral_grid_state import BehavioralGridState, RelativeLane
-from decision_making.src.planning.behavioral.default_config import DEFAULT_STATIC_RECIPE_FILTERING
+from decision_making.src.planning.behavioral.default_config import DEFAULT_STATIC_RECIPE_FILTERING, \
+    DEFAULT_DYNAMIC_RECIPE_FILTERING
 from decision_making.src.planning.behavioral.data_objects import AggressivenessLevel, StaticActionRecipe
+from decision_making.src.planning.behavioral.evaluators.heuristic_action_spec_evaluator import \
+    HeuristicActionSpecEvaluator
+from decision_making.src.planning.behavioral.evaluators.naive_value_approximator import NaiveValueApproximator
+from decision_making.src.planning.behavioral.evaluators.velocity_profile import VelocityProfile
+from decision_making.src.planning.behavioral.filtering.action_spec_filter_bank import FilterIfNone
+from decision_making.src.planning.behavioral.filtering.action_spec_filtering import ActionSpecFiltering
+from decision_making.src.planning.behavioral.planner.single_step_behavioral_planner import SingleStepBehavioralPlanner
+from decision_making.src.planning.utils.map_utils import MapUtils
 from decision_making.src.planning.utils.math import Math
 from decision_making.src.planning.utils.optimal_control.poly1d import QuarticPoly1D
+from decision_making.src.prediction.road_following_predictor import RoadFollowingPredictor
 from decision_making.src.state.state import ObjectSize, EgoState, State
 from mapping.src.service.map_service import MapService
 
@@ -127,3 +139,56 @@ def test_specifyGoal_acceleration_accelerationsCoverage():
     assert len(max_acc) >= 3
     assert max_acc[2] / max_acc[1] < 3
     assert max_acc[1] / max_acc[0] < 3
+
+
+def test_specifyGoal_findSafeNotAggressiveAction():
+    """
+    Ego velocity 23 m/s, object velocity 14 m/s, the distance 92 m.
+    Verify specifications coverage and existence of at least one static action, that is not too aggressive and safe.
+    """
+    logger = Logger("test_specifyGoal_static")
+    road_id = 20
+    ego_lon = 400.
+    lane_width = MapService.get_instance().get_road(road_id).lane_width
+    length = 4
+    size = ObjectSize(length, 2, 1)
+
+    action_space = ActionSpaceContainer(logger, [StaticActionSpace(logger, DEFAULT_STATIC_RECIPE_FILTERING)])
+    spec_evaluator = HeuristicActionSpecEvaluator(logger)
+    action_spec_validator = ActionSpecFiltering([FilterIfNone()], logger)
+    road_frenet = MapUtils.get_road_rhs_frenet_by_road_id(road_id)
+
+    ego_vel = 23
+    F_vel = 14
+    dist = 92
+
+    ego = MapUtils.create_canonic_ego(0, ego_lon, lane_width / 2, ego_vel, size, road_frenet)
+    F_lon = ego_lon + dist
+    F = MapUtils.create_canonic_object(1, 0, F_lon, lane_width / 2, F_vel, size, road_frenet)
+
+    state = State(None, [F], ego)
+    behavioral_state = BehavioralGridState.create_from_state(state, logger)
+    recipes = action_space.recipes
+    recipes_mask = action_space.filter_recipes(recipes, behavioral_state)
+
+    for i, recipe in enumerate(recipes):
+        if recipe.relative_lane != RelativeLane.SAME_LANE or not np.isclose(recipe.velocity, 50 / 3.6):
+            recipes_mask[i] = False
+
+    # Action specification
+    specs = np.full(recipes.__len__(), None)
+    valid_action_recipes = [recipe for i, recipe in enumerate(recipes) if recipes_mask[i]]
+    specs[recipes_mask] = action_space.specify_goals(valid_action_recipes, behavioral_state)
+
+    specs_mask = action_spec_validator.filter_action_specs(specs, behavioral_state)
+    valid_idxs = np.where(specs_mask)[0]
+    assert len(valid_idxs) > 1
+    assert specs[valid_idxs][0].t / specs[valid_idxs][1].t < 3
+
+    # State-Action Evaluation
+    costs = spec_evaluator.evaluate(behavioral_state, recipes, list(specs), specs_mask)
+    safe_not_aggressive_indices = []
+    for i in valid_idxs:
+        if specs[i].t > 5 and costs[i] < np.inf:
+            safe_not_aggressive_indices.append(i)
+    assert len(safe_not_aggressive_indices) > 0
