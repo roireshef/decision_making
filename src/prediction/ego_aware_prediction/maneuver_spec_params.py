dbfbc313ff1322794772383487a744979a6e2ec7
Max Bluvstein <xzjsyy@gm.com>
2018-05-29 17:20:21 +0300

    updated maneuver model

--- src/prediction/ego_aware_prediction/maneuver_spec_params.py
+++ src/prediction/ego_aware_prediction/maneuver_spec_params.py
@@ -2,10 +2,10 @@ from decision_making.src.messages.str_serializable import StrSerializable
 
 
 class ManeuverSpecParams(StrSerializable):
-    def __init__(self, T_d: float, avg_s_a: float, s_a_final: float, relative_lane: float, lat_normalized: float):
+    def __init__(self, T_s: float, avg_s_a: float, s_a_final: float, relative_lane: float, lat_normalized: float):
         """
         Parametrization of Frenet based maneuver
-        :param T_d: lateral maneuver duration in [sec]
+        :param T_s: lateral maneuver duration in [sec]
         :param avg_s_a: average acceleration in [m/s^2]
         :param s_a_final: final acceleration in [m/s^2]
         :param relative_lane: relative lane (integer in [lanes] units)
@@ -15,4 +15,4 @@ class ManeuverSpecParams(StrSerializable):
         self.relative_lane = relative_lane
         self.s_a_final = s_a_final
         self.avg_s_a = avg_s_a
-        self.T_d = T_d
+        self.T_s = T_s
