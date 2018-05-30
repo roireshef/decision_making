ee6f5b276f39aa671770a95c045c2a2eba410bd2
uriv <uri.verner@gm.com>
2018-05-30 10:22:20 +0300

    added profiler logging level param to module and updated log msgs to lazy evaluation

--- src/infra/dm_module.py
+++ src/infra/dm_module.py
@@ -1,9 +1,10 @@
 from abc import abstractmethod, ABCMeta
-from logging import Logger
+from rte.python.logger.AV_logger import AV_Logger, INFO
 
 from common_data.src.communication.pubsub.pubsub import PubSub
 import six
 import rte.python.profiler as prof
+
 
 @six.add_metaclass(ABCMeta)
 class DmModule:
@@ -11,14 +12,16 @@ class DmModule:
     Abstract class which is implemented in functional DM modules and facades.
     """
     def __init__(self, pubsub, logger):
-        # type: (PubSub, Logger) -> None
+        # type: (PubSub, AV_Logger) -> None
         self.pubsub = pubsub
         """
         :param dds: Inter-process communication interface.
         :param logger: Logging interface.
         """
+        self.name = self.__class__.__name__
         self.logger = logger
-        self.logger.info("initializing module: " + self.__class__.__name__)
+        self.logger.info("initializing module: %s", self.name)
+        self.profiler_log_level = INFO
 
     @abstractmethod
     def _start_impl(self):
@@ -30,7 +33,7 @@ class DmModule:
 
     def start(self):
         # type: () -> None
-        self.logger.info("starting module: " + self.__class__.__name__)
+        self.logger.info("starting module: %s", self.name)
         self._start_impl()
 
     @abstractmethod
@@ -43,7 +46,7 @@ class DmModule:
 
     def stop(self):
         # type: () -> None
-        self.logger.info("stopping module: " + self.__class__.__name__)
+        self.logger.info("stopping module: %s", self.name)
         self._stop_impl()
 
     @abstractmethod
@@ -59,8 +62,8 @@ class DmModule:
         """
         Perform triggered action and write logging messages.
         """
-        self.logger.debug("executing periodic action at module: " + self.__class__.__name__)
-        with prof.time_range(self.__class__.__name__ + ":periodic"):
+        self.logger.debug("executing periodic action at module: %s", self.name)
+        with prof.time_range("%s:periodic", self.name, log_level=self.profiler_log_level):
             self._periodic_action_impl()
-        self.logger.debug("finished periodic action at module: " + self.__class__.__name__)
+        self.logger.debug("finished periodic action at module: %s", self.name)
 
