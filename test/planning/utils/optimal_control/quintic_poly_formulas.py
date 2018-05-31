8a25493eba64f9096910ec988cf63e0832e923a5
Gabi <gabi.guetta@gm.com>
2018-05-31 17:02:06 +0300

    wip

--- test/planning/utils/optimal_control/quintic_poly_formulas.py
+++ test/planning/utils/optimal_control/quintic_poly_formulas.py
@@ -6,11 +6,10 @@ from sympy import symbols
 from sympy.matrices import *
 
 from decision_making.paths import Paths
-from decision_making.src.global_constants import LON_ACC_LIMITS, BP_JERK_S_JERK_D_TIME_WEIGHTS, VELOCITY_LIMITS, \
-    BP_ACTION_T_LIMITS, EPS, FILTER_V_0_GRID, FILTER_A_0_GRID, FILTER_S_T_GRID, \
-    FILTER_V_T_GRID, SAFE_DIST_TIME_DELAY
+from decision_making.src.global_constants import LON_ACC_LIMITS, VELOCITY_LIMITS, \
+    BP_ACTION_T_LIMITS, EPS
 from decision_making.src.planning.behavioral.data_objects import ActionType
-from decision_making.src.planning.utils.file_utils import BinaryReadWrite, TextReadWrite
+from decision_making.src.planning.utils.file_utils import BinaryReadWrite
 from decision_making.src.planning.utils.math import Math
 from decision_making.src.planning.utils.numpy_utils import UniformGrid
 from decision_making.src.planning.utils.optimal_control.poly1d import QuinticPoly1D
@@ -60,28 +59,32 @@ class QuinticMotionSymbolicsCreator:
         package_delta_s_t = (sT + vT * t - x_t.subs(s0, 0).subs(aT, 0)).simplify()
         package_distance_from_target_deriv = sp.diff(package_delta_s_t, t).simplify()
         package_a_t = sp.diff(package_v_t, t).simplify()
+        package_j_t = sp.diff(package_a_t, t).simplify()
 
         cost_desmos = cost.subs(a0, 0).simplify()
         cost_diff_desmos = cost_diff.subs(a0, 0).simplify()
         delta_s_t_desmos = package_delta_s_t.subs(a0, 0).simplify()
         v_t_desmos = package_v_t.subs(a0, 0).simplify()
         a_t_desmos = package_a_t.subs(a0, 0).simplify()
+        j_t_desmos = package_j_t.subs(a0, 0).simplify()
 
-        return package_v_t, package_delta_s_t, package_distance_from_target_deriv, package_a_t, cost_desmos, \
-               cost_diff_desmos, delta_s_t_desmos, v_t_desmos, a_t_desmos
+        return package_v_t, package_delta_s_t, package_distance_from_target_deriv, package_a_t, package_j_t,\
+               cost_desmos, cost_diff_desmos, delta_s_t_desmos, v_t_desmos, a_t_desmos, j_t_desmos
 
 
 class QuinticMotionPredicatesCreator:
     """This class creates predicates for filtering trajectories before specification according to initial velocity and
      acceleration, distance from target vehicle, and final velocity"""
+
     def __init__(self, v0_grid: UniformGrid, a0_grid: UniformGrid, sT_grid: UniformGrid, vT_grid: UniformGrid,
-                 T_m: float, predicates_resources_target_directory: str):
+                 T_m: float, T_safety: float, predicates_resources_target_directory: str):
         """
         :param v0_grid: A grid of initial velocities by which the predicates will be created (typically constant)
         :param a0_grid: A grid of initial accelerations by which the predicates will be created (typically constant)
         :param sT_grid: A grid of initial distances from target by which the predicates will be created (typically constant)
         :param vT_grid: A grid of final velocities by which the predicates will be created (typically constant)
-        :param T_m: Safety time interval for following/overtaking actions (typically constant)
+        :param T_m: Specification margin time interval for following/overtaking actions (typically constant)
+        :param T_safety: Safety margin time interval for following/overtaking actions (typically constant)
         :param predicates_resources_target_directory: A target directory inside resources directory where the predicates
                 will be created(typically constant)
         """
@@ -90,10 +93,11 @@ class QuinticMotionPredicatesCreator:
         self.sT_grid = sT_grid
         self.vT_grid = vT_grid
         self.T_m = T_m
+        self.T_safety = T_safety
 
-        self.predicates_resources_target_directory = predicates_resources_target_directory # 'predicates'
+        self.predicates_resources_target_directory = predicates_resources_target_directory  # 'predicates'
         self.predicate = np.full(shape=[len(v0_grid), len(a0_grid), len(sT_grid), len(vT_grid)],
-                        fill_value=False)
+                                 fill_value=False)
 
     @staticmethod
     def create_quintic_motion_funcs(a_0, v_0, v_T, s_T, T, T_m):
@@ -103,7 +107,7 @@ class QuinticMotionPredicatesCreator:
         :param v_T: desired velocity [m/s^2]
         :param s_T: initial distance from target [m]
         :param T: action_time [s]
-        :param T_m: safety time behind or ahead of target vehicle [s]
+        :param T_m: Specification margin time interval behind or ahead of target vehicle [s]
         :return: lambda functions of distance_from_target, the coefficients of the derivative polynomial of distance
          from target, velocity and acceleration w.r.t time (valid in the range [0,T])
         """
@@ -113,14 +117,28 @@ class QuinticMotionPredicatesCreator:
                QuinticPoly1D.acceleration_profile_function(a_0, v_0, v_T, s_T, T, T_m)
 
     @staticmethod
-    def generate_predicate_value(action_type, w_T, w_J, a_0, v_0, v_T, s_T, T_m):
+    def generate_predicate_value(action_type, w_T, w_J, a_0, v_0, v_T, s_T, T_m, T_safety):
+        """
+        Generates the actual predicate value (true/false) for the given action,weights and scenario params
+        :param action_type:
+        :param w_T: weight of Time component in time-jerk cost function
+        :param w_J: weight of longitudinal jerk component in time-jerk cost function
+        :param a_0: initial acceleration [m/s^2]
+        :param v_0: initial velocity [m/s]
+        :param v_T: desired final velocity [m/s]
+        :param s_T: initial distance from target car (+/- constant safety margin) [m]
+        :param T_m: specification margin from target vehicle [s]
+        :param T_safety: safety margin from target vehicle [s]
+        :return: True if given parameters will generate a feasible trajectory that meets time, velocity and
+                acceleration constraints and doesn't get into target vehicle safety zone.
+        """
         time_cost_poly_coefs = \
             QuinticPoly1D.time_cost_function_derivative_coefs(np.array([w_T]), np.array([w_J]),
                                                               np.array([a_0]), np.array([v_0]),
                                                               np.array([v_T]), np.array([s_T]),
                                                               np.array([T_m]))[0]
         cost_roots_reals = Math.find_real_roots_in_limits(time_cost_poly_coefs, np.array(
-            [EPS, BP_ACTION_T_LIMITS[1]]))
+            [0, BP_ACTION_T_LIMITS[1]]))
         extremum_T = cost_roots_reals[np.isfinite(cost_roots_reals)]
 
         if len(extremum_T) == 0:
@@ -128,25 +146,32 @@ class QuinticMotionPredicatesCreator:
 
         T = extremum_T.min()  # First extrema is our local (and sometimes global) minimum
 
-        delta_s_t_func, coefs_s_der, v_t_func, a_t_func = QuinticMotionPredicatesCreator.create_quintic_motion_funcs(a_0, v_0,
-                                                                                      v_T, s_T,
-                                                                                      T,
-                                                                                      T_m=T_m)
+        # Handling the case of an action where we'd like to continue doing what we're doing, so action time is zero
+        # or very small and gets quantized to zero.
+        if T == 0:
+            return True
+
+        delta_s_t_func, coefs_s_der, v_t_func, a_t_func = QuinticMotionPredicatesCreator.create_quintic_motion_funcs(
+            a_0, v_0,
+            v_T, s_T,
+            T,
+            T_m=T_m)
         time_res_for_extremum_query = 0.01
-        s_roots_reals = Math.find_real_roots_in_limits(coefs_s_der, np.array([time_res_for_extremum_query, T-time_res_for_extremum_query]))
+        s_roots_reals = Math.find_real_roots_in_limits(coefs_s_der, np.array(
+            [time_res_for_extremum_query, T - time_res_for_extremum_query]))
         extremum_delta_s_val = delta_s_t_func(s_roots_reals[np.isfinite(s_roots_reals)])
 
-        t = np.arange(0, T, time_res_for_extremum_query)
+        t = np.arange(0, T + EPS, time_res_for_extremum_query)
         min_v, max_v = min(v_t_func(t)), max(v_t_func(t))
         min_a, max_a = min(a_t_func(t)), max(a_t_func(t))
 
-        is_T_in_range = (T >= EPS) and (T <= BP_ACTION_T_LIMITS[1] + EPS)
+        is_T_in_range = (T <= BP_ACTION_T_LIMITS[1] + EPS)
         is_vel_in_range = (min_v >= VELOCITY_LIMITS[0] - EPS) and (max_v <= VELOCITY_LIMITS[1] + EPS)
         is_acc_in_range = (min_a >= LON_ACC_LIMITS[0] - EPS) and (max_a <= LON_ACC_LIMITS[1] + EPS)
         if action_type == ActionType.FOLLOW_VEHICLE:
-            is_dist_safe = np.all(extremum_delta_s_val >= T_m * v_T)
+            is_dist_safe = np.all(extremum_delta_s_val >= T_safety * v_T)
         elif action_type == ActionType.OVERTAKE_VEHICLE:
-            is_dist_safe = np.all(extremum_delta_s_val <= T_m * v_T)
+            is_dist_safe = np.all(extremum_delta_s_val <= T_safety * v_T)
         else:
             is_dist_safe = True
 
@@ -163,6 +188,7 @@ class QuinticMotionPredicatesCreator:
         for action_type in action_types:
             margin_sign = +1 if action_type == ActionType.FOLLOW_VEHICLE else -1
             T_m = self.T_m * margin_sign
+            T_safety = self.T_safety * margin_sign
             sT_grid = margin_sign * self.sT_grid.array
             for weight in jerk_time_weights:
                 w_J, w_T = weight[0], weight[2]
@@ -174,9 +200,10 @@ class QuinticMotionPredicatesCreator:
                             for j, v_T in enumerate(self.vT_grid):
                                 self.predicate[k, m, i, j] = \
                                     QuinticMotionPredicatesCreator.generate_predicate_value(
-                                        action_type, w_T, w_J, a_0, v_0, v_T, s_T, T_m)
+                                        action_type, w_T, w_J, a_0, v_0, v_T, s_T, T_m, T_safety)
 
                 output_predicate_file_name = '%s_predicate_wT_%.2f_wJ_%.2f.bin' % (action_type.name.lower(), w_T, w_J)
-                output_predicate_file_path = Paths.get_resource_absolute_path_filename('%s/%s' % (self.predicates_resources_target_directory,
-                                                                                                  output_predicate_file_name))
+                output_predicate_file_path = Paths.get_resource_absolute_path_filename(
+                    '%s/%s' % (self.predicates_resources_target_directory,
+                               output_predicate_file_name))
                 BinaryReadWrite.save(array=self.predicate, file_path=output_predicate_file_path)
