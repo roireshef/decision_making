2e4a2c863ff3e34821b05b3f221a6f5c046d1516
liat <liat.sless@gm.com>
2018-05-31 14:43:23 +0300

    Added new predictor and tests

--- src/prediction/utils/prediction_utils.py
+++ src/prediction/utils/prediction_utils.py
@@ -1,14 +1,67 @@
 from typing import List
 import numpy as np
 
-from decision_making.src.global_constants import PREDICTION_LOOKAHEAD_COMPENSATION_RATIO
-from decision_making.src.planning.types import CartesianTrajectory, CartesianPath2D
+from decision_making.src.global_constants import PREDICTION_LOOKAHEAD_COMPENSATION_RATIO, WERLING_TIME_RESOLUTION
+from decision_making.src.planning.types import CartesianTrajectory, CartesianPath2D, FS_SV, FS_SX
+from decision_making.src.prediction.ego_aware_prediction.ended_maneuver_params import EndedManeuverParams
+from decision_making.src.prediction.ego_aware_prediction.maneuver_spec import ManeuverSpec
 from decision_making.src.state.state import DynamicObject
+from decision_making.src.state.state_utils import get_object_fstate
 from mapping.src.service.map_service import MapService
 from mapping.src.transformations.geometry_utils import CartesianFrame
 
 
 class PredictionUtils:
+    @staticmethod
+    def convert_to_maneuver_spec(object_state: DynamicObject,
+                                 ended_maneuver_params: EndedManeuverParams) -> ManeuverSpec:
+
+        """
+        Converts the parameters of the maneuver to a complete maneuver spec
+        :param object_state: the dynamic object to predict
+        :param ended_maneuver_params: the maneuver parameters to be converted to maneuver spec
+        """
+
+        assert -0.5 <= ended_maneuver_params.lat_normalized <= 0.5
+
+        map_api = MapService.get_instance()
+        road_id = object_state.road_localization.road_id
+
+        # Get Object's Frenet frame
+        road_frenet = map_api.get_road_center_frenet_frame(road_id=road_id)
+
+        # Object's initial state in Frenet frame
+        obj_init_fstate = get_object_fstate(object_state=object_state, frenet_frame=road_frenet)
+
+        # Calculate object's initial state in Frenet frame according to model
+        road_center_lanes_lat = map_api.get_center_lanes_latitudes(road_id=road_id)
+        object_center_lane_latitude = road_center_lanes_lat[object_state.road_localization.lane_num]
+        lane_width = map_api.get_road(road_id=road_id).lane_width
+        num_lanes = map_api.get_road(road_id=road_id).lanes_num
+        road_width = lane_width * num_lanes
+
+        # Motion model (in Frenet frame)
+        t_axis = np.arange(0.0, ended_maneuver_params.T_s + 10 * np.finfo(float).eps, WERLING_TIME_RESOLUTION)
+        # Calculate velocity according to average acceleration
+        s_v_vec = obj_init_fstate[FS_SV] + ended_maneuver_params.avg_s_a * t_axis
+        # Clip negative velocities if starting velocity is positive
+        if obj_init_fstate[FS_SV] > 0:
+            s_v_vec = np.clip(s_v_vec, 0.0, np.inf)
+        s_x_vec = obj_init_fstate[FS_SX] + np.cumsum(s_v_vec * WERLING_TIME_RESOLUTION)
+
+        s_x_final = s_x_vec[-1]
+        s_v_final = s_v_vec[-1]
+        s_a_final = ended_maneuver_params.s_a_final
+        d_x_final = (-road_width / 2.0 + object_center_lane_latitude) + lane_width * (
+        ended_maneuver_params.relative_lane + ended_maneuver_params.lat_normalized)
+        d_v_final = 0.0
+        d_a_final = 0.0
+
+        obj_final_fstate = np.array([s_x_final, s_v_final, s_a_final, d_x_final, d_v_final, d_a_final])
+
+        return ManeuverSpec(init_state=obj_init_fstate, final_state=obj_final_fstate, T_s=ended_maneuver_params.T_s,
+                            T_d=ended_maneuver_params.T_s)
+
     @staticmethod
     def convert_ctrajectory_to_dynamic_objects(dynamic_object: DynamicObject, predictions: CartesianTrajectory,
                                                prediction_timestamps: np.ndarray) -> List[DynamicObject]:
