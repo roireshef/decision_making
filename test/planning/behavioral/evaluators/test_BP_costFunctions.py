bbb1135cffb188a299d0ef5c35e78adb69914677
mz8cj6 <vlad.goldner@gm.com>
2018-06-04 16:23:33 +0300

    comfort cost tests

--- test/planning/behavioral/evaluators/test_BP_costFunctions.py
+++ test/planning/behavioral/evaluators/test_BP_costFunctions.py
@@ -45,15 +45,33 @@ def test_calcEfficiencyCost_differentDeviationsFromDesiredVel_costsComplyEfficie
     assert eff_cost5 == 2*eff_cost3
 
 
-def test_calcComfortCost():
+def test_calcComfortCost_differentMaxApproxLatTimeAndLonDist_maxTimeImposesHighJerk():
+    """
+    Test lateral and longitudinal comfort costs:
+        verify that T_d_max imposes the same high jerk for different T_d_approx
+        if T_d_max is not imposed, the lateral jerk is lower, although the approximated T_d is the same
+        increasing T_d_approx decreases lateral jerk
+        affect of spec.s on the longitudinal jerk
+    """
     ego_fstate = np.array([0, 0, 0, 0, 0, 0])
-    spec = ActionSpec(t=10, v=20, s=100, d=3.5)
+    spec = ActionSpec(t=10, v=15, s=100, d=3.5)
     T_d_max = 3
+    # verify that T_d_max imposes the same high jerk for different T_d_approx
     lon_cost1, lat_cost1 = BP_CostFunctions.calc_comfort_cost(ego_fstate, spec, T_d_max, T_d_approx=T_d_max)
     lon_cost2, lat_cost2 = BP_CostFunctions.calc_comfort_cost(ego_fstate, spec, T_d_max, T_d_approx=T_d_max+3)
     lon_cost3, lat_cost3 = BP_CostFunctions.calc_comfort_cost(ego_fstate, spec, T_d_max, T_d_approx=T_d_max-1)
     assert lat_cost1 == lat_cost2 == lat_cost3 and lon_cost1 == lon_cost2 == lon_cost3
 
     # if T_d_max is not imposed, the lateral jerk is lower, although the approximated T_d is the same
-    lon_cost4, lat_cost4 = BP_CostFunctions.calc_comfort_cost(ego_fstate, spec, np.inf, T_d_approx=T_d_max)
-    assert lat_cost1 > lat_cost4 and lon_cost1 == lon_cost4
+    _, lat_cost4 = BP_CostFunctions.calc_comfort_cost(ego_fstate, spec, T_d_max=10, T_d_approx=T_d_max)
+    assert lat_cost1 > lat_cost4
+
+    # increasing T_d_approx decreases lateral jerk
+    _, lat_cost5 = BP_CostFunctions.calc_comfort_cost(ego_fstate, spec, T_d_max=10, T_d_approx=T_d_max+1)
+    assert lat_cost5 < lat_cost4
+
+    # in our case increase of spec.s increases longitudinal jerk
+    spec_longer_s = copy.deepcopy(spec)
+    spec_longer_s.s += 10
+    lon_cost6, lat_cost6 = BP_CostFunctions.calc_comfort_cost(ego_fstate, spec_longer_s, T_d_max, T_d_approx=T_d_max)
+    assert lon_cost6 > lon_cost1 and lat_cost6 == lat_cost1
