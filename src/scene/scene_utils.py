56eeb9268214daed0d72f40ea60ed09aafffd609
RoiReshef <roi.reshef@gm.com>
2018-10-10 12:47:16 +0300

    made use in numpy conversions of coordinate classes

--- src/scene/scene_utils.py
+++ src/scene/scene_utils.py
@@ -20,18 +20,8 @@ class SceneUtils:
         road_id = scene.host_localization.lane_segment_id
         obj_id = 0
         timestamp = DynamicObject.sec_to_ticks(scene.timestamp_sec)
-        cartesian_state = np.array([scene.host_localization.s_cartesian_localization.east_x,
-                                    scene.host_localization.s_cartesian_localization.north_y,
-                                    scene.host_localization.s_cartesian_localization.heading,
-                                    scene.host_localization.s_cartesian_localization.velocity_longitudinal,
-                                    scene.host_localization.s_cartesian_localization.acceleration_longitudinal,
-                                    scene.host_localization.s_cartesian_localization.curvature])
-        road_fstate = np.array([scene.host_localization.s_lane_frenet_coordinate.s,
-                                scene.host_localization.s_lane_frenet_coordinate.s_dot,
-                                scene.host_localization.s_lane_frenet_coordinate.s_dotdot,
-                                scene.host_localization.s_lane_frenet_coordinate.d,
-                                scene.host_localization.s_lane_frenet_coordinate.d_dot,
-                                scene.host_localization.s_lane_frenet_coordinate.d_dotdot])
+        cartesian_state = scene.host_localization.s_cartesian_localization.numpy()
+        road_fstate = scene.host_localization.s_lane_frenet_coordinate.numpy()
         size = ObjectSize(EGO_LENGTH, EGO_WIDTH, EGO_HEIGHT)
         confidence = 1.0
 
@@ -39,18 +29,8 @@ class SceneUtils:
 
         dynamic_objects = []
         for object_loc in scene.object_localizations:
-            cartesian_state = np.array([object_loc.s_object_hypotheses[0].s_cartesian_localization.east_x,
-                                        object_loc.s_object_hypotheses[0].s_cartesian_localization.north_y,
-                                        object_loc.s_object_hypotheses[0].s_cartesian_localization.heading,
-                                        object_loc.s_object_hypotheses[0].s_cartesian_localization.velocity_longitudinal,
-                                        object_loc.s_object_hypotheses[0].s_cartesian_localization.acceleration_longitudinal,
-                                        object_loc.s_object_hypotheses[0].s_cartesian_localization.curvature])
-            road_fstate = np.array([object_loc.s_object_hypotheses[0].s_lane_frenet_coordinate.s,
-                                    object_loc.s_object_hypotheses[0].s_lane_frenet_coordinate.s_dot,
-                                    object_loc.s_object_hypotheses[0].s_lane_frenet_coordinate.s_dotdot,
-                                    object_loc.s_object_hypotheses[0].s_lane_frenet_coordinate.d,
-                                    object_loc.s_object_hypotheses[0].s_lane_frenet_coordinate.d_dot,
-                                    object_loc.s_object_hypotheses[0].s_lane_frenet_coordinate.d_dotdot])
+            cartesian_state = object_loc.s_object_hypotheses[0].s_cartesian_localization.numpy()
+            road_fstate = object_loc.s_object_hypotheses[0].s_lane_frenet_coordinate.numpy()
             size = size
             confidence = 1.0
             dynamic_objects.append(DynamicObject(object_loc.object_id, timestamp, cartesian_state,
@@ -64,46 +44,26 @@ class SceneUtils:
 
         # TODO: This logic assumes there is only one hypothesis for an object's location
         if obj_id == 0:
-            return np.array([scene.host_localization.s_cartesian_localization.east_x,
-                             scene.host_localization.s_cartesian_localization.north_y,
-                             scene.host_localization.s_cartesian_localization.heading,
-                             scene.host_localization.s_cartesian_localization.velocity_longitudinal,
-                             scene.host_localization.s_cartesian_localization.acceleration_longitudinal,
-                             scene.host_localization.s_cartesian_localization.curvature])
+            return scene.host_localization.s_cartesian_localization.numpy()
         else:
 
             desired_object_loc = [object_loc for object_loc in scene.object_localizations
                                   if object_loc.object_id == obj_id]
             assert len(desired_object_loc) == 1
-            return np.array([desired_object_loc[0].s_object_hypotheses[0].s_cartesian_localization.east_x,
-                             desired_object_loc[0].s_object_hypotheses[0].s_cartesian_localization.north_y,
-                             desired_object_loc[0].s_object_hypotheses[0].s_cartesian_localization.heading,
-                             desired_object_loc[0].s_object_hypotheses[0].s_cartesian_localization.velocity_longitudinal,
-                             desired_object_loc[0].s_object_hypotheses[0].s_cartesian_localization.acceleration_longitudinal,
-                             desired_object_loc[0].s_object_hypotheses[0].s_cartesian_localization.curvature])
+            return desired_object_loc[0].s_object_hypotheses[0].s_cartesian_localization.numpy()
 
     @staticmethod
     def get_mapstate_from_scene(scene: SceneMessage, obj_id: int) -> MapState:
 
         if obj_id == 0:
-            fstate = np.array([scene.host_localization.s_lane_frenet_coordinate.s,
-                               scene.host_localization.s_lane_frenet_coordinate.s_dot,
-                               scene.host_localization.s_lane_frenet_coordinate.s_dotdot,
-                               scene.host_localization.s_lane_frenet_coordinate.d,
-                               scene.host_localization.s_lane_frenet_coordinate.d_dot,
-                               scene.host_localization.s_lane_frenet_coordinate.d_dotdot])
+            fstate = scene.host_localization.s_lane_frenet_coordinate.numpy()
             return MapState(fstate, scene.host_localization.lane_segment_id)
         else:
 
             desired_object_loc = [object_loc for object_loc in scene.object_localizations
                                   if object_loc.object_id == obj_id]
             assert len(desired_object_loc) == 1
-            fstate = np.array([desired_object_loc[0].s_object_hypotheses[0].s_lane_frenet_coordinate.s,
-                               desired_object_loc[0].s_object_hypotheses[0].s_lane_frenet_coordinate.s_dot,
-                               desired_object_loc[0].s_object_hypotheses[0].s_lane_frenet_coordinate.s_dotdot,
-                               desired_object_loc[0].s_object_hypotheses[0].s_lane_frenet_coordinate.d,
-                               desired_object_loc[0].s_object_hypotheses[0].s_lane_frenet_coordinate.d_dot,
-                               desired_object_loc[0].s_object_hypotheses[0].s_lane_frenet_coordinate.d_dotdot])
+            fstate = desired_object_loc[0].s_object_hypotheses[0].s_lane_frenet_coordinate.numpy()
             return MapState(fstate, desired_object_loc[0].s_lane_frenet_coordinate.lane_segment_id)
 
     @staticmethod
