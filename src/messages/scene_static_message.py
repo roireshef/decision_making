9c469c655fb88c4472cbde06b49ac5d7e2879826
Gabi <gabi.guetta@gm.com>
2018-11-05 17:43:38 +0200

    fixed scene_static array definition and added type hints

--- src/messages/scene_static_message.py
+++ src/messages/scene_static_message.py
@@ -2,6 +2,7 @@ from enum import Enum
 from typing import List
 
 import numpy as np
+from gm_lcm import LcmNumpyArray
 
 from Rte_Types.sub_structures import TsSYSAdjacentLane, TsSYSLaneManeuver, TsSYSBoundaryPoint, TsSYSLaneCoupling, \
     TsSYSNominalPathPoint, TsSYSStaticTrafficFlowControl, TsSYSDynamicStatus, TsSYSDynamicTrafficFlowControl, \
@@ -37,10 +38,10 @@ class MapLaneType(Enum):
 
 
 class MapRoadSegmentType(Enum):
-   e_MapRoadType_Normal = 0,
-   e_MapRoadType_Intersection = 1,
-   e_MapRoadType_TurnOnly = 2,
-   e_MapRoadType_Unknown = 3
+   Normal = 0,
+   Intersection = 1,
+   TurnOnly = 2,
+   Unknown = 3
 
 
 class MovingDirection(Enum):
@@ -96,21 +97,97 @@ class MapLaneMarkerType(Enum):
     MapLaneMarkerType_Unknown                                      = 255
 
 
+
+class RoadObjectType(Enum):
+
+    Yield                                               =  0
+    StopSign                                            =  1
+    VerticalStack_RYG_TrafficLight                      =  2
+    HorizontalStack_RYG_TrafficLight                    =  3
+    YieldVerticalStack_RYG_TrafficLight_WithInd         =  4
+    HorizontalStack_RYG_TrafficLight_WithInd            =  5
+    Red_TrafficLight                                    =  6
+    Yellow_TrafficLight                                 =  7
+    StopBar_Left                                        =  8
+    StopBar_Right                                       =  9
+    LaneLeftTurnMarker_Start                            =  10
+    LaneLeftTurnMarker_End                              =  11
+    LaneRightTurnMarker_Start                           =  12
+    LaneRightTurnMarker_End                             =  13
+    LaneStraightLeftTurnMarker_Start                    =  14
+    LaneStraightLeftTurnMarker_End                      =  15
+    LaneStraightRightTurnMarker_Start                   =  16
+    LaneStraightRightTurnMarker_End                     =  17
+    LaneStopMarker_MidLeft                              =  18
+    LaneStopMarker_MidRight                             =  19
+    LaneStopMarker_Diamond_Start                        =  20
+    LaneStopMarker_Diamond_end                          =  21
+    LightPost                                           =  22
+    Dashed_LaneMarker_MidStart                          =  23
+    Dashed_LaneMarker_MidEnd                            =  24
+
+
+class TrafficSignalState(Enum):
+
+    NO_DETECTION                 = 0
+    RED                          = 1
+    YELLOW                       = 2
+    RED_YELLOW                   = 3
+    GREEN                        = 4
+    GREEN_YELLOW                 = 6
+    FLASHING_BIT                 = 8  # (flashing modifier, not valid by itself)
+    FLASHING_RED                 = 9
+    FLASHING_YELLOW              = 10
+    FLASHING_GREEN               = 12
+    UP_DOWN_ARROW_BIT            = 16 # (up/down arrow modifier, not valid by itself)
+    STRAIGHT_ARROW_RED           = 17
+    STRAIGHT_ARROW_YELLOW        = 18
+    STRAIGHT_ARROW_GREEN         = 20
+    LEFT_ARROW_BIT               = 32 # (left arrow modifier, not valid by itself)
+    LEFT_ARROW_RED               = 33
+    LEFT_ARROW_YELLOW            = 34
+    LEFT_ARROW_GREEN             = 36
+    FLASHING_LEFT_ARROW_RED      = 41
+    FLASHING_LEFT_ARROW_YELLOW   = 42
+    FLASHING_LEFT_ARROW_GREEN    = 44
+    RIGHT_ARROW_BIT              = 64 # (right arrow modifier, not valid by itself)
+    RIGHT_ARROW_RED              = 65
+    RIGHT_ARROW_YELLOW           = 66
+    RIGHT_ARROW_GREEN            = 68
+    FLASHING_RIGHT_ARROW_RED     = 73
+    FLASHING_RIGHT_ARROW_YELLOW  = 74
+    FLASHING_RIGHT_ARROW_GREEN   = 76
+    RAILROAD_CROSSING_UNKNOWN    = 96
+    RAILROAD_CROSSING_RED        = 97
+    RAILROAD_CROSSING_CLEAR      = 100
+    RAILROAD_CROSSING_BLOCKED    = 104
+    PEDISTRIAN_SAME_DIR_UNKNOWN   = 108
+    PEDISTRIAN_SAME_DIR_STOPPED   = 109
+    PEDISTRIAN_SAME_DIR_WARNING   = 110
+    PEDISTRIAN_SAME_DIR_CROSS     = 112
+    PEDISTRIAN_PERP_DIR_UNKNOWN   = 116
+    PEDISTRIAN_PERP_DIR_STOPPED   = 117
+    PEDISTRIAN_PERP_DIR_WARNING   = 118
+    PEDISTRIAN_PERP_DIR_CROSS     = 120
+    UNKNOWN                       = 121
+    OFF                           = 122
+
+
 class SceneRoadSegment(PUBSUB_MSG_IMPL):
     e_Cnt_road_segment_id = int
     e_Cnt_road_id = int
     e_Cnt_lane_segment_id_count = int
-    a_Cnt_lane_segment_id = List[int]
+    a_Cnt_lane_segment_id = np.ndarray
     e_e_road_segment_type = MapRoadSegmentType
     e_Cnt_upstream_segment_count = int
-    a_Cnt_upstream_road_segment_id = List[int]
+    a_Cnt_upstream_road_segment_id = np.ndarray
     e_Cnt_downstream_segment_count = int
-    a_Cnt_downstream_road_segment_id = List[int]
+    a_Cnt_downstream_road_segment_id = np.ndarray
 
     def __init__(self, e_Cnt_road_segment_id, e_Cnt_road_id, e_Cnt_lane_segment_id_count, a_Cnt_lane_segment_id,
                  e_e_road_segment_type, e_Cnt_upstream_segment_count, a_Cnt_upstream_road_segment_id,
                  e_Cnt_downstream_segment_count, a_Cnt_downstream_road_segment_id):
-        # type: (int, int, int, List[int], MapRoadSegmentType, int, List[int], int, List[int]) -> None
+        # type: (int, int, int, np.ndarray, MapRoadSegmentType, int, np.ndarray, int, np.ndarray) -> None
         """
         Road-segment information
         :param e_Cnt_road_segment_id: ID of this Road-segment
@@ -191,13 +268,13 @@ class SceneRoadSegment(PUBSUB_MSG_IMPL):
 class SceneRoadIntersection(PUBSUB_MSG_IMPL):
     e_i_road_intersection_id = int
     e_Cnt_lane_coupling_count = int
-    a_i_lane_coupling_segment_ids = List[int]
+    a_i_lane_coupling_segment_ids = np.ndarray
     e_Cnt_intersection_road_segment_count = int
-    a_i_intersection_road_segment_ids = List[int]
+    a_i_intersection_road_segment_ids = np.ndarray
 
     def __init__(self, e_i_road_intersection_id, e_Cnt_lane_coupling_count, a_i_lane_coupling_segment_ids,
                  e_Cnt_intersection_road_segment_count, a_i_intersection_road_segment_ids):
-        # type (int, int, List[int], int, List[int]) -> None
+        # type: (int, int, np.ndarray, int, np.ndarray) -> None
         """
         Road-intersection information
         :param e_i_road_intersection_id: ID of this road-intersection
@@ -250,81 +327,6 @@ class SceneRoadIntersection(PUBSUB_MSG_IMPL):
                                           :pubsubMsg.a_i_intersection_road_segment_ids.num_dimensions])
                               , buffer=np.array(pubsubMsg.a_i_intersection_road_segment_ids.data)
                               , dtype=int))
-
-
-class RoadObjectType(Enum):
-
-    Yield                                               =  0
-    StopSign                                            =  1
-    VerticalStack_RYG_TrafficLight                      =  2
-    HorizontalStack_RYG_TrafficLight                    =  3
-    YieldVerticalStack_RYG_TrafficLight_WithInd         =  4
-    HorizontalStack_RYG_TrafficLight_WithInd            =  5
-    Red_TrafficLight                                    =  6
-    Yellow_TrafficLight                                 =  7
-    StopBar_Left                                        =  8
-    StopBar_Right                                       =  9
-    LaneLeftTurnMarker_Start                            =  10
-    LaneLeftTurnMarker_End                              =  11
-    LaneRightTurnMarker_Start                           =  12
-    LaneRightTurnMarker_End                             =  13
-    LaneStraightLeftTurnMarker_Start                    =  14
-    LaneStraightLeftTurnMarker_End                      =  15
-    LaneStraightRightTurnMarker_Start                   =  16
-    LaneStraightRightTurnMarker_End                     =  17
-    LaneStopMarker_MidLeft                              =  18
-    LaneStopMarker_MidRight                             =  19
-    LaneStopMarker_Diamond_Start                        =  20
-    LaneStopMarker_Diamond_end                          =  21
-    LightPost                                           =  22
-    Dashed_LaneMarker_MidStart                          =  23
-    Dashed_LaneMarker_MidEnd                            =  24
-
-
-class TrafficSignalState(Enum):
-
-    NO_DETECTION                 = 0
-    RED                          = 1
-    YELLOW                       = 2
-    RED_YELLOW                   = 3
-    GREEN                        = 4
-    GREEN_YELLOW                 = 6
-    FLASHING_BIT                 = 8  # (flashing modifier, not valid by itself)
-    FLASHING_RED                 = 9
-    FLASHING_YELLOW              = 10
-    FLASHING_GREEN               = 12
-    UP_DOWN_ARROW_BIT            = 16 # (up/down arrow modifier, not valid by itself)
-    STRAIGHT_ARROW_RED           = 17
-    STRAIGHT_ARROW_YELLOW        = 18
-    STRAIGHT_ARROW_GREEN         = 20
-    LEFT_ARROW_BIT               = 32 # (left arrow modifier, not valid by itself)
-    LEFT_ARROW_RED               = 33
-    LEFT_ARROW_YELLOW            = 34
-    LEFT_ARROW_GREEN             = 36
-    FLASHING_LEFT_ARROW_RED      = 41
-    FLASHING_LEFT_ARROW_YELLOW   = 42
-    FLASHING_LEFT_ARROW_GREEN    = 44
-    RIGHT_ARROW_BIT              = 64 # (right arrow modifier, not valid by itself)
-    RIGHT_ARROW_RED              = 65
-    RIGHT_ARROW_YELLOW           = 66
-    RIGHT_ARROW_GREEN            = 68
-    FLASHING_RIGHT_ARROW_RED     = 73
-    FLASHING_RIGHT_ARROW_YELLOW  = 74
-    FLASHING_RIGHT_ARROW_GREEN   = 76
-    RAILROAD_CROSSING_UNKNOWN    = 96
-    RAILROAD_CROSSING_RED        = 97
-    RAILROAD_CROSSING_CLEAR      = 100
-    RAILROAD_CROSSING_BLOCKED    = 104
-    PEDISTRIAN_SAME_DIR_UNKNOWN   = 108
-    PEDISTRIAN_SAME_DIR_STOPPED   = 109
-    PEDISTRIAN_SAME_DIR_WARNING   = 110
-    PEDISTRIAN_SAME_DIR_CROSS     = 112
-    PEDISTRIAN_PERP_DIR_UNKNOWN   = 116
-    PEDISTRIAN_PERP_DIR_STOPPED   = 117
-    PEDISTRIAN_PERP_DIR_WARNING   = 118
-    PEDISTRIAN_PERP_DIR_CROSS     = 120
-    UNKNOWN                       = 121
-    OFF                           = 122
 
 
 class AdjacentLane(PUBSUB_MSG_IMPL):
@@ -658,6 +660,7 @@ class SceneLaneSegment(PUBSUB_MSG_IMPL):
                  e_Cnt_left_boundary_points_count, as_left_boundary_points, e_Cnt_right_boundary_points_count,
                  as_right_boundary_points, e_i_downstream_road_intersection_id, e_Cnt_lane_coupling_count,
                  as_lane_coupling):
+        # type: (int, int, MapLaneType, int, List[StaticTrafficFlowControl], int, List[DynamicTrafficFlowControl], int, List[AdjacentLane], int, List[AdjacentLane], int, List[LaneManeuver], int, List[LaneManeuver], float, int, List[NominalPathPoint], int, List[BoundaryPoint], int, List[BoundaryPoint], int, int, List[LaneCoupling]) -> None
         """
         Lane-segment information
         :param e_i_lane_segment_id: ID of this lane-segment
@@ -779,15 +782,6 @@ class SceneLaneSegment(PUBSUB_MSG_IMPL):
     @classmethod
     def deserialize(cls, pubsubMsg):
         # type: (TsSYSSceneLaneSegment) -> SceneLaneSegment
-
-
-
-        pubsub_msg.as_lane_coupling = list()
-        for i in range(pubsub_msg.e_Cnt_lane_coupling_count):
-            pubsub_msg.as_lane_coupling.append(self.as_lane_coupling[i].serialize())
-
-
-
 
         as_static_traffic_flow_control = list()
         for i in range(pubsubMsg.e_Cnt_static_traffic_flow_control_count):
