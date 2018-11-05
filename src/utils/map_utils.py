97430f9d14aa05ac303f0d1c0d1fb536910361b0
mz8cj6 <vlad.goldner@gm.com>
2018-11-05 09:24:02 +0200

    implement lane-based _get_lookahead_points in MapUtils

--- src/utils/map_utils.py
+++ src/utils/map_utils.py
@@ -7,6 +7,7 @@ from decision_making.src.messages.navigation_plan_message import NavigationPlanM
 from decision_making.src.planning.behavioral.data_objects import RelativeLane
 from decision_making.src.planning.types import FP_DX, FP_SX, C_X, C_Y
 from decision_making.src.planning.utils.frenet_serret_frame import FrenetSerret2DFrame
+from mapping.src.exceptions import raises, LongitudeOutOfRoad, RoadNotFound
 from mapping.src.service.map_service import MapService
 from mapping.src.transformations.geometry_utils import CartesianFrame
 
@@ -75,7 +76,6 @@ class MapUtils:
         map_api = MapService.get_instance()
         if road_segment_id is None:
             # find the closest road segment
-            map_api = MapService.get_instance()
             relevant_road_ids = map_api._find_roads_containing_point(x, y)
             closest_road_id = map_api._find_closest_road(x, y, relevant_road_ids)
         else:
@@ -147,18 +147,9 @@ class MapUtils:
             lookahead_dist += starting_lon
             starting_lon = 0
 
-        map_api = MapService.get_instance()
-        road_segment_id = MapUtils.get_road_segment_by_lane(lane_id)
-        # convert starting lane fpoint to starting road fpoint
-        starting_cpoint = MapUtils.get_lane_frenet_frame(lane_id).fpoint_to_cpoint(np.array([starting_lon, 0]))
-        starting_road_fpoint = map_api._rhs_roads_frenet[road_segment_id].cpoint_to_fpoint(starting_cpoint)
-        # use old get_lookahead_points by the road coordinates
-        shifted, _ = map_api.get_lookahead_points(initial_road_id=road_segment_id,
-                                                  initial_lon=starting_road_fpoint[FP_SX],
-                                                  lookahead_dist=lookahead_dist,
-                                                  desired_lat=starting_road_fpoint[FP_DX],
-                                                  navigation_plan=navigation_plan)
-        # TODO change to precise resampling
+        shifted, _ = MapUtils._get_lookahead_points(lane_id, starting_lon, lookahead_dist, desired_lat=0,
+                                                    navigation_plan=navigation_plan)
+        # TODO: change to precise resampling
         _, resampled, _ = CartesianFrame.resample_curve(curve=shifted, step_size=TRAJECTORY_ARCLEN_RESOLUTION)
 
         center_lane_reference_route = FrenetSerret2DFrame.fit(resampled)
@@ -244,3 +235,110 @@ class MapUtils:
         is_on_road = bool(-dist_to_right_border <= fpoint[FP_DX] <= dist_to_left_border)
 
         return lane_id, fpoint[FP_SX], fpoint[FP_DX], relative_yaw, is_on_road
+
+    @staticmethod
+    @raises(RoadNotFound, LongitudeOutOfRoad)
+    def _get_lookahead_points(initial_lane_id: int, initial_lon: float, lookahead_dist: float, desired_lat: float,
+                              navigation_plan: NavigationPlanMsg):
+        """
+        Given a longitude on specific road, return all the points along this (and next) road(s) until reaching
+        a lookahead of exactly <desired_lon> meters ahead. In addition, shift all points <desired_lat_shift> laterally,
+        relative to the roads right-side.
+        :param initial_lane_id: the initial lane_id (the vehicle is current on)
+        :param initial_lon: initial longitude along <initial_road_id>
+        :param lookahead_dist: the desired distance of lookahead in [m].
+        :param desired_lat: desired lateral shift of points **relative to lane center**
+        :param navigation_plan: the relevant navigation plan to iterate over its road IDs.
+        :return: a numpy array of points size Nx2, and the yaw of initial road longitude [rad]
+        """
+        map_api = MapService.get_instance()
+        # find the final point's (according to desired lookahead distance) road_id and longitude along this road
+        lane_ids, final_lon = MapUtils._advance_on_plan(initial_lane_id, initial_lon, lookahead_dist, navigation_plan)
+
+        # exact projection of the initial point and final point on the road
+        init_x, init_y, init_yaw = MapUtils.convert_from_lane_to_map_coordinates(initial_lane_id, initial_lon, desired_lat)
+        init_pos = np.array([init_x, init_y])
+        final_x, final_y, _ = MapUtils.convert_from_lane_to_map_coordinates(lane_ids[-1], final_lon, desired_lat)
+        final_pos = np.array([final_x, final_y])
+
+        # shift points (laterally) and concatenate all points of all relevant roads
+        shifted_points = np.concatenate([MapUtils._shift_lane_points_to_latitude(lid, desired_lat) for lid in lane_ids])
+
+        # calculate accumulate longitudinal distance for all points
+        longitudes = np.cumsum(np.concatenate([np.append([0], np.diff(map_api._longitudes[lid])) for lid in lane_ids]))
+
+        # trim shifted points from both sides according to initial point and final (desired) point
+        shifted_points = shifted_points[np.greater(longitudes - initial_lon, 0) &
+                                        np.less(longitudes - initial_lon, lookahead_dist)]
+
+        # Build path
+        path = np.concatenate(([init_pos], shifted_points, [final_pos]))
+
+        # Remove duplicate points (start of next road == end of last road)
+        path = path[np.append(np.sum(np.diff(path, axis=0), axis=1) != 0.0, [True])]
+
+        return path, init_yaw
+
+    @staticmethod
+    @raises(RoadNotFound, LongitudeOutOfRoad)
+    def _advance_on_plan(initial_lane_id: int, initial_lon: float, lookahead_dist: float,
+                         navigation_plan: NavigationPlanMsg) -> [np.array, float]:
+        """
+        Given a longitude on specific road (<initial_road_id> and <initial_lon>), advance (lookahead) <desired_lon>
+        distance. The lookahead iterates over the next roads specified in the <navigation_plan> and returns: (the final
+        road id, the longitude along this road). If <desired_lon> is more than the distance to end of the plan, a
+        LongitudeOutOfRoad exception is thrown.
+        :param initial_lane_id: the initial lane_id (the vehicle is current on)
+        :param initial_lon: initial longitude along <initial_lane_id>
+        :param lookahead_dist: the desired distance of lookahead in [m].
+        :param navigation_plan: the relevant navigation plan to iterate over its road IDs.
+        :return: (road_id, longitudinal distance [m] from the beginning of <road_id>)
+        """
+        initial_road_id = MapUtils.get_road_segment_by_lane(initial_lane_id)
+        current_road_idx_in_plan = navigation_plan.get_road_index_in_plan(initial_road_id)
+        roads_ids = navigation_plan.road_ids[current_road_idx_in_plan:]
+
+        # collect relevant lane_ids with their lengths
+        upstream_lanes = MapUtils.get_upstream_lanes(initial_lane_id)
+        lane_lengths = [MapUtils.get_lane_length(initial_lane_id)]
+        lane_ids = [initial_lane_id]
+        for road_id in roads_ids[1:]:
+            next_lane = [lid for lid in upstream_lanes if MapUtils.get_road_segment_by_lane(lid) == road_id]
+            if len(next_lane) < 1:
+                raise RoadNotFound("Upstream lane was not found in navigation plan")
+            lane_ids.append(next_lane[0])
+            lane_lengths.append(MapUtils.get_lane_length(next_lane[0]))
+            upstream_lanes = MapUtils.get_upstream_lanes(next_lane[0])
+
+        # distance to roads-ends
+        lanes_dist_to_end = np.cumsum(np.append([lane_lengths[0] - initial_lon], lane_lengths[1:]))
+        # how much of lookahead_dist is left after this road
+        lanes_leftovers = np.subtract(lookahead_dist, lanes_dist_to_end)
+
+        try:
+            target_lane_idx = np.where(lanes_leftovers < 0)[0][0]
+            return lane_ids[:(target_lane_idx+1)], lanes_leftovers[target_lane_idx] + lane_lengths[target_lane_idx]
+        except IndexError:
+            raise LongitudeOutOfRoad("The specified navigation plan is short {} meters to advance {} in longitude"
+                                     .format(lanes_leftovers[-1], lookahead_dist))
+
+    @staticmethod
+    def _shift_lane_points_to_latitude(lane_id: int, lateral_shift: float) -> np.array:
+        """
+        Given points list along a lane, shift them laterally by lat_shift [m]
+        :param lane_id: lane id
+        :param lateral_shift: shift in meters
+        :return: shifted points array (Nx2)
+        """
+        points = MapService.get_instance()._lane_points[lane_id]
+        # calculate direction unit vectors
+        points_direction = np.diff(points, axis=0)
+        norms = np.linalg.norm(points_direction, axis=1)[np.newaxis].T
+        norms[np.where(norms == 0.0)] = 1.0
+        direction_unit_vec = np.divide(points_direction, norms)
+        # calculate normal unit vectors
+        normal_unit_vec = np.c_[-direction_unit_vec[:, 1], direction_unit_vec[:, 0]]
+        normal_unit_vec = np.concatenate((normal_unit_vec, normal_unit_vec[-1, np.newaxis]))
+        # shift points by lateral_shift
+        shifted_points = points + normal_unit_vec * lateral_shift
+        return shifted_points
