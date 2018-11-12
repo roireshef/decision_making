ca5e61e3771b00a56b8eeac44f703d773fa12cbe
gzlncf <vladimir.novikov@gm.com>
2018-11-12 09:45:41 +0200

    Modified cmakes according to new directories structure

--- buildDefaults.cmake
+++ buildDefaults.cmake
@@ -1,9 +1,9 @@
-find_file(ROOT_APP buildDefaults.cmake ${REPOSITORY_HOME}/../application)
+find_file(ROOT_APP buildDefaults.cmake ${REPOSITORY_HOME}/..)
 	
 if (ROOT_APP)
 	# message(found root ${ROOT_APP})
 	set(DEF_ROOT ${ROOT_APP})
-	set(REPOSITORY_HOME ${REPOSITORY_HOME}/../application)
+	set(REPOSITORY_HOME ${REPOSITORY_HOME}/..)
 	include( ${ROOT_APP})
 	return()
 else()
