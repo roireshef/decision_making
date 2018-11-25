d516fe89b57004783f32fb5270a9d92ae32996dc
Gabi <gabi.guetta@gm.com>
2018-11-25 10:55:33 +0200

    -refactored constructor and properties -fixed indexing bug

--- src/planning/utils/generalized_frenet_serret_frame.py
+++ src/planning/utils/generalized_frenet_serret_frame.py
@@ -26,26 +26,13 @@ class FrenetSubSegment:
 
 class GeneralizedFrenetSerretFrame(FrenetSerret2DFrame):
     def __init__(self, points: CartesianPath2D, T: np.ndarray, N: np.ndarray, k: np.ndarray, k_tag: np.ndarray,
-                 sub_segments: List[FrenetSubSegment]):
+                 segments_id: np.ndarray, segments_s_offsets: np.ndarray, segments_ds: np.ndarray,
+                 segments_point_offset: np.ndarray):
         super().__init__(points, T, N, k, k_tag, None)
-        self.sub_segments = sub_segments
-
-    @property
-    def _segments_s_offsets(self):
-        """
-        :return: The accumulated longitudinal progress on the generalized frenet frame for each segment in self.sub_segments,
-        e.g.  segments_s_offsets[2] contains the length of (subsegment #0+ subsegment #1)
-        """
-        return np.insert(np.cumsum([seg.s_end - seg.s_start for seg in self.sub_segments]), 0, 0., axis=0)
-
-    @property
-    def _segments_points_offset(self):
-        """
-        :return: The accumulated number of points participating in the generation of the generalized frenet frame
-         for each segment in self.sub_segments, e.g.  segments_points_offset[2] contains the number of points
-          taken from subsegment #0 + the number of points taken from subsegment #1
-        """
-        return np.insert(np.array([seg.num_points_so_far for seg in self.sub_segments]), 0, 0., axis=0)
+        self._segments_id = segments_id
+        self._segments_s_offsets = segments_s_offsets
+        self._segments_ds = segments_ds
+        self._segments_point_offset = segments_point_offset
 
     @property
     def s_max(self):
@@ -65,7 +52,15 @@ class GeneralizedFrenetSerretFrame(FrenetSerret2DFrame):
         :return: A new GeneralizedFrenetSerretFrame built out of different other frenet frames.
         """
 
-        exact_sub_segments = sub_segments
+        segments_id = np.array([sub_seg.segment_id for sub_seg in sub_segments])
+        segments_s_start = np.array([sub_seg.s_start for sub_seg in sub_segments])
+        segments_s_end = np.array([sub_seg.s_end for sub_seg in sub_segments])
+        segments_ds = np.array([sub_seg.ds for sub_seg in sub_segments])
+        segments_num_points_so_far = np.zeros(shape=[len(sub_segments)], dtype=int)
+
+        # The accumulated longitudinal progress on the generalized frenet frame for each segment,
+        # e.g.  segments_s_offsets[2] contains the length of (subsegment #0 + subsegment #1)
+        segments_s_offsets = np.insert(np.cumsum(segments_s_end - segments_s_start), 0, 0., axis=0)
 
         points = np.empty(shape=[0, 2])
         T = np.empty(shape=[0, 2])
@@ -75,14 +70,14 @@ class GeneralizedFrenetSerretFrame(FrenetSerret2DFrame):
 
         for i in range(len(frenet_frames)):
             frame = frenet_frames[i]
-            start_ind = int(np.floor(exact_sub_segments[i].s_start / exact_sub_segments[i].ds))
+            start_ind = int(np.floor(segments_s_start[i] / segments_ds[i]))
             # if this is not the last frame then the next already has this frame's last point as its first - omit it.
             if i < len(frenet_frames) - 1:
                 # if this frame is not the last frame, it must end in s_max
-                assert exact_sub_segments[i].s_end == frame.s_max
+                assert segments_s_end[i] == frame.s_max
                 end_ind = frame.points.shape[0] - 1
             else:
-                end_ind = int(np.ceil(exact_sub_segments[i].s_end / exact_sub_segments[i].ds))
+                end_ind = int(np.ceil(segments_s_end[i] / segments_ds[i]))
 
             points = np.vstack((points, frame.points[start_ind:end_ind, :]))
             T = np.vstack((T, frame.T[start_ind:end_ind, :]))
@@ -90,9 +85,14 @@ class GeneralizedFrenetSerretFrame(FrenetSerret2DFrame):
             k = np.vstack((k, frame.k[start_ind:end_ind, :]))
             k_tag = np.vstack((k_tag, frame.k_tag[start_ind:end_ind, :]))
 
-            exact_sub_segments[i].num_points_so_far = points.shape[0]
+            segments_num_points_so_far[i] = points.shape[0]
 
-        return cls(points, T, N, k, k_tag, exact_sub_segments)
+        # The accumulated number of points participating in the generation of the generalized frenet frame
+        # for each segment, segments_points_offset[2] contains the number of points taken from subsegment #0
+        # plus the number of points taken from subsegment #1.
+        segments_point_offset = np.insert(segments_num_points_so_far, 0, 0., axis=0)
+
+        return cls(points, T, N, k, k_tag, segments_id, segments_s_offsets, segments_ds, segments_point_offset)
 
     def convert_from_segment_states(self, frenet_states: FrenetStates2D, segment_ids: List[int]) -> FrenetStates2D:
         """
@@ -129,7 +129,7 @@ class GeneralizedFrenetSerretFrame(FrenetSerret2DFrame):
         s_offset = self._segments_s_offsets[segment_idxs]
         new_frenet_states = frenet_states.copy()
         new_frenet_states[..., FS_SX] -= s_offset
-        return list(segment_idxs), new_frenet_states
+        return self._segments_id[segment_idxs], new_frenet_states
 
     def convert_to_segment_state(self, frenet_state: FrenetState2D) -> (int, FrenetState2D):
         """
@@ -137,7 +137,8 @@ class GeneralizedFrenetSerretFrame(FrenetSerret2DFrame):
         :param frenet_state: a frenet_state on the generalized frenet frame.
         :return: a tuple: (the segment_id, usually lane_id, this frenet_state will land on after the conversion, the resulted frenet state)
         """
-        return self.convert_to_segment_states(frenet_state[np.newaxis, ...])[0]
+        ids, fstates = self.convert_to_segment_states(frenet_state[np.newaxis, ...])
+        return ids[0], fstates[0]
 
     def _get_segment_idxs_from_ids(self, segment_ids: NumpyIndicesArray):
         """
@@ -145,8 +146,7 @@ class GeneralizedFrenetSerretFrame(FrenetSerret2DFrame):
         :param segment_ids:
         :return:
         """
-        all_ids = np.array([seg.segment_id for seg in self.sub_segments], dtype=np.int)
-        return npi.indices(all_ids, segment_ids)
+        return npi.indices(self._segments_id, segment_ids)
 
     def _get_segment_idxs_from_s(self, s_values: np.ndarray):
         """
@@ -170,13 +170,13 @@ class GeneralizedFrenetSerretFrame(FrenetSerret2DFrame):
         O_idx, delta_s = Euclidean.project_on_piecewise_linear_curve(points, self.O)
         # given the fractional index of the point (O_idx+delta_s), find which segment it belongs to based
         # on the points offset of each segment
-        segment_idx_per_point = np.searchsorted(self._segments_points_offset, np.add(O_idx, delta_s)) - 1
+        segment_idx_per_point = np.searchsorted(self._segments_point_offset, np.add(O_idx, delta_s)) - 1
         # get ds of every point based on the ds of the segment
-        ds = np.array([segment.ds for segment in self.sub_segments])[segment_idx_per_point]
+        ds = self._segments_ds[segment_idx_per_point]
         # The approximate longitudinal progress is the longitudinal offset of the segment plus the in-segment-index
         #  times the segment ds.
         s_approx = self._segments_s_offsets[segment_idx_per_point] + \
-                   (((O_idx - self._segments_points_offset[segment_idx_per_point]) + delta_s) * ds)
+                   (((O_idx - self._segments_point_offset[segment_idx_per_point]) + delta_s) * ds)
 
         return s_approx
 
@@ -191,11 +191,11 @@ class GeneralizedFrenetSerretFrame(FrenetSerret2DFrame):
         # get the index of the segment that contains each s value
         segment_idxs = self._get_segment_idxs_from_s(s)
         # get ds of every point based on the ds of the segment
-        ds = np.array([segment.ds for segment in self.sub_segments])[segment_idxs]
+        ds = self._segments_ds[segment_idxs]
         # subtracting the s offset, we get the s value in the associated segment.
         s_in_segment = s - self._segments_s_offsets[segment_idxs]
         # get the points offset of the segment that each longitudinal value resides in.
-        segment_points_offset = self._segments_points_offset[segment_idxs]
+        segment_points_offset = self._segments_point_offset[segment_idxs]
         # get the progress in index units (progress_in_points is a "floating-point index")
         progress_in_points = np.divide(s_in_segment, ds) + segment_points_offset
         # calculate and return the integer and fractional parts of the index
