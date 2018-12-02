f187ed82d2bc42f6589bd9df29d514eecb0bc053
Alison Below <alison.below@gm.com>
2018-12-01 20:51:56 -0500

    Add scene subscription limit to subscriber (to test with first message from replayed data)

--- test/utils/test_map_utils_subscriber.py
+++ test/utils/test_map_utils_subscriber.py
@@ -17,15 +17,18 @@ from decision_making.src.utils.map_utils import MapUtils
 
 
 class SceneSubscriber():
-    def __init__(self):
+    def __init__(self, limit_for_num_msgs=False, msg_limit=None):
         self.pubsub = create_pubsub(PubSubMessageTypes)
         self._scene_static_lock = Lock()
         self._scene_static = None
+        self._limit_for_num_msgs = limit_for_num_msgs
+        self._msg_limit = msg_limit
         self.running = False
 
     def start_sub(self) -> None:
         self.pubsub.subscribe(SCENE_STATIC, self._scene_static_callback)
         self.running = True
+        self._num_msgs_rcvd = 0
         print("INFO: Scene subscription active")
 
     def stop_sub(self) -> None:
@@ -36,8 +39,15 @@ class SceneSubscriber():
     def _scene_static_callback(self, scene_static: TsSYSSceneStatic, args: Any):
         try:
             with self._scene_static_lock:
-                self._scene_static = SceneStatic.deserialize(scene_static)
-                self._add_to_scene_model()
+                if self._limit_for_num_msgs:
+                    if self._num_msgs_rcvd < self._msg_limit:
+                        self._scene_static = SceneStatic.deserialize(scene_static)
+                        self._add_to_scene_model()
+                else:
+                    self._scene_static = SceneStatic.deserialize(scene_static)
+                    self._add_to_scene_model()
+                
+                self._num_msgs_rcvd += 1
 
         except Exception as e:
             print("ERROR: StateModule._scene_dynamic_callback failed due to %s", format_exc())
@@ -48,6 +58,9 @@ class SceneSubscriber():
 
 def main():
     scene_sub = SceneSubscriber()
+    # Use below if want to limit subscriber to one scene static message
+    # scene_sub = SceneSubscriber(True, 1)
+    
     if scene_sub: 
         scene_sub.start_sub()
 
