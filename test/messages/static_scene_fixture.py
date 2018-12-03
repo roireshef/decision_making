8149bb3258f5a1dd07385ea012a5896a708b8500
Gabi <gabi.guetta@gm.com>
2018-12-03 14:42:12 +0200

    array to list

--- test/messages/static_scene_fixture.py
+++ test/messages/static_scene_fixture.py
@@ -37,13 +37,13 @@ def scene_static():
         num_lanes = map_model.get_road_data(road_id).lanes_num
 
         try:
-            upstream_roads = np.ndarray(map_model.get_prev_road(road_id))
+            upstream_roads = np.array(map_model.get_prev_road(road_id))
         except NextRoadNotFound:
-            upstream_roads = np.ndarray([])
+            upstream_roads = np.array([])
         try:
-            downstream_roads = np.ndarray(map_model.get_next_road(road_id))
+            downstream_roads = np.array(map_model.get_next_road(road_id))
         except NextRoadNotFound:
-            downstream_roads = np.ndarray([])
+            downstream_roads = np.array([])
         lane_ids = np.array([map_api._lane_by_address[(road_id, i)] for i in range(num_lanes)])
         scene_road_segment = SceneRoadSegment(e_Cnt_road_segment_id=road_id, e_Cnt_road_id=0,
                                               e_Cnt_lane_segment_id_count=num_lanes,
@@ -57,9 +57,9 @@ def scene_static():
         scene_road_segments.append(scene_road_segment)
     scene_lane_segments = []
     for lane_id in map_api._lane_address:
-        road_segment_id, lane_ordinal  = map_api._lane_address[lane_id]
-        right_adj_lanes=[map_api._lane_by_address[(road_segment_id,k)] for k  in range(lane_ordinal)]
-        left_adj_lanes=[map_api._lane_by_address[(road_segment_id,k)] for k  in range(lane_ordinal,
+        road_segment_id, lane_ordinal = map_api._lane_address[lane_id]
+        right_adj_lanes = [map_api._lane_by_address[(road_segment_id, k)] for k in range(lane_ordinal)]
+        left_adj_lanes = [map_api._lane_by_address[(road_segment_id, k)] for k in range(lane_ordinal+1,
                                                                                       map_model.get_road_data(road_id).lanes_num)]
         downstream_id, upstream_id = get_connectivity_lane_segment(map_api, road_segment_id, lane_ordinal, lane_id)
         lane_frenet = map_api._lane_frenet[lane_id]
@@ -80,12 +80,11 @@ def scene_static():
             point[NominalPathPoint.CeSYS_NominalPathPoint_e_l_right_offset.value] = half_lane_width
             nominal_points.append(point)
 
+        left_boundry_point = [BoundaryPoint(MapLaneMarkerType.MapLaneMarkerType_SolidSingleLine_BottsDots,
+                                                     0, lane_frenet.s_max)]
 
-        left_boundry_point = np.array([BoundaryPoint(MapLaneMarkerType.MapLaneMarkerType_SolidSingleLine_BottsDots,
-                                                     0, lane_frenet.s_max)])
-
-        right_boundry_point = np.array([BoundaryPoint(MapLaneMarkerType.MapLaneMarkerType_SolidSingleLine_BottsDots,
-                                           0, lane_frenet.s_max)])
+        right_boundry_point = [BoundaryPoint(MapLaneMarkerType.MapLaneMarkerType_SolidSingleLine_BottsDots,
+                                           0, lane_frenet.s_max)]
 
         downstream_lane_segment_connectivity = LaneSegmentConnectivity(downstream_id, ManeuverType.STRAIGHT_CONNECTION)
         upstream_lane_segment_connectivity = LaneSegmentConnectivity(upstream_id, ManeuverType.STRAIGHT_CONNECTION)
@@ -128,10 +127,11 @@ def scene_static():
                            e_Cnt_num_road_intersections=0,
                            as_scene_road_intersection=[],
                            e_Cnt_num_road_segments=len(scene_road_segments),
-                           as_scene_road_segment=np.array(scene_road_segments))
+                           as_scene_road_segment=scene_road_segments)
 
     scene = SceneStatic(s_Header=header, s_MapOrigin=map_origin, s_Data=data)
     return scene
+
 
 if __name__ == '__main__':
     scene_static()
