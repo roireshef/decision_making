3ee216fc9206c40fc6500e20a1081d1c38df10e3
Pinaki Gupta <pinaki.gupta@gm.com>
2019-01-24 18:22:41 -0500

    Some more Scene Static Lite parsing updates

--- src/planning/route/cost_based_route_planner.py
+++ src/planning/route/cost_based_route_planner.py
@@ -1,4 +1,27 @@
-from decision_making.src.planning.route.route_planner import RoutePlanner
+from decision_making.src.messages.scene_static_lite_message import SceneStaticLite,DataSceneStaticLite
+
+class RoutePlannerData():
+    
+    def __init__(self):
+        self.route_segments = [] # list: ordered RouteSegment's upto the variable size e_Cnt_num_road_segments
+        self.LaneSegments = {} # dict : key - segment ID, value - LaneSegmentLite. The dict should contain all the lane segments listed in self.route_segment_ids. 
+
+
+    def plan(self,Scene:SceneStaticLite): # TODO: Set function annotaion
+        # Create route plan
+        pass
+
+    def Update_LaneSegmentData(self,Scene:SceneStaticLite):
+        for i in range(Scene.s_Data.e_Cnt_num_lane_segments):
+            self.LaneSegments[Scene.s_Data.as_scene_lane_segment[i].e_i_lane_segment_id] = \
+            Scene.s_Data.as_scene_lane_segment[i]
+        pass
+
+    def Update_RoutePlanData(self,Scene:SceneStaticLite):
+        for i in range(Scene.s_Data.e_Cnt_num_road_segments):
+            self.route_segments.append(Scene.s_Data.as_scene_lane_segment[i].e_i_lane_segment_id)
+        pass
+
 
 
 class RoutePlanData(metaclass=ABCMeta):
