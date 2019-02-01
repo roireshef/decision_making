36f5e02f68ec49c751e8e72f4941a332e26c9c21
Rouhollah Jafari <rouhollah.jafari@gm.com>
2019-02-01 15:23:28 -0500

    update to fix import issue

--- src/planning/behavioral/behavioral_planning_facade.py
+++ src/planning/behavioral/behavioral_planning_facade.py
@@ -21,8 +21,8 @@ from decision_making.src.messages.navigation_plan_message import NavigationPlanM
 from decision_making.src.messages.scene_static_message import SceneStatic
 from decision_making.src.messages.trajectory_parameters import TrajectoryParams
 from decision_making.src.messages.visualization.behavioral_visualization_message import BehavioralVisualizationMsg
-from decision_making.src.planning.behavioral.planner.cost_based_behavioral_planner import \
-    CostBasedBehavioralPlanner
+from decision_making.src.planning.behavioral.planner.cost_based_behavioral_planner import CostBasedBehavioralPlanner
+
 from decision_making.src.planning.trajectory.samplable_trajectory import SamplableTrajectory
 from decision_making.src.planning.types import CartesianExtendedState
 from decision_making.src.planning.utils.localization_utils import LocalizationUtils
