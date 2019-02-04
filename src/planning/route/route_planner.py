576962331eedfb792c7c41b996f674fc1324911f
Anthony Perez <anthony.perez@gm.com>
2019-02-04 17:16:13 -0500

    Bug fixes

--- src/planning/route/route_planner.py
+++ src/planning/route/route_planner.py
@@ -1,11 +1,11 @@
 from abc import ABCMeta, abstractmethod
 from cost_based_route_planner import RoutePlannerInputData
-from common_data.interface.py.idl_generated_files.Rte_Types.sub_structures import TsSYS_DataRoutePlan
+from common_data.interface.Rte_Types.python.sub_structures import TsSYSDataRoutePlan
 
 
 
     @abstractmethod
-    def plan(self,RouteData:RoutePlannerInputData)->TsSYS_DataRoutePlan: # TODO: Set function annotaion
+    def plan(self,RouteData:RoutePlannerInputData)->TsSYSDataRoutePlan: # TODO: Set function annotaion
         """Add comments"""
         pass
 
