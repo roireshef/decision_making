c08e597c0812948f683ae573e6a7d0c957c5fc0c
RoiReshef <roi.reshef@gm.com>
2019-02-11 19:04:49 +0200

    added fix to allow recursive serialization of lists

---
+++ test/messages/str_serializable.py
@@ -0,0 +1,23 @@
+from decision_making.src.global_constants import PUBSUB_MSG_IMPL
+from typing import List
+
+
+class SimpleStruct(PUBSUB_MSG_IMPL):
+    x = int
+    y = str
+
+    def __init__(self, x: int, y: str):
+        self.y = y
+        self.x = x
+
+
+class MessageWithNestedLists(PUBSUB_MSG_IMPL):
+    ll = List[List[SimpleStruct]]
+
+    def __init__(self, ll: List[List[SimpleStruct]]):
+        self.ll = ll
+
+
+def test_toDict_messageWithNestedLists_terminatesSuccessfully():
+    ll = MessageWithNestedLists([[SimpleStruct(1, 'one'), SimpleStruct(2, 'two')],[SimpleStruct(3, 'three')]])
+    print(ll.__str__())
