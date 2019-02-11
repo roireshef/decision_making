e5de64c8d3fa6db3bb982ae54739bef4f57e33ef
mz8cj6 <vlad.goldner@gm.com>
2019-02-11 14:49:11 +0200

    add unit test for the lateral acceleration filter

--- test/planning/behavioral/behavioral_state_fixtures.py
+++ test/planning/behavioral/behavioral_state_fixtures.py
@@ -3,7 +3,7 @@ from typing import List
 import numpy as np
 import pytest
 
-from decision_making.src.global_constants import EPS
+from decision_making.src.global_constants import EPS, FILTER_V_T_GRID
 from decision_making.src.scene.scene_static_model import SceneStaticModel
 from decision_making.src.messages.navigation_plan_message import NavigationPlanMsg
 from decision_making.src.planning.behavioral.behavioral_grid_state import BehavioralGridState, RelativeLane, \
@@ -13,9 +13,10 @@ from decision_making.src.planning.behavioral.data_objects import DynamicActionRe
 from decision_making.src.state.map_state import MapState
 from decision_making.src.state.state import OccupancyState, State, ObjectSize, EgoState, DynamicObject
 from decision_making.src.utils.map_utils import MapUtils
-from decision_making.test.messages.static_scene_fixture import scene_static
+from decision_making.test.messages.static_scene_fixture import scene_static, scene_static_ovalmilford
 
 NAVIGATION_PLAN = NavigationPlanMsg(np.array(range(20, 30)))
+MILFORD_NAVIGATION_PLAN = NavigationPlanMsg(np.array([1]))
 EGO_LANE_LON = 120.  # ~2 meters behind end of a lane segment
 
 
@@ -181,6 +182,33 @@ def state_with_objects_for_filtering_too_aggressive():
 
 
 @pytest.fixture(scope='function')
+def state_without_objects_ovalmilford():
+
+    SceneStaticModel.get_instance().set_scene_static(scene_static_ovalmilford())
+
+    road_segment_ids = MapUtils.get_road_segment_ids()
+    road_segment_id = road_segment_ids[0]
+
+    # Stub of occupancy grid
+    occupancy_state = OccupancyState(0, np.array([]), np.array([]))
+
+    car_size = ObjectSize(length=2.5, width=1.5, height=1.0)
+
+    # Ego state
+    ego_lane_lon = 100
+    ego_vel = 20
+    ego_lane_id = MapUtils.get_lanes_ids_from_road_segment_id(road_segment_id)[1]
+
+    map_state = MapState(np.array([ego_lane_lon, ego_vel, 0, 0, 0, 0]), ego_lane_id)
+    ego_state = EgoState.create_from_map_state(obj_id=0, timestamp=0, map_state=map_state, size=car_size, confidence=1)
+
+    dynamic_objects: List[DynamicObject] = list()
+
+    yield State(occupancy_state=occupancy_state, dynamic_objects=dynamic_objects, ego_state=ego_state)
+
+
+
+@pytest.fixture(scope='function')
 def behavioral_grid_state(state_with_sorrounding_objects: State):
     yield BehavioralGridState.create_from_state(state_with_sorrounding_objects,
                                                 NAVIGATION_PLAN, None)
@@ -216,6 +244,14 @@ def follow_vehicle_recipes_towards_front_cells():
 @pytest.fixture(scope='function')
 def follow_lane_recipes():
     velocity_grid = np.arange(0, 30 + EPS, 6)
+    yield [StaticActionRecipe(RelativeLane.SAME_LANE, velocity, agg)
+           for velocity in velocity_grid
+           for agg in AggressivenessLevel]
+
+
+@pytest.fixture(scope='function')
+def all_follow_lane_recipes():
+    velocity_grid = np.arange(0, FILTER_V_T_GRID.end + EPS, 2)
     yield [StaticActionRecipe(RelativeLane.SAME_LANE, velocity, agg)
            for velocity in velocity_grid
            for agg in AggressivenessLevel]
