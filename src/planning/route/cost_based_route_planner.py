2e0112a7e1ea3bd46c9e9e4dacb2f12eade4ffc9
Pinaki Gupta <pinaki.gupta@gm.com>
2019-02-12 16:40:11 -0500

    "Added some more comments to the cost based planner"

--- src/planning/route/cost_based_route_planner.py
+++ src/planning/route/cost_based_route_planner.py
@@ -73,26 +73,41 @@ class CostBasedRoutePlanner(RoutePlanner):
         a_Cnt_num_lane_segments = []
         as_route_plan_lane_segments = []
 
-        for reverseroadsegidx, (roadsegID,lanesegIDs) in enumerate(reversed(RouteData.route_lanesegments.items())):
+        #iterate over all road segments in the route plan in the reverse sequence. Enumerate the iterable to get the index also
+        # index -> reverseroadsegidx
+        # key -> roadsegID
+        # value -> lanesegIDs 
+        reverse_enumerated_route_lanesegments = enumerate(reversed(RouteData.route_lanesegments.items()))
+        for reverseroadsegidx, (roadsegID,lanesegIDs) in reverse_enumerated_route_lanesegments:
             # roadsegidx = num_road_segments - reverseroadsegidx
             AllRouteLanesInThisRoadSeg = []
-            for lanesegidx, lanesegID in enumerate(lanesegIDs):
-                lanesegData = RouteData.LaneSegmentDict[lanesegID]
-                LaneOccCost = 0
+    
+            # Now iterate over all the lane segments inside  the enumerate(road segment)
+            # index -> lanesegidx
+            # value -> lanesegID
+            enumerated_lanesegIDs = enumerate(lanesegIDs)
+            for lanesegidx, lanesegID in enumerated_lanesegIDs:
+                lanesegLiteData = RouteData.LaneSegmentDict[lanesegID] # Access all the lane segment lite data from lane segment dict
+                # initialize lane costs
+                LaneOccCost = 0 
                 LaneEndCost = 0
                 
                 # -------------------------------------------
                 # Calculate lane occupancy costs
                 # -------------------------------------------
-                for Idx in range(lanesegData.e_Cnt_num_active_lane_attributes):
-                    LaneAttrIdx = lanesegData.a_i_active_lane_attribute_indices[Idx]
-                    LaneAttr = lanesegData.a_cmp_lane_attributes[LaneAttrIdx]
-                    LaneAttrConf = lanesegData.a_cmp_lane_attribute_confidences[LaneAttrIdx]
-                    if (LaneAttrConf<0.7):
+                
+                # Now iterate over all the active lane attributes for the lane segment
+                for Idx in range(lanesegLiteData.e_Cnt_num_active_lane_attributes):
+                    # LaneAttrIdx gives the index lookup for lane attributes and confidences
+                    LaneAttrIdx = lanesegLiteData.a_i_active_lane_attribute_indices[Idx]
+                    LaneAttr = lanesegLiteData.a_cmp_lane_attributes[LaneAttrIdx]
+                    LaneAttrConf = lanesegLiteData.a_cmp_lane_attribute_confidences[LaneAttrIdx]
+                    if (LaneAttrConf<0.7): # change to a config param later
                         continue
                     LaneAttrOccCost = CostBasedRoutePlanner.LaneAttrBsdOccCost(LaneAttrIdx,LaneAttr)
                     #print("LaneAttrConf ",LaneAttrConf," LaneAttrIdx",LaneAttrIdx," LaneAttr ",LaneAttr," LaneAttrOccCost ",LaneAttrOccCost)
-                    LaneOccCost = LaneOccCost + LaneAttrOccCost
+                    LaneOccCost = LaneOccCost + LaneAttrOccCost #Add costs from all lane attributes
+
                 # Normalize to the [0, 1] range
                 LaneOccCost = max(min(LaneOccCost,1),0)
                 
@@ -105,14 +120,13 @@ class CostBasedRoutePlanner(RoutePlanner):
                 if (reverseroadsegidx==0) : # the last road segment in the route; put all endcosts to 0
                     LaneEndCost = 0
                 elif (LaneOccCost==1):
-                    LaneEndCost = 1 # Can't occupy the lane, can't end up in the lane
-                elif (reverseroadsegidx>0):# if reverseroadsegidx=0 a.k.a last lane in current route view lane end cost = 0 
-                    # as we don't know the next segment. 
+                    LaneEndCost = 1 # Can't occupy the lane, end cost must be MAX(=1)
+                elif (reverseroadsegidx>0):
                     MinDwnStreamLaneOccCost = 1
                     # Search iteratively for the next segment lanes that are downstream to the current lane and in the route.
                     # At this point assign the end cost of current lane = Min occ costs (of all downstream lanes)
-                    for Idx in range(lanesegData.e_Cnt_downstream_lane_count): # search through all downstream lanes to to current lane
-                        DownStreamlanesegID = lanesegData.as_downstream_lanes[Idx].e_i_lane_segment_id
+                    for Idx in range(lanesegLiteData.e_Cnt_downstream_lane_count): # search through all downstream lanes to to current lane
+                        DownStreamlanesegID = lanesegLiteData.as_downstream_lanes[Idx].e_i_lane_segment_id
                         NextRoadSegLanes = RouteData.route_lanesegments[prev_roadsegID] # All lane IDs in downstream roadsegment in route to the 
                         #currently indexed roadsegment in the loop
                         if DownStreamlanesegID in NextRoadSegLanes: # verify if the downstream lane is in the route
@@ -122,26 +136,27 @@ class CostBasedRoutePlanner(RoutePlanner):
                             # confirm -> roadsegidx+1 in the RoutPlan == reverseroadsegidx-1 in the reversed RoutePlan
                             MinDwnStreamLaneOccCost = min(MinDwnStreamLaneOccCost,DownStreamLaneOccCost)
                         else:
+                            # Add exception later
                             print(" DownStreamlanesegID ",DownStreamlanesegID," not found in NextRoadSegLanes ",NextRoadSegLanes)
                             print("prev_roadsegID",prev_roadsegID)
                     LaneEndCost = MinDwnStreamLaneOccCost
+                else:
+                    print(" Bad value for reverseroadsegidx :",reverseroadsegidx) # Add exception later
                 
-                            
+                # Construct RoutePlanLaneSegment for the lane and add to the RoutePlanLaneSegment container for this Road Segment        
                 CurrRoutePlanLaneSegment = RoutePlanLaneSegment(e_i_lane_segment_id= lanesegID, \
                     e_cst_lane_occupancy_cost = LaneOccCost,e_cst_lane_end_cost = LaneEndCost)
-                #print("CurrRoutePlanLaneSegment",CurrRoutePlanLaneSegment)
                 AllRouteLanesInThisRoadSeg.append(CurrRoutePlanLaneSegment)
+
+            # At this point we have at least iterated once through the road segment loop once. prev_roadsegID is set to be used for 
+            # the next road segment loop
             prev_roadsegID = roadsegID
-            # NewRoutePlan.e_Cnt_num_road_segments = reverseroadsegidx
+
+            # push back the road segment sepecific info , as the road seg iteration is reverse 
             a_i_road_segment_ids.insert(0,roadsegID)
             a_Cnt_num_lane_segments.insert(0,lanesegidx+1)
             as_route_plan_lane_segments.insert(0,AllRouteLanesInThisRoadSeg)
 
-
-            #print("AllRouteLanesInThisRoadSeg ")
-            #for RouteLanes in AllRouteLanesInThisRoadSeg:
-            #    print(RouteLanes)
-            #NewRoutePlan.as_route_plan_lane_segments.append(AllRouteLanesInThisRoadSeg)
 
         NewRoutePlan = DataRoutePlan(e_b_is_valid = valid, e_Cnt_num_road_segments= num_road_segments, a_i_road_segment_ids=np.array(a_i_road_segment_ids), \
                                         a_Cnt_num_lane_segments= np.array(a_Cnt_num_lane_segments), as_route_plan_lane_segments=as_route_plan_lane_segments )
