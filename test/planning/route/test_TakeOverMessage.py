d1096e38725b3cc5d8fde3573d483810e80ff23f
Rouhollah Jafari <rouhollah.jafari@gm.com>
2019-02-15 17:04:33 -0500

    updates regarding the changes in takeover function - still not working

--- test/planning/route/test_TakeOverMessage.py
+++ test/planning/route/test_TakeOverMessage.py
@@ -5,19 +5,25 @@ from typing import List
 import pytest
 from logging import Logger
 
+
+from decision_making.src.infra.pubsub import PubSub
+from rte.python.logger.AV_logger import AV_Logger
+
 from decision_making.src.messages.route_plan_message import RoutePlan, RoutePlanLaneSegment, DataRoutePlan
      
 from decision_making.src.planning.route.route_planner import RoutePlanner, RoutePlannerInputData
 
 from decision_making.test.messages.static_scene_fixture import scene_static
+from decision_making.test.planning.route.scene_static_publisher import SceneStaticPublisher
 
 from decision_making.src.planning.behavioral.behavioral_planning_facade import BehavioralPlanningFacade
 from decision_making.src.state.state import State
-# form decision_making.test.messages.planning.behavioral.behavioral_state_fixtures import 
+from decision_making.src.scene.scene_static_model import SceneStaticModel
+from decision_making.test.planning.behavioral.behavioral_state_fixtures import state_with_no_sorrounding_objects_for_takover_message
 
 
 def test_setTakeoverMessage_simpleScene_takeoverFlag():
-
+    
     # route plan input
     num_road_segments = 2
 
@@ -31,9 +37,18 @@ def test_setTakeoverMessage_simpleScene_takeoverFlag():
 
     route_plan = DataRoutePlan(True, num_road_segments, road_segment_ids, num_lane_segments,route_plan_lane_segments)
 
-    #TODO: come up with a way to generate state
+    # setting the scene static for MApUtil functions
 
-    takeover_msg = BehavioralPlanningFacade.set_takeover_message(route_plan = route_plan, state= None)
+    logger = AV_Logger.get_logger("")
+    pubsub = PubSub()
+    
+    scene_static_obj = SceneStaticPublisher(pubsub = pubsub , logger = logger)
+    scene_static_data = scene_static_obj._generate_data()
+
+    takeover_msg = BehavioralPlanningFacade.set_takeover_message(route_plan = route_plan, state= state_with_no_sorrounding_objects_for_takover_message, \
+                                                                 scene_static =scene_static_data)
+
+    assert takeover_msg.s_Data.e_b_is_takeover_needed == False
 
 
 
