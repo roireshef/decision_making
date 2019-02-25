ad92a213ef8b38f9513f89fc07acf7d2def9bc4c
Pinaki Gupta <pinaki.gupta@gm.com>
2019-02-25 12:43:46 -0500

    Filter the road and lane seg dict data s.t. only the segments in the route are inserted in those container

--- src/planning/route/route_planner.py
+++ src/planning/route/route_planner.py
@@ -7,33 +7,37 @@ from decision_making.src.messages.scene_static_message import SceneStaticBase, N
 class RoutePlannerInputData():
 
     def __init__(self, scene: SceneStaticBase, nav_plan: NavigationPlan):
-        #self.route_roadsegments = [] # list: ordered RouteSegment's upto the variable size e_Cnt_num_road_segments
         self.route_lanesegments = OrderedDict() # dict:  key - road segment IDs (sorted as in routeplan) , value - list(LaneSegmentID)
         self.route_lanesegs_base_as_dict = {} # dict : key - lane segment ID, value - LaneSegmentBase.
         #The dict should contain all the lane segments listed in self.route_segment_ids.
         self.route_roadsegs_as_dict = {} # dict : key - road segment ID, value - Road Segments.
         #The dict should contain all the lane segments listed in self.route_segment_ids.
-        self._update_dict_data(scene)
+
+        self._update_dict_data(scene,nav_plan)
         self._update_routeplan_data(nav_plan)
 
         # This method updates route_lanesegs_base_as_dict : all the lanesegment base structures for lane in the route, as a dictionary for fast access
         #                     route_roadsegs_as_dict : all the roadsegments in the route as a dictionary for fast access
-        # Input is Scene Static Base
-    def _update_dict_data(self, scene: SceneStaticBase)->None:
+        # Input is Scene Static Base and Navigation Plan (to filter only the road and lane segments that are in the navigation route)
+    def _update_dict_data(self, scene: SceneStaticBase, nav_plan: NavigationPlan)->None:
         for i in range(scene.e_Cnt_num_lane_segments):
             e_i_lane_segment_id = scene.as_scene_lane_segments[i].e_i_lane_segment_id
-            self.route_lanesegs_base_as_dict[e_i_lane_segment_id] = scene.as_scene_lane_segments[i]
+            e_i_road_segment_id = scene.as_scene_lane_segments[i].e_i_road_segment_id
+            if e_i_road_segment_id in nav_plan.a_i_road_segment_ids: # Verify if these road segs are in route.
+                self.route_lanesegs_base_as_dict[e_i_lane_segment_id] = scene.as_scene_lane_segments[i]
+
         for i in range(scene.e_Cnt_num_road_segments):
             e_i_road_segment_id = scene.as_scene_road_segment[i].e_i_road_segment_id
-            self.route_roadsegs_as_dict[e_i_road_segment_id] = scene.as_scene_road_segment[i]
+            if e_i_road_segment_id in nav_plan.a_i_road_segment_ids: # Verify if these road segs are in route.
+                self.route_roadsegs_as_dict[e_i_road_segment_id] = scene.as_scene_road_segment[i]
+
 
     def _update_routeplan_data(self, nav_plan: NavigationPlan)->None:
         for road_seg_idx in range(nav_plan.e_Cnt_num_road_segments):
             road_seg = nav_plan.a_i_road_segment_ids[road_seg_idx]
-            #self.route_roadsegments.append(road_seg)
             laneseg_ids_in_this_roadseg = []
-            #print("route_roadsegments",self.route_roadsegments)
-            for j in range(self.route_roadsegs_as_dict[road_seg].e_Cnt_lane_segment_id_count):
+            e_Cnt_lane_segment_id_count = self.route_roadsegs_as_dict[road_seg].e_Cnt_lane_segment_id_count
+            for j in range(e_Cnt_lane_segment_id_count):
                 laneseg_ids_in_this_roadseg.append(self.route_roadsegs_as_dict[road_seg].a_i_lane_segment_ids[j])
             self.route_lanesegments[road_seg]=laneseg_ids_in_this_roadseg
 
