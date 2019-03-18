93690f72c4169521fd605d26375e9028c6f945d1
Anthony Perez <anthony.perez@gm.com>
2019-03-18 08:32:15 -0400

    Took care of some TODOs

--- test/planning/route/scene_static_publisher_facade.py
+++ test/planning/route/scene_static_publisher_facade.py
@@ -9,35 +9,30 @@ from decision_making.src.messages.scene_static_message import SceneStatic
 from decision_making.test.planning.route.scene_static_publisher import SceneStaticPublisher
 
 class SceneStaticPublisherFacade(DmModule):
-    """
-    TODO: Add description
-
-    Args:
-        pubsub: TODO: Add description
-        logger: TODO: Add description
-    """
     def __init__(self, pubsub: PubSub, logger: Logger, publisher: SceneStaticPublisher):
+        """
+        :param pubsub: Middleware
+        :param logger: Logger
+        :param publisher: Class that creates scene static data
+        """
         super().__init__(pubsub=pubsub, logger=logger)
         self.logger.info("Initialized Scene Static Publisher")
         self._publisher = publisher
 
     def _start_impl(self):
-        """ TODO: Add description """
         pass
 
     def _stop_impl(self):
-        """ TODO: Add description """
         pass
 
     def _periodic_action_impl(self):
-        """ TODO: Add description """
+        """ The main function that is executed periodically. """
         try:
             # Generate Data and Publish Message
             self._publish_scene_static(self._publisher.generate_data())
 
         except Exception as e:
-            self.logger.critical("SceneStaticPublisher: UNHANDLED EXCEPTION: %s. Trace: %s",
-                                 e, format_exc())
+            self.logger.critical("SceneStaticPublisher: UNHANDLED EXCEPTION: %s. Trace: %s", e, format_exc())
 
     def _publish_scene_static(self, scene_static: SceneStatic) -> None:
         """ Publish SCENE_STATIC message """
