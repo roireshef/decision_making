22866058991352a07f025747fbf5552e1fa6bd6e
RoiReshef <roi.reshef@gm.com>
2019-03-26 13:28:42 +0200

    changed default_config to include FilterForSafetyTowardsTargetVehicle

--- src/planning/behavioral/default_config.py
+++ src/planning/behavioral/default_config.py
@@ -1,6 +1,6 @@
 from decision_making.src.global_constants import BEHAVIORAL_PLANNING_NAME_FOR_LOGGING
 from decision_making.src.planning.behavioral.filtering.action_spec_filter_bank import FilterIfNone as ASpecFilterIfNone, \
-    FilterForKinematics
+    FilterForKinematics, FilterForSafetyTowardsTargetVehicle
 from decision_making.src.planning.behavioral.filtering.action_spec_filtering import ActionSpecFiltering
 from decision_making.src.planning.behavioral.filtering.recipe_filter_bank import FilterIfNone as RecipeFilterIfNone, \
     FilterActionsTowardsNonOccupiedCells, FilterActionsTowardBackAndParallelCells, FilterOvertakeActions, \
@@ -9,13 +9,21 @@ from decision_making.src.planning.behavioral.filtering.recipe_filtering import R
 from rte.python.logger.AV_logger import AV_Logger
 
 # TODO: remove FilterIfAggressive() once we introduce lateral and longitudinal acceleration checks in Cartesian frame
-DEFAULT_STATIC_RECIPE_FILTERING = RecipeFiltering(filters=[RecipeFilterIfNone(), FilterIfNoLane(), FilterIfAggressive(),
-                                                           FilterLaneChanging(), FilterBadExpectedTrajectory('predicates')],
-                                                  logger=AV_Logger.get_logger(BEHAVIORAL_PLANNING_NAME_FOR_LOGGING))
-DEFAULT_DYNAMIC_RECIPE_FILTERING = RecipeFiltering(filters=[RecipeFilterIfNone(), FilterActionsTowardsNonOccupiedCells(),
+DEFAULT_STATIC_RECIPE_FILTERING = RecipeFiltering(filters=[RecipeFilterIfNone(),
+                                                           FilterIfNoLane(),
+                                                           # FilterIfAggressive(),
+                                                           FilterLaneChanging(),
+                                                           # FilterBadExpectedTrajectory('predicates')
+                                                           ], logger=AV_Logger.get_logger(BEHAVIORAL_PLANNING_NAME_FOR_LOGGING))
+DEFAULT_DYNAMIC_RECIPE_FILTERING = RecipeFiltering(filters=[RecipeFilterIfNone(),
+                                                            FilterActionsTowardsNonOccupiedCells(),
                                                             FilterActionsTowardBackAndParallelCells(),
-                                                            FilterOvertakeActions(), FilterLaneChanging(),
-                                                            FilterBadExpectedTrajectory('predicates')],
+                                                            FilterOvertakeActions(),
+                                                            FilterLaneChanging(),
+                                                            # FilterBadExpectedTrajectory('predicates')
+                                                            ],
                                                    logger=AV_Logger.get_logger(BEHAVIORAL_PLANNING_NAME_FOR_LOGGING))
-DEFAULT_ACTION_SPEC_FILTERING = ActionSpecFiltering(filters=[ASpecFilterIfNone(), FilterForKinematics()],
+DEFAULT_ACTION_SPEC_FILTERING = ActionSpecFiltering(filters=[ASpecFilterIfNone(),
+                                                             FilterForKinematics(),
+                                                             FilterForSafetyTowardsTargetVehicle()],
                                                     logger=AV_Logger.get_logger(BEHAVIORAL_PLANNING_NAME_FOR_LOGGING))
