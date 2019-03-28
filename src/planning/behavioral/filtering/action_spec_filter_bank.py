0a47b9541ce674721afda1c6c1e7fe0f3542e4d1
Zahy Bnaya <zahy.bnaya@gm.com>
2019-03-28 15:08:11 +0200

    added the FilterForKinematics

--- src/planning/behavioral/filtering/action_spec_filter_bank.py
+++ src/planning/behavioral/filtering/action_spec_filter_bank.py
@@ -4,14 +4,17 @@ import numpy as np
 import six
 from abc import ABCMeta, abstractmethod
 from decision_making.src.global_constants import LAT_ACC_LIMITS, EPS, FILTER_A_0_GRID, FILTER_V_0_GRID, \
-    TRAJECTORY_TIME_RESOLUTION, BP_ACTION_T_LIMITS, FILTER_V_T_GRID, BP_JERK_S_JERK_D_TIME_WEIGHTS_FOLLOW_LANE
+    TRAJECTORY_TIME_RESOLUTION, BP_ACTION_T_LIMITS, FILTER_V_T_GRID, BP_JERK_S_JERK_D_TIME_WEIGHTS_FOLLOW_LANE, \
+    WERLING_TIME_RESOLUTION, VELOCITY_LIMITS, LON_ACC_LIMITS
 from decision_making.src.planning.behavioral.behavioral_grid_state import BehavioralGridState
 from decision_making.src.planning.behavioral.behavioral_state import BehavioralState
-from decision_making.src.planning.behavioral.data_objects import ActionSpec, ActionType, AggressivenessLevel
+from decision_making.src.planning.behavioral.data_objects import ActionSpec, ActionType, AggressivenessLevel, \
+    StaticActionRecipe, DynamicActionRecipe
 from decision_making.src.planning.behavioral.filtering.action_spec_filtering import \
     ActionSpecFilter
 from decision_making.src.planning.behavioral.filtering.recipe_filter_bank import FilterLimitsViolatingTrajectory
-from decision_making.src.planning.types import FS_DX, FS_SX, C_V, C_K
+from decision_making.src.planning.trajectory.samplable_werling_trajectory import SamplableWerlingTrajectory
+from decision_making.src.planning.types import FS_DX, FS_SX, C_V, C_K, FS_SA, LIMIT_MIN
 from decision_making.src.planning.utils.generalized_frenet_serret_frame import GeneralizedFrenetSerretFrame
 from decision_making.src.planning.utils.numpy_utils import NumpyUtils
 from decision_making.src.planning.utils.optimal_control.poly1d import QuinticPoly1D
@@ -156,6 +159,49 @@ class ConstraintStoppingAtLocationFilter(ConstraintSpecFilter):
 
     def _condition(self, target_values, constraints_values) -> bool:
         return target_values <= constraints_values
+
+
+class FilterForKinematics(ActionSpecFilter):
+    def filter(self, action_specs: List[ActionSpec], behavioral_state: BehavioralGridState) -> List[bool]:
+        relative_lanes = np.array([spec.relative_lane for spec in action_specs])
+
+        initial_fstates = np.array([behavioral_state.projected_ego_fstates[lane] for lane in relative_lanes])
+        terminal_fstates = np.array([spec.as_fstate() for spec in action_specs])
+        T = np.array([spec.t for spec in action_specs])
+
+        constraints_s = np.concatenate((initial_fstates[:, :(FS_SA+1)], terminal_fstates[:, :(FS_SA+1)]), axis=1)
+        constraints_d = np.concatenate((initial_fstates[:, FS_DX:], terminal_fstates[:, FS_DX:]), axis=1)
+
+        A_inv = np.linalg.inv(QuinticPoly1D.time_constraints_tensor(T))
+        poly_coefs_s = QuinticPoly1D.zip_solve(A_inv, constraints_s)
+        poly_coefs_d = QuinticPoly1D.zip_solve(A_inv, constraints_d)
+
+        are_valid = []
+        for poly_s, poly_d, t, lane, spec in zip(poly_coefs_s, poly_coefs_d, T, relative_lanes, action_specs):
+
+            time_samples = np.arange(0, t + EPS, WERLING_TIME_RESOLUTION)
+            frenet_frame = behavioral_state.extended_lane_frames[lane]
+            total_time = max(BP_ACTION_T_LIMITS[LIMIT_MIN], t)
+
+            samplable_trajectory = SamplableWerlingTrajectory(0, t, t, total_time, frenet_frame, poly_s, poly_d)
+            samples = samplable_trajectory.sample(time_samples)
+
+            is_valid_in_cartesian = KinematicUtils.filter_by_cartesian_limits(samples[np.newaxis, ...],
+                                                                 VELOCITY_LIMITS, LON_ACC_LIMITS, LAT_ACC_LIMITS)[0]
+
+            # if the action is static, there's a chance the first coefficient is zero, and this is a problem for the
+            # Math.roots function
+            first_non_zero = np.argmin(np.equal(poly_s, 0)) if isinstance(spec.recipe, StaticActionRecipe) else 0
+            is_valid_in_frenet = KinematicUtils.filter_by_longitudinal_frenet_limits(poly_s[np.newaxis, first_non_zero:], np.array([t]),
+                                                                                     LON_ACC_LIMITS, VELOCITY_LIMITS, frenet_frame.s_limits)
+
+            are_valid.append(np.logical_and(is_valid_in_cartesian, is_valid_in_frenet)[0])
+
+        # TODO: remove - for debug only
+        had_dynmiacs = sum([isinstance(spec.recipe, DynamicActionRecipe) for spec in action_specs]) > 0
+        valid_dynamics = sum([valid and isinstance(spec.recipe, DynamicActionRecipe) for spec, valid in zip(action_specs, are_valid)])
+
+        return are_valid
 
 
 class FilterByLateralAcceleration(ActionSpecFilter):
