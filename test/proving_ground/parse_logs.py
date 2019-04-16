b83a3970bd8afcebf637224a7721079dfa02d3e1
RoiReshef <roi.reshef@gm.com>
2019-04-16 18:34:14 +0300

    add trajectory information

--- test/proving_ground/parse_logs.py
+++ test/proving_ground/parse_logs.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from decision_making.paths import Paths
-from decision_making.src.global_constants import NEGLIGIBLE_DISPOSITION_LAT, NEGLIGIBLE_DISPOSITION_LON
-from decision_making.src.planning.types import FS_SV, C_V, FS_SX, FS_SA, C_A, C_K
+from decision_making.src.global_constants import NEGLIGIBLE_DISPOSITION_LAT, NEGLIGIBLE_DISPOSITION_LON, EPS
+from decision_making.src.messages.scene_common_messages import Timestamp
+from decision_making.src.planning.types import FS_SV, C_V, FS_SX, FS_SA, C_A, C_K, C_X, C_Y
 from decision_making.src.state.state import EgoState
 
 
@@ -39,6 +40,9 @@ def plot_dynamics(path: str):
     tp_if_lon_err = []
     tp_if_lat_err = []
     tp_if_time = []
+
+    trajectory = []
+    trajectory_time = []
 
     while True:
         text = f.readline()
@@ -108,35 +112,46 @@ def plot_dynamics(path: str):
                 tp_if_lat_err.append(errors[1])
                 tp_if_time.append(float(kv_strings[-1].split(': ')[1]))
 
+        if 'Publishing Trajectory' in text:
+            traj_str = text.split('Publishing Trajectory: ')[1]
+            traj_dict = ast.literal_eval(traj_str)
+            points_counter = int(traj_dict['s_Data']['e_Cnt_NumValidTrajectoryWaypoints'])
+            timestamp = Timestamp(traj_dict['s_Data']['s_Timestamp']['e_Cnt_Secs'], traj_dict['s_Data']['s_Timestamp']['e_Cnt_FractionSecs'])
+            trajectory.append(np.array(traj_dict['s_Data']['a_TrajectoryWaypoints']['array']).reshape(
+                traj_dict['s_Data']['a_TrajectoryWaypoints']['shape'])[:points_counter])
+            trajectory_time.append(timestamp.timestamp_in_seconds)
+
+
 
     f = plt.figure(1)
-    plt.plot(411)
-    ax1 = plt.subplot(6,  1, 1)
-    ego_sv_plot,  = plt.plot(timestamp_in_sec, ego_sv)
-    other_sv_plot,  = plt.plot(timestamp_in_sec, other_sv)
-    plt.xlabel('time[s]')
-    plt.ylabel('velocity[m/s]')
-    plt.legend([ego_sv_plot, other_sv_plot], ['ego_sv', 'other_sv'])
+    plt.plot(521)
 
-    ax2 = plt.subplot(6,  1, 2, sharex=ax1)
-    ego_sa_plot,  = plt.plot(timestamp_in_sec, ego_sa)
-    plt.xlabel('time[s]')
-    plt.ylabel('acceleration[m]')
-    plt.legend([ego_sa_plot], ['ego_sa'])
-
-    ax3 = plt.subplot(6,  1, 3, sharex=ax1)
+    ax1 = plt.subplot(5, 2, 1)
     ego_cx_plot,  = plt.plot(timestamp_in_sec, ego_sx)
     other_cx_plot,  = plt.plot(timestamp_in_sec, other_sx)
     plt.xlabel('time[s]')
     plt.ylabel('longitude[m]')
     plt.legend([ego_cx_plot, other_cx_plot], ['ego_s', 'other_s'])
 
-    ax4 = plt.subplot(6,  1, 4, sharex=ax1)
+    ax2 = plt.subplot(5, 2, 3, sharex=ax1)
+    ego_sv_plot,  = plt.plot(timestamp_in_sec, ego_sv)
+    other_sv_plot,  = plt.plot(timestamp_in_sec, other_sv)
+    plt.xlabel('time[s]')
+    plt.ylabel('velocity[m/s]')
+    plt.legend([ego_sv_plot, other_sv_plot], ['ego_sv', 'other_sv'])
+
+    ax3 = plt.subplot(5, 2, 5, sharex=ax1)
+    ego_sa_plot,  = plt.plot(timestamp_in_sec, ego_sa)
+    plt.xlabel('time[s]')
+    plt.ylabel('acceleration[m]')
+    plt.legend([ego_sa_plot], ['ego_sa'])
+
+    ax4 = plt.subplot(5, 2, 7, sharex=ax1)
     plt.plot(recipe_time, recipe_desc, 'o--')
     plt.xlabel('time[s]')
     plt.ylabel('recipe')
 
-    ax5 = plt.subplot(6,  1, 5, sharex=ax1)
+    ax5 = plt.subplot(5, 2, 9, sharex=ax1)
     spec_t_plot,  = plt.plot(spec_time, spec_t, 'o-')
     spec_v_plot,  = plt.plot(spec_time, spec_v, 'o-')
     # spec_s_plot,  = plt.plot(spec_time, spec_s)
@@ -145,7 +160,7 @@ def plot_dynamics(path: str):
     plt.ylabel('sepc_time/spec_velocity')
     plt.legend([spec_t_plot, spec_v_plot], ['spec_t [s]', 'spec_v [m/s]'])
 
-    ax6 = plt.subplot(6, 1, 6, sharex=ax1)
+    ax6 = plt.subplot(5, 2, 2, sharex=ax1)
     bp_if_lon,  = plt.plot(bp_if_time, bp_if_lon_err, 'o-.')
     bp_if_lat,  = plt.plot(bp_if_time, bp_if_lat_err, 'o--')
     tp_if_lon,  = plt.plot(tp_if_time, tp_if_lon_err, 'o-.')
@@ -159,6 +174,35 @@ def plot_dynamics(path: str):
     plt.ylabel('loc/trakcing errors')
     plt.legend([bp_if_lon, bp_if_lat, tp_if_lon, tp_if_lat, lon_th, lat_th],
                ['BP-Lon', 'BP-Lat', 'TP-Lon', 'TP-Lat', 'Lon threshold', 'Lat threshold'])
+
+
+    ax7 = plt.subplot(5, 2, 4, sharex=ax1)
+    for t, traj in zip(trajectory_time, trajectory):
+        plt.plot(t + np.arange(len(traj)) * 0.1, traj[:, C_X], '-.')
+
+    plt.xlabel('time[s]')
+    plt.ylabel('trajectories (x position)')
+
+    ax8 = plt.subplot(5, 2, 6, sharex=ax1)
+    for t, traj in zip(trajectory_time, trajectory):
+        plt.plot(t + np.arange(len(traj)) * 0.1, traj[:, C_Y], '-.')
+
+    plt.xlabel('time[s]')
+    plt.ylabel('trajectories (y position)')
+
+    ax9 = plt.subplot(5, 2, 8, sharex=ax1)
+    for t, traj in zip(trajectory_time, trajectory):
+        plt.plot(t + np.arange(len(traj)) * 0.1, traj[:, C_V], '-.')
+
+    plt.xlabel('time[s]')
+    plt.ylabel('trajectories (vel.)')
+
+    ax10 = plt.subplot(5, 2, 10, sharex=ax1)
+    for t, traj in zip(trajectory_time, trajectory):
+        plt.plot(t + np.arange(len(traj)) * 0.1, traj[:, C_A], '-.')
+
+    plt.xlabel('time[s]')
+    plt.ylabel('trajectories (acc.)')
 
     plt.show()
 
