9e21f722bed2704d9224e2453e9bab7f68c1c2e3
mz8cj6 <vlad.goldner@gm.com>
2019-05-07 17:44:28 +0300

    in BP for chosen action in only_padding_mode, create baseline samplable trajectory with linear polynomials

--- src/planning/behavioral/planner/cost_based_behavioral_planner.py
+++ src/planning/behavioral/planner/cost_based_behavioral_planner.py
@@ -1,6 +1,7 @@
 import numpy as np
 import six
 from abc import abstractmethod, ABCMeta
+from decision_making.src.planning.utils.kinematics_utils import KinematicUtils
 from logging import Logger
 from typing import Optional, List, Dict
 
@@ -147,15 +148,22 @@ class CostBasedBehavioralPlanner:
         :return: a SamplableWerlingTrajectory object
         """
         # Note: We create the samplable trajectory as a reference trajectory of the current action.
-        # We assume correctness only of the longitudinal axis, and set T_d to be equal to T_s.
-        A_inv = np.linalg.inv(QuinticPoly1D.time_constraints_matrix(action_spec.t))
         goal_fstate = action_spec.as_fstate()
+        if action_spec.only_padding_mode:
+            # in case of very short action, create samplable trajectory using linear polynomials from ego time,
+            # such that it passes through the goal at goal time
+            ego_by_goal_state = np.copy(goal_fstate)
+            ego_by_goal_state[FS_SX] -= action_spec.v * action_spec.t
+            poly_coefs_s, poly_coefs_d = KinematicUtils.create_linear_profile_polynomials(ego_by_goal_state)
+        else:
+            # We assume correctness only of the longitudinal axis, and set T_d to be equal to T_s.
+            A_inv = np.linalg.inv(QuinticPoly1D.time_constraints_matrix(action_spec.t))
 
-        constraints_s = np.concatenate((ego_fstate[FS_SX:(FS_SA + 1)], goal_fstate[FS_SX:(FS_SA + 1)]))
-        constraints_d = np.concatenate((ego_fstate[FS_DX:(FS_DA + 1)], goal_fstate[FS_DX:(FS_DA + 1)]))
+            constraints_s = np.concatenate((ego_fstate[FS_SX:(FS_SA + 1)], goal_fstate[FS_SX:(FS_SA + 1)]))
+            constraints_d = np.concatenate((ego_fstate[FS_DX:(FS_DA + 1)], goal_fstate[FS_DX:(FS_DA + 1)]))
 
-        poly_coefs_s = QuinticPoly1D.solve(A_inv, constraints_s[np.newaxis, :])[0]
-        poly_coefs_d = QuinticPoly1D.solve(A_inv, constraints_d[np.newaxis, :])[0]
+            poly_coefs_s = QuinticPoly1D.solve(A_inv, constraints_s[np.newaxis, :])[0]
+            poly_coefs_d = QuinticPoly1D.solve(A_inv, constraints_d[np.newaxis, :])[0]
 
         minimal_horizon = trajectory_parameters.trajectory_end_time - timestamp
 
