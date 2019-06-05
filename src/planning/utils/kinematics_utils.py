507ff67e9196f6621b31be7d3a7ce20710bf760a
Gabi <gabi.guetta@gm.com>
2019-06-05 16:52:14 +0300

    added eps to filter

--- src/planning/utils/kinematics_utils.py
+++ src/planning/utils/kinematics_utils.py
@@ -1,7 +1,6 @@
 import numpy as np
 from decision_making.src.global_constants import FILTER_V_T_GRID, FILTER_V_0_GRID, BP_JERK_S_JERK_D_TIME_WEIGHTS, \
-    LON_ACC_LIMITS, TRAJECTORY_TIME_RESOLUTION
-from decision_making.src.global_constants import MAX_CURVATURE
+    LON_ACC_LIMITS, TRAJECTORY_TIME_RESOLUTION, EPS
 from decision_making.src.planning.behavioral.data_objects import AggressivenessLevel
 from decision_making.src.planning.types import C_V, C_A, C_K, Limits, FrenetState2D, FS_SV, FS_SX
 from decision_making.src.planning.types import CartesianExtendedTrajectories
@@ -67,7 +66,7 @@ class KinematicUtils:
         # (2) applies negative acceleration to reduce velocity until it reaches the desired velocity, if necessary
         # (3) keeps the velocity under the desired velocity limit.
         conforms_desired = np.logical_or(
-            np.all(np.logical_or(lon_acceleration < 0, lon_velocity <= desired_velocity), axis=1),
+            np.all(np.logical_or(lon_acceleration < 0, lon_velocity <= desired_velocity + EPS), axis=1),
             (lon_acceleration[:, 0] > lon_acceleration[:, 1]))
 
         # check velocity and acceleration limits
