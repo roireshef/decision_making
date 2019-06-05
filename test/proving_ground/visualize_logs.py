19e193baab09d41d7f6f87488320d966699141f4
Gabi <gabi.guetta@gm.com>
2019-06-05 16:55:07 +0300

    fixed bug

--- test/proving_ground/visualize_logs.py
+++ test/proving_ground/visualize_logs.py
@@ -75,14 +75,14 @@ def plot_dynamics(log_file_path: str):
                 other_cv.append(0.0)
                 other_sv.append(0.0)
                 other_sx.append(0.0)
+                euclid_dist.append(0.0)
             else:
                 other_cv.append(dyn_obj_list[0]['_cached_cartesian_state']['array'][C_V])
                 other_sv.append(dyn_obj_list[0]['_cached_map_state']['lane_fstate']['array'][FS_SV])
                 other_sx.append(dyn_obj_list[0]['_cached_map_state']['lane_fstate']['array'][FS_SX])
-
-            ego_cx_cy = np.array(state_dict['ego_state']['_cached_cartesian_state']['array'][C_X: C_Y + 1])
-            other_cx_cy = np.array(dyn_obj_list[0]['_cached_cartesian_state']['array'][C_X: C_Y + 1])
-            euclid_dist.append(np.linalg.norm(ego_cx_cy - other_cx_cy))
+                ego_cx_cy = np.array(state_dict['ego_state']['_cached_cartesian_state']['array'][C_X: C_Y + 1])
+                other_cx_cy = np.array(dyn_obj_list[0]['_cached_cartesian_state']['array'][C_X: C_Y + 1])
+                euclid_dist.append(np.linalg.norm(ego_cx_cy - other_cx_cy))
 
         if 'Chosen behavioral action spec' in text:
             spec_str = text.split('Chosen behavioral action spec ')[1]
