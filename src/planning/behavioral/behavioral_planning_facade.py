ea4354e7c4eca3062784be40301e49894d71e3dc
RoiReshef <roi.reshef@gm.com>
2019-06-11 11:59:42 +0300

    added get_scene_static breakdown

--- src/planning/behavioral/behavioral_planning_facade.py
+++ src/planning/behavioral/behavioral_planning_facade.py
@@ -252,13 +252,15 @@ class BehavioralPlanningFacade(DmModule):
         return takeover_message
 
     def _get_current_scene_static(self) -> SceneStatic:
-        is_success, serialized_scene_static = self.pubsub.get_latest_sample(topic=UC_SYSTEM_SCENE_STATIC, timeout=1)
+        with DMProfiler(f'{self.__class__.__name__}._get_current_scene_static.get_latest_sample'):
+            is_success, serialized_scene_static = self.pubsub.get_latest_sample(topic=UC_SYSTEM_SCENE_STATIC, timeout=1)
 
         # TODO Move the raising of the exception to LCM code. Do the same in trajectory facade
         if serialized_scene_static is None:
             raise MsgDeserializationError("Pubsub message queue for %s topic is empty or topic isn\'t subscribed" %
                                           UC_SYSTEM_SCENE_STATIC)
-        scene_static = SceneStatic.deserialize(serialized_scene_static)
+        with DMProfiler(f'{self.__class__.__name__}._get_current_scene_static.deserialize'):
+            scene_static = SceneStatic.deserialize(serialized_scene_static)
         if scene_static.s_Data.s_SceneStaticBase.e_Cnt_num_lane_segments == 0 and scene_static.s_Data.s_SceneStaticBase.e_Cnt_num_road_segments == 0:
             raise MsgDeserializationError("SceneStatic map was received without any road or lanes")
         self.logger.debug("%s: %f" % (LOG_MSG_SCENE_STATIC_RECEIVED, scene_static.s_Header.s_Timestamp.timestamp_in_seconds))
