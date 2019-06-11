f0c9646eba5b845be200b262a95f957fc7afe6be
Anthony Perez <anthony.perez@gm.com>
2019-06-11 12:57:18 -0400

    Added extra lines because of PEP 8

--- src/exceptions.py
+++ src/exceptions.py
@@ -124,6 +124,7 @@ class ConstraintFilterHaltWithValue(Exception):
     def value(self) -> bool:
         return self._value
 
+
 class NoActionsLeftForBPError(Exception):
     pass
 
@@ -145,35 +146,46 @@ class EgoStationBeyondLaneLength(MappingException):
 class RoutePlanningException(Exception):
     pass
 
+
 class RepeatedRoadSegments(RoutePlanningException):
     pass
+
 
 class EgoRoadSegmentNotFound(RoutePlanningException):
     pass
 
+
 class EgoLaneOccupancyCostIncorrect(RoutePlanningException):
     pass
+
 
 class RoadSegmentLaneSegmentMismatch(RoutePlanningException):
     pass
 
+
 class MissingInputInformation(RoutePlanningException):
     pass
+
 
 class NavigationSceneDataMismatch(RoutePlanningException):
     pass
 
+
 class LaneSegmentDataNotFound(RoutePlanningException):
     pass
+
 
 class RoadSegmentDataNotFound(RoutePlanningException):
     pass
 
+
 class LaneAttributeNotFound(RoutePlanningException):
     pass
 
+
 class DownstreamLaneDataNotFound(RoutePlanningException):
     pass
+
 
 def raises(*e):
     # type: (Exception)
