af58b9fcf1d61721e133639679fa1ef565bbd44f
mz8cj6 <vlad.goldner@gm.com>
2019-06-13 18:22:40 +0300

    add constant TP_DESIRED_VELOCITY_DEVIATION

--- src/global_constants.py
+++ src/global_constants.py
@@ -56,6 +56,10 @@ LAT_JERK_COST_WEIGHT = 1.0                  # cost of lateral jerk
 # [m/sec] speed to plan towards by default in BP
 BEHAVIORAL_PLANNING_DEFAULT_DESIRED_SPEED = 90/3.6  # TODO - get this value from the map
 
+# [m/sec] the addition to BEHAVIORAL_PLANNING_DEFAULT_DESIRED_SPEED for TP
+# we allow higher desired velocity in TP than in BP because TP & BP are not synchronized
+TP_DESIRED_VELOCITY_DEVIATION = 1
+
 # [m/s] min & max velocity limits are additional parameters for TP and for Static Recipe enumeration
 VELOCITY_LIMITS = np.array([0.0, 100/3.6])
 VELOCITY_STEP = 10/3.6
