07c1427a709942e8ce87d8816757e4be89e118e3
Anthony Perez <anthony.perez@gm.com>
2019-07-25 13:33:09 -0400

    Removed functionality related to mirroring objects
    This functionality and associated unit tests should be added wtih the
    rest of the actor projection work later. It is not related to the GFF
    extension work.

--- test/planning/behavioral/test_behavioralGridState.py
+++ test/planning/behavioral/test_behavioralGridState.py
@@ -1,17 +1,11 @@
-import numpy as np
 from decision_making.src.planning.behavioral.behavioral_grid_state import BehavioralGridState
 from decision_making.src.planning.behavioral.data_objects import RelativeLane
 from decision_making.src.planning.types import FS_SX
 from decision_making.src.utils.map_utils import MapUtils
 from rte.python.logger.AV_logger import AV_Logger
-from decision_making.src.messages.scene_static_message import SceneStatic
-from decision_making.src.scene.scene_static_model import SceneStaticModel
-from decision_making.src.state.state import DynamicObject, MapState, ObjectSize
-
 import numpy as np
 from decision_making.test.planning.behavioral.behavioral_state_fixtures import behavioral_grid_state, \
     state_with_sorrounding_objects, state_with_surrounding_objects_and_off_map_objects, route_plan_20_30
-from decision_making.test.messages.scene_static_fixture import left_lane_split_scene_static, scene_static_short_testable
 
 def test_createFromState_8objectsAroundEgo_correctGridSize(state_with_sorrounding_objects, route_plan_20_30):
     """
@@ -62,26 +56,3 @@ def test_calculateLongitudinalDifferences_8objectsAroundEgo_accurate(state_with_
         target_gff_fstate = behavioral_grid_state.extended_lane_frames[rel_lane].convert_from_segment_state(
             map_state.lane_fstate, map_state.lane_id)
         assert longitudinal_distances[i] == target_gff_fstate[FS_SX] - behavioral_grid_state.projected_ego_fstates[rel_lane][FS_SX]
-
-def test_createMirrorObjects_laneSplit_carInOverlap(left_lane_split_scene_static: SceneStatic):
-    """
-    Validate that mirror object is correctly placed in overlapping lane
-    """
-    SceneStaticModel.get_instance().set_scene_static(left_lane_split_scene_static)
-    # Create other car in lane 21, which overlaps with lane 22
-    dyn_obj = DynamicObject.create_from_map_state(obj_id=10, timestamp=5, map_state=MapState(np.array([1,1,0,0,0,0]), 21),
-                                                  size=ObjectSize(1,1,1), confidence=1, off_map=False)
-    all_objects = BehavioralGridState._create_mirror_objects([dyn_obj])
-    mirror_objects = [obj for obj in all_objects if obj.obj_id == -10]
-    assert mirror_objects[0].map_state.lane_id == 22
-
-def test_createMirrorObjects_laneSplit_carNotInOverlap(scene_static_short_testable: SceneStatic):
-    """
-    Validate that no mirror object is created if there is no overlap
-    """
-    SceneStaticModel.get_instance().set_scene_static(scene_static_short_testable)
-    # Create other car in lane 21 which does NOT overlap with any other lane
-    dyn_obj = DynamicObject.create_from_map_state(obj_id=10, timestamp=5, map_state=MapState(np.array([1,1,0,0,0,0]), 21),
-                                                  size=ObjectSize(1,1,1), confidence=1, off_map=False)
-    all_objects = BehavioralGridState._create_mirror_objects([dyn_obj])
-    assert len(all_objects) == 1 and all_objects[0] == dyn_obj
