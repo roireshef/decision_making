a442b8646f7fde3ef83ac9987908b7f061fd5215
Rouhollah Jafari <rouhollah.jafari@gm.com>
2019-08-09 10:22:05 -0400

    moved the common functions from binary_cost class to route_plan class

--- src/planning/route/route_planner.py
+++ src/planning/route/route_planner.py
@@ -3,11 +3,14 @@ from abc import ABCMeta, abstractmethod
 from collections import OrderedDict
 from typing import List, Dict, Optional
 import rte.python.profiler as prof
-from decision_making.src.messages.route_plan_message import DataRoutePlan, RoutePlanRoadSegment
+from decision_making.src.messages.route_plan_message import DataRoutePlan, RoutePlanRoadSegment, RoutePlanLaneSegment
 from decision_making.src.messages.scene_static_message import SceneStaticBase, NavigationPlan, \
     SceneRoadSegment, SceneLaneSegmentBase
 from decision_making.src.exceptions import MissingInputInformation, RepeatedRoadSegments, raises,\
-    NavigationSceneDataMismatch, LaneSegmentDataNotFound, RoadSegmentDataNotFound
+    NavigationSceneDataMismatch, LaneSegmentDataNotFound, RoadSegmentDataNotFound, LaneAttributeNotFound
+from decision_making.src.messages.scene_static_enums import RoutePlanLaneSegmentAttr, LaneMappingStatusType, \
+    MapLaneDirection, GMAuthorityType, LaneConstructionType, ManeuverType
+from decision_making.src.global_constants import LANE_ATTRIBUTE_CONFIDENCE_THRESHOLD, HIGH_COST, LOW_COST
 
 RoadSegmentDict = Dict[int, SceneRoadSegment]
 LaneSegmentBaseDict = Dict[int, SceneLaneSegmentBase]
@@ -251,6 +254,139 @@ class RoutePlanner(metaclass=ABCMeta):
     def __init__(self):
         self._route_plan_lane_segments_reversed: RoutePlanRoadSegment = []
         self._route_plan_input_data: RoutePlannerInputData = None
+
+    def get_route_plan_lane_segments(self) -> RoutePlanRoadSegment:
+        return list(reversed(self._route_plan_lane_segments_reversed))
+
+    @staticmethod
+    # Following method is kept public in order to unit test the method from outside the class
+    def mapping_status_based_occupancy_cost(mapping_status_attribute: LaneMappingStatusType) -> float:
+        """
+        Cost of lane map type. Current implementation is binary cost.
+        :param mapping_status_attribute: type of mapped
+        :return: normalized cost (LOW_COST to HIGH_COST)
+        """
+        if ((mapping_status_attribute == LaneMappingStatusType.CeSYS_e_LaneMappingStatusType_HDMap) or
+                (mapping_status_attribute == LaneMappingStatusType.CeSYS_e_LaneMappingStatusType_MDMap)):
+            return LOW_COST
+        return HIGH_COST
+
+    @staticmethod
+    # Following method is kept public in order to unit test the method from outside the class
+    def construction_zone_based_occupancy_cost(construction_zone_attribute: LaneConstructionType) -> float:
+        """
+        Cost of construction zone type. Current implementation is binary cost.
+        :param construction_zone_attribute: type of lane construction
+        :return: Normalized cost (LOW_COST to HIGH_COST)
+        """
+        if ((construction_zone_attribute == LaneConstructionType.CeSYS_e_LaneConstructionType_Normal) or
+                (construction_zone_attribute == LaneConstructionType.CeSYS_e_LaneConstructionType_Unknown)):
+            return LOW_COST
+        return HIGH_COST
+
+    @staticmethod
+    # Following method is kept public in order to unit test the method from outside the class
+    def lane_dir_in_route_based_occupancy_cost(lane_dir_in_route_attribute: MapLaneDirection) -> float:
+        """
+        Cost of lane direction. Current implementation is binary cost.
+        :param lane_dir_in_route_attribute: map lane direction in respect to host
+        :return: Normalized cost (LOW_COST to HIGH_COST)
+        """
+        if ((lane_dir_in_route_attribute == MapLaneDirection.CeSYS_e_MapLaneDirection_SameAs_HostVehicle) or
+                (lane_dir_in_route_attribute == MapLaneDirection.CeSYS_e_MapLaneDirection_Left_Towards_HostVehicle) or
+                (lane_dir_in_route_attribute == MapLaneDirection.CeSYS_e_MapLaneDirection_Right_Towards_HostVehicle)):
+            return LOW_COST
+        return HIGH_COST
+
+    @staticmethod
+    # Following method is kept public in order to unit test the method from outside the class
+    def gm_authority_based_occupancy_cost(gm_authority_attribute: GMAuthorityType) -> float:
+        """
+        Cost of GM authorized driving area. Current implementation is binary cost.
+        :param gm_authority_attribute: type of GM authority
+        :return: Normalized cost (LOW_COST to HIGH_COST)
+        """
+        if gm_authority_attribute == GMAuthorityType.CeSYS_e_GMAuthorityType_None:
+            return LOW_COST
+        return HIGH_COST
+
+    @staticmethod
+    @raises(LaneAttributeNotFound)
+    # Following method is kept public in order to unit test the method from outside the class
+    def lane_attribute_based_occupancy_cost(lane_attribute_index: int,
+                                            lane_attribute_value: int) -> float:  # if else logic
+        """
+        This method is a wrapper on the individual lane attribute cost calculations and arbitrates
+        according to the (input) lane attribute, which lane attribute method to invoke
+        :param lane_attribute_index: pointer to the concerned lane attribute in RoutePlanLaneSegmentAttr enum
+        :param lane_attribute_value: value of the pointed lane attribute
+        :return: Normalized lane occupancy cost based on the concerned lane attribute (LOW_COST to HIGH_COST)
+        """
+        if 'attribute_based_occupancy_cost_methods' not in RoutePlanner.lane_attribute_based_occupancy_cost.__dict__:
+            # The above if check and then setting of attribute_based_occupancy_cost_methods within the if block is equivalent of
+            # making attribute_based_occupancy_cost_methods a static dictionary (of [lane_attribute_index, lane attribute based occupancy_cost
+            # calculation methods])
+            attribute_based_occupancy_cost_methods = {
+                RoutePlanLaneSegmentAttr.CeSYS_e_RoutePlanLaneSegmentAttr_MappingStatus:
+                    RoutePlanner.mapping_status_based_occupancy_cost,
+
+                RoutePlanLaneSegmentAttr.CeSYS_e_RoutePlanLaneSegmentAttr_GMFA:
+                    RoutePlanner.gm_authority_based_occupancy_cost,
+
+                RoutePlanLaneSegmentAttr.CeSYS_e_RoutePlanLaneSegmentAttr_Construction:
+                    RoutePlanner.construction_zone_based_occupancy_cost,
+
+                RoutePlanLaneSegmentAttr.CeSYS_e_RoutePlanLaneSegmentAttr_Direction:
+                    RoutePlanner.lane_dir_in_route_based_occupancy_cost
+            }
+
+        if lane_attribute_index in attribute_based_occupancy_cost_methods:
+            # Following is equivalent of (pythonic way) executing a switch statement
+            occupancy_cost_method = attribute_based_occupancy_cost_methods[lane_attribute_index]  #
+            return occupancy_cost_method(lane_attribute_value)
+        else:
+            raise LaneAttributeNotFound(
+                'Binary Cost Based Route Planner: lane_attribute_index {0} not supported'.format(lane_attribute_index))
+
+    @staticmethod
+    @raises(LaneAttributeNotFound)
+    # Following method is kept public in order to unit test the method from outside the class
+    def lane_occupancy_cost_calc(lane_segment_base_data: SceneLaneSegmentBase) -> float:
+        """
+        Calculates lane occupancy cost for a single lane segment
+        :param lane_segment_base_data: SceneLaneSegmentBase for the concerned lane
+        :return: LaneOccupancyCost, cost to the AV if it occupies the lane.
+        """
+
+        # Now iterate over all the active lane attributes for the lane segment
+        for lane_attribute_index in lane_segment_base_data.a_i_active_lane_attribute_indices:
+            # lane_attribute_index gives the index lookup for lane attributes and confidences
+            if lane_attribute_index < len(lane_segment_base_data.a_cmp_lane_attributes):
+                lane_attribute_value = lane_segment_base_data.a_cmp_lane_attributes[lane_attribute_index]
+            else:
+                raise LaneAttributeNotFound(
+                    'Binary Cost Based Route Planner: lane_attribute_index {0} doesn\'t have corresponding lane attribute value'
+                    .format(lane_attribute_index))
+
+            if lane_attribute_index < len(lane_segment_base_data.a_cmp_lane_attribute_confidences):
+                lane_attribute_confidence = lane_segment_base_data.a_cmp_lane_attribute_confidences[
+                    lane_attribute_index]
+            else:
+                raise LaneAttributeNotFound(
+                    'Binary Cost Based Route Planner: lane_attribute_index {0} doesn\'t have corresponding lane attribute '
+                    'confidence value'.format(lane_attribute_index))
+
+            if (lane_attribute_confidence < LANE_ATTRIBUTE_CONFIDENCE_THRESHOLD):
+                continue
+
+            lane_attribute_occupancy_cost = RoutePlanner.lane_attribute_based_occupancy_cost(
+                lane_attribute_index=lane_attribute_index, lane_attribute_value=lane_attribute_value)
+            # Check if the lane is unoccupiable
+            if (lane_attribute_occupancy_cost == HIGH_COST):
+                return HIGH_COST
+
+        return LOW_COST
+
     @abstractmethod
     def plan(self, route_plan_input_data: RoutePlannerInputData) -> DataRoutePlan:
         """Abstract route planner method. Implementation details will be in child class/methods """
