7c70962c44acff51dbb112e5515702fd74b16d07
DudiBenEli <dudy.beneli@gm.com>
2019-08-20 10:24:09 +0300

    merge with master

--- src/planning/behavioral/default_config.py
+++ src/planning/behavioral/default_config.py
@@ -2,9 +2,7 @@ from decision_making.src.global_constants import BEHAVIORAL_PLANNING_NAME_FOR_LO
 from decision_making.src.planning.behavioral.filtering.action_spec_filter_bank import FilterIfNone as ASpecFilterIfNone, \
     FilterForKinematics, FilterForSafetyTowardsTargetVehicle, StaticTrafficFlowControlFilter, \
     BeyondSpecStaticTrafficFlowControlFilter, BeyondSpecCurvatureFilter, FilterForLaneSpeedLimits, \
-    BeyondSpecSpeedLimitFilter, FilterStopActionIfTooSoonByTime
-    BeyondSpecStaticTrafficFlowControlFilter, BeyondSpecCurvatureFilter, FilterForLaneSpeedLimits, \
-    BeyondSpecSpeedLimitFilter, FilterForSLimit
+    BeyondSpecSpeedLimitFilter, FilterStopActionIfTooSoonByTime, FilterForSLimit
 from decision_making.src.planning.behavioral.filtering.action_spec_filtering import ActionSpecFiltering
 from decision_making.src.planning.behavioral.filtering.recipe_filter_bank import FilterIfNone as RecipeFilterIfNone, \
     FilterActionsTowardsNonOccupiedCells, FilterActionsTowardBackAndParallelCells, FilterOvertakeActions, \
