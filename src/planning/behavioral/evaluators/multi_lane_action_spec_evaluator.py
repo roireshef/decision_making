5b6af39e4ae778d934b331fb96eb1a601678ebf7
Anthony Perez <anthony.perez@gm.com>
2019-08-20 19:59:40 -0400

    Bug fix
    LEFT_LANE was used instead of RIGHT_LANE

--- src/planning/behavioral/evaluators/multi_lane_action_spec_evaluator.py
+++ src/planning/behavioral/evaluators/multi_lane_action_spec_evaluator.py
@@ -43,10 +43,10 @@ class MultiLaneActionSpecEvaluator(ActionSpecEvaluator):
         minimum_cost_lane = RelativeLane.SAME_LANE
 
         is_left_augmented = RelativeLane.LEFT_LANE in gffs and (gffs[RelativeLane.LEFT_LANE].gff_type == GFF_Type.Augmented or
-                                               gffs[RelativeLane.LEFT_LANE].gff_type == GFF_Type.AugmentedPartial)
+                                                                gffs[RelativeLane.LEFT_LANE].gff_type == GFF_Type.AugmentedPartial)
 
         is_right_augmented = RelativeLane.RIGHT_LANE in gffs and (gffs[RelativeLane.RIGHT_LANE].gff_type == GFF_Type.Augmented or
-                                                gffs[RelativeLane.RIGHT_LANE].gff_type == GFF_Type.AugmentedPartial)
+                                                                  gffs[RelativeLane.RIGHT_LANE].gff_type == GFF_Type.AugmentedPartial)
 
         diverging_indices = {}
 
@@ -56,7 +56,7 @@ class MultiLaneActionSpecEvaluator(ActionSpecEvaluator):
 
         if is_right_augmented:
             # Find where gffs[RelativeLane.RIGHT_LANE].segment_ids and gffs[RelativeLane.SAME_LANE].segment_ids begin to diverge
-            diverging_indices[RelativeLane.RIGHT_LANE] = np.argwhere(gffs[RelativeLane.LEFT_LANE].segment_ids != gffs[RelativeLane.SAME_LANE].segment_ids)[0][0]
+            diverging_indices[RelativeLane.RIGHT_LANE] = np.argwhere(gffs[RelativeLane.RIGHT_LANE].segment_ids != gffs[RelativeLane.SAME_LANE].segment_ids)[0][0]
 
         # After determining if the left or right lane diverges first, look at the lane occupancy costs for the lanes where the divergence occurs.
         # Target the lane with the lower cost.
