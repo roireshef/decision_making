e291ff2a04ed3aa0fa04f17753bd104bbc99c06d
Anthony Perez <anthony.perez@gm.com>
2019-08-20 23:36:35 -0400

    Making constant values explicit floats

--- src/global_constants.py
+++ src/global_constants.py
@@ -25,7 +25,7 @@ PLANNING_LOOKAHEAD_DIST = 100.0
 MAX_HORIZON_DISTANCE = 400
 
 # [m] distance to the end of a partial GFF at which the vehicle must not be in
-PARTIAL_GFF_END_PADDING = 5
+PARTIAL_GFF_END_PADDING = 5.0
 
 # The necessary lateral margin in [m] that needs to be taken in order to assume that it is not in car's way
 LATERAL_SAFETY_MARGIN_FROM_OBJECT = 0.0
@@ -34,7 +34,7 @@ LATERAL_SAFETY_MARGIN_FROM_OBJECT = 0.0
 MINIMUM_REQUIRED_COST_DIFFERENCE = 0.1
 
 # The minimum distance in [m] of valid lane for a GFF to exist
-MINIMUM_REQUIRED_DIST_LANE_AHEAD = 1
+MINIMUM_REQUIRED_DIST_LANE_AHEAD = 1.0
 
 # Prefer left or right split when the costs are the same
 PREFER_LEFT_SPLIT_OVER_RIGHT_SPLIT = False
