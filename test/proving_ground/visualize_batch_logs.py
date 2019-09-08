284fc1a6646c7f886c61d6a17257eb859bfb3f00
DudiBenEli <dudy.beneli@gm.com>
2019-09-08 17:37:44 +0300

    support more semantic scenarios

--- test/proving_ground/visualize_batch_logs.py
+++ test/proving_ground/visualize_batch_logs.py
@@ -9,7 +9,7 @@ from matplotlib.backends.backend_pdf import PdfPages
 if __name__ == "__main__":
     path = Paths.get_repo_path()
     # Enter path of json file to analyze here:
-    json_filename = path + '/../simulation/ground_truth_data/sim_metadata/run_info_file_2019_09_04_09_20_52_743122.json'
+    json_filename = path + '/../simulation/ground_truth_data/sim_metadata/run_info_file_2019_09_08_15_56_09_054772.json'
     with open(json_filename, 'r') as f:
         batch_data = f.read()
         batch_data = "{" + batch_data.replace("\n", "").replace("\'", "")[0:-1] + "}"
@@ -33,6 +33,27 @@ if __name__ == "__main__":
                     ego_initial_velocity = sim["Scenario Params"]["global_params"]["general"]["initial_velocity"]
                     player_velocity = sim["Scenario Params"]["player_params"]["slow_player"]["v2"]
                     sim_title = sim_type + "_" + str(round(ego_initial_velocity, 1)) + "_" + str(round(player_velocity, 1))
+                elif sim_type == "LANE_SPLITS_NO_ACTORS" or sim_type == "SPEED_LIMIT_CHANGE":
+                    track_pos_pair = sim["Scenario Params"]["global_params"]["Start_and_stop_Track_Positions"]["track_pos_pair"]
+                    sim_title = sim_type + "_" + track_pos_pair
+                elif sim_type == "LANE_SPLITS_ACTOR_FOLLOWING":
+                    ego_initial_velocity = sim["Scenario Params"]["global_params"]["general"]["initial_velocity"]
+                    track_pos_pair = sim["Scenario Params"]["global_params"]["Start_and_stop_Track_Positions"]["track_pos_pair"]
+                    sim_title = sim_type + "_" + track_pos_pair + "_" + str(round(ego_initial_velocity, 1))
+                elif sim_type == "LEAD_ACCEL" or sim_type == "LEAD_DECEL":
+                    ego_initial_velocity = sim["Scenario Params"]["global_params"]["general"]["initial_velocity"]
+                    player_velocity = sim["Scenario Params"]["player_params"]["slow_player"]["v2"]
+                    player_acceleration = sim["Scenario Params"]["player_params"]["stop_n_go"]["a"]
+                    sim_title = sim_type + "_" + str(round(ego_initial_velocity, 1)) + + "_" + str(round(player_acceleration, 1)) + "_" + str(round(player_velocity, 1))
+                elif sim_type == "STOP_SIGN_ACTOR_DRIVE_THROUGH":
+                    override_headway = sim["Scenario Params"]["player_params"]["lane_keeping"]["override_headway"]
+                    sim_title = sim_type + "_" + str(round(override_headway, 1))
+                elif sim_type == "STOP_SIGN_WITH_STOPPED_ACTOR":
+                    rel_dis = sim["Scenario Params"]["player_params"]["Stand_and_start_parameters"]["rel_dis"]
+                    sim_title = sim_type + "_" + str(round(rel_dis))
+                elif sim_type == "STOP_SIGN_NO_ACTORS":
+                    # this case has no varying params
+                    sim_title = sim_type
                 else:
                     sim_title = sim_type + "_" + sim_key
                 log_file_name = path + '/../simulation/ground_truth_data/data/sim_' + sim_key + '/UC_logs/AV_Log_dm_main.log'
