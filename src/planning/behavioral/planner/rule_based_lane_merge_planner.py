95b49452d1880bbcf93b14ded88e0d28acd67b97
Vlad Goldner <mz8cj6@gm.com>
2019-10-16 11:11:19 +0300

    3.8

--- src/planning/behavioral/planner/rule_based_lane_merge_planner.py
+++ src/planning/behavioral/planner/rule_based_lane_merge_planner.py
@@ -24,7 +24,7 @@ class ScenarioParams:
     def __init__(self, worst_case_back_actor_accel: float = WORST_CASE_BACK_CAR_ACCEL,
                  worst_case_front_actor_decel: float = WORST_CASE_FRONT_CAR_DECEL,
                  ego_reaction_time: float = SAFETY_HEADWAY, back_actor_reaction_time: float = SAFETY_HEADWAY,
-                 front_rss_decel: float = -LON_ACC_LIMITS[LIMIT_MIN], back_rss_decel: float = 4.,
+                 front_rss_decel: float = -LON_ACC_LIMITS[LIMIT_MIN], back_rss_decel: float = 3.8,
                  ego_max_velocity: float = MAX_VELOCITY, actors_max_velocity: float = MAX_VELOCITY):
         """
         :param worst_case_back_actor_accel: worst case braking deceleration of front actor prior the merge
