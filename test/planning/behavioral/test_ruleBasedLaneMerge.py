d4fe2ae01d61c00b48a31a6627a330b21089664a
Vlad Goldner <mz8cj6@gm.com>
2019-11-06 09:36:28 +0200

    merge with master

--- test/planning/behavioral/test_ruleBasedLaneMerge.py
+++ test/planning/behavioral/test_ruleBasedLaneMerge.py
@@ -67,16 +67,16 @@ def test_canSolveByRuleBased_fasterBackCarIsNotFarEnough_success():
     assert ret
 
 
-def test_canSolveByRuleBased_closeFrontCarRequiresStrongBrake_failure():
-    ego_fstate = np.array([60, 20, 0])
+def test_canSolveByRuleBased_closeFrontCarRequiresStrongBrake_success():
+    ego_fstate = np.array([37.55195556, 18.16185665,  4.42184403])
     ego_len = 5
-    actor1 = LaneMergeActorState(-50, 25, ego_len)
-    actor2 = LaneMergeActorState(30, 25, ego_len)
+    actor1 = LaneMergeActorState(-32.551955563549306, 25, ego_len)
+    actor2 = LaneMergeActorState(92.6480444364507, 25, ego_len)
     actors = [actor1, actor2]
-    red_line_s = 90
+    red_line_s = 240
     state = LaneMergeState.create_thin_state(ego_len, ego_fstate, actors, red_line_s)
     ret = RuleBasedLaneMergePlanner.choose_max_vel_quartic_trajectory(state)[0]
-    assert not ret
+    assert ret
 
 
 def test_canSolveByRuleBased_failure():
