d7ba7e63c94fc215c0021b655c26316c8de1c8e7
Ksenia Kaganer <ksenia.kaganer@gm.com>
2019-11-18 13:01:44 +0200

    added informative error messages due to map issues

--- src/exceptions.py
+++ src/exceptions.py
@@ -164,7 +164,7 @@ class RoadSegmentLaneSegmentMismatch(RoutePlanningException):
     pass
 
 
-class MissingInputInformation(RoutePlanningException):
+class MissingMapInformation(RoutePlanningException):
     pass
 
 
