cfe8d7ad82564c00ec10d35ffea6a29a4953e23e
Vlad Goldner <mz8cj6@gm.com>
2019-11-19 12:27:18 +0200

    make encode state to be @staticmethod

--- src/planning/behavioral/state/lane_merge_state.py
+++ src/planning/behavioral/state/lane_merge_state.py
@@ -9,12 +9,12 @@ from decision_making.src.planning.behavioral.state.behavioral_grid_state import 
     RoadSemanticOccupancyGrid, DynamicObjectWithRoadSemantics
 from decision_making.src.planning.behavioral.data_objects import RelativeLane, RelativeLongitudinalPosition
 import numpy as np
-from decision_making.src.planning.types import FS_DX, FS_SX, FS_2D_LEN, FrenetState1D, FS_1D_LEN, FrenetState2D, FS_SV
+from decision_making.src.planning.types import FS_DX, FS_SX, FS_2D_LEN, FrenetState1D, FS_1D_LEN, FrenetState2D, FS_SV, \
+    FS_SA
 from decision_making.src.planning.utils.generalized_frenet_serret_frame import GeneralizedFrenetSerretFrame
 from decision_making.src.state.state import State, EgoState, DynamicObject, ObjectSize
 from decision_making.src.utils.map_utils import MapUtils
-from gym.spaces.tuple import Tuple as GymTuple
-import torch
+from gym.spaces.tuple_space import Tuple as GymTuple
 from typing import List, Dict
 
 
@@ -160,33 +160,43 @@ class LaneMergeState(BehavioralGridState):
         ego_fstate2D = np.concatenate((ego_fstate, np.zeros(FS_1D_LEN)))
         return cls(road_occupancy_grid, ego_state, {}, {RelativeLane.SAME_LANE: ego_fstate2D}, red_line_s, target_rel_lane)
 
-    def encode_state(self) -> GymTuple:
+    @staticmethod
+    def encode_state(ego_fstate_1d: FrenetState1D, red_line_s_on_ego_gff: float,
+                     actors_states: List[LaneMergeActorState]) -> GymTuple:
         """
         Encode and normalize the LaneMergeState for RL model usage.
+        :param ego_fstate_1d: 1-dim longitudinal Frenet state of ego
+        :param red_line_s_on_ego_gff: s of the red line on host's GFF
+        :param actors_states: list of actor states
         :return: tuple of host state and actors state (of type torch.tensor)
         """
         # normalize host & actors states
-        host_state = self._encode_host_state() / np.array([LANE_MERGE_STATE_FAR_AWAY_DISTANCE, LANE_MERGE_ACTION_SPACE_MAX_VELOCITY, 1])
-        actors_state = self._encode_actors_state() / np.array([1, LANE_MERGE_ACTION_SPACE_MAX_VELOCITY])[..., np.newaxis]
+        host_state = LaneMergeState._encode_host_state(ego_fstate_1d, red_line_s_on_ego_gff) / \
+                     np.array([LANE_MERGE_STATE_FAR_AWAY_DISTANCE, LANE_MERGE_ACTION_SPACE_MAX_VELOCITY, 1])
+        actors_state = LaneMergeState._encode_actors_state(actors_states) / \
+                       np.array([1, LANE_MERGE_ACTION_SPACE_MAX_VELOCITY])[..., np.newaxis]
         return host_state, actors_state
 
-    def _encode_host_state(self) -> np.array:
+    @staticmethod
+    def _encode_host_state(ego_fstate_1d: FrenetState1D, red_line_s_on_ego_gff: float) -> np.array:
         """
         Encode host state of the LaneMergeState for RL model usage.
+        :param ego_fstate_1d: 1-dim longitudinal Frenet state of ego
+        :param red_line_s_on_ego_gff: s of the red line on host's GFF
         :return: numpy array with encoded host state
         """
-        # encode host
-        host_state = np.copy(self.ego_fstate_1d)
-        # replace the host station coordinate with its distance to red line
-        host_state[FS_SX] = self.red_line_s_on_ego_gff - host_state[FS_SX]
+        # encode host: replace the host station coordinate with its distance to red line
+        host_state = np.array([red_line_s_on_ego_gff - ego_fstate_1d[FS_SX], ego_fstate_1d[FS_SV], ego_fstate_1d[FS_SA]])
         return host_state[np.newaxis, :]
 
-    def _encode_actors_state(self) -> np.array:
+    @staticmethod
+    def _encode_actors_state(actors_states: List[LaneMergeActorState]) -> np.array:
         """
         Encode actors state of the LaneMergeState for RL model usage.
         In the current implementation the road is divided into a grid with resolution
         LANE_MERGE_STATE_OCCUPANCY_GRID_RESOLUTION, and each cell is set 1 iff an actor appears in the cell.
         In addition, the actor's velocity is appended to the appropriated cell.
+        :param actors_states: list of actor states
         :return: numpy array with encoded actors state
         """
         # actors state is an occupancy grid containing the different vehicles' distance from merge and velocity
@@ -197,13 +207,13 @@ class LaneMergeState(BehavioralGridState):
         # init for empty grid cells
         actors_exist_default = np.zeros(shape=(1, num_of_grid_cells))
         actors_vel_default = -LANE_MERGE_ACTION_SPACE_MAX_VELOCITY * np.ones(shape=(1, num_of_grid_cells))
-        actors_states = np.vstack((actors_exist_default, actors_vel_default))
+        encoded_actors_states = np.vstack((actors_exist_default, actors_vel_default))
 
-        for actor in self.actors_states:
+        for actor in actors_states:
             actor_exists = 1
             actor_grid_cell = np.floor(actor.s_relative_to_ego / LANE_MERGE_STATE_OCCUPANCY_GRID_RESOLUTION).\
                                   astype(int) + num_of_onesided_grid_cells
             if 0 <= actor_grid_cell <= num_of_grid_cells - 1:
-                actors_states[:, actor_grid_cell] = np.array([actor_exists, actor.velocity])
+                encoded_actors_states[:, actor_grid_cell] = np.array([actor_exists, actor.velocity])
 
-        return actors_states
+        return encoded_actors_states
