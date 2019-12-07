ae958abd4c5cec6c61c00493828abd8eb9f1c027
RoiReshef <roi.reshef@gm.com>
2019-12-07 12:17:23 +0200

    fixed broken imports

--- src/messages/trajectory_plan_message.py
+++ src/messages/trajectory_plan_message.py
@@ -2,7 +2,8 @@ import numpy as np
 
 from interface.Rte_Types.python.sub_structures.TsSYS_TrajectoryPlan import TsSYSTrajectoryPlan
 from interface.Rte_Types.python.sub_structures.TsSYS_DataTrajectoryPlan import TsSYSDataTrajectoryPlan
-from decision_making.src.messages.serialization import PUBSUB_MSG_IMPL, TRAJECTORY_WAYPOINT_SIZE
+from decision_making.src.messages.serialization import PUBSUB_MSG_IMPL
+from decision_making.src.global_constants import TRAJECTORY_WAYPOINT_SIZE
 from decision_making.src.messages.scene_common_messages import Timestamp, MapOrigin, Header
 
 
