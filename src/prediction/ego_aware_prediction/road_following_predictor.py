ef2df7d213920185dbfe081085b283810003c575
yzystl <gabi.guetta@gm.com>
2019-12-19 17:05:37 +0200

    removed unused method

--- src/prediction/ego_aware_prediction/road_following_predictor.py
+++ src/prediction/ego_aware_prediction/road_following_predictor.py
@@ -55,52 +55,6 @@ class RoadFollowingPredictor(EgoAwarePredictor):
 
         return predicted_objects_states_dict
 
-    def predict_state(self, state: State, prediction_timestamps: np.ndarray,
-                      action_trajectory: SamplableTrajectory) -> (List[State]):
-        """
-        Predicts the future states of the given state, for the specified timestamps
-        :param state: the initial state to begin prediction from
-        :param prediction_timestamps: np array of timestamps in [sec] to predict states for. In ascending order.
-        Global, not relative
-        :param action_trajectory: the ego's planned action trajectory. If given, ego is predicted according to it,
-        otherwise the predicted ego states will be none.
-        :return: a list of non markov predicted states for the requested prediction_timestamp, and a full state for the
-        terminal predicted state
-        """
-
-        # Simple object-wise prediction
-        object_ids = [obj.obj_id for obj in state.dynamic_objects]
-
-        if action_trajectory is not None:
-            extended_sampled_action_trajectory = action_trajectory.sample(time_points=prediction_timestamps)
-
-        predicted_objects_states_dict = self.predict_objects(state=state, object_ids=object_ids,
-                                                             prediction_timestamps=prediction_timestamps,
-                                                             action_trajectory=action_trajectory)
-
-        # Aggregate all object together with ego into list of future states
-        future_states: List[State] = list()
-
-        for time_idx in range(len(prediction_timestamps)):
-            predicted_dynamic_objects = [future_object_states[time_idx] for future_object_states in
-                                         predicted_objects_states_dict.values()]
-
-            if action_trajectory is not None:
-                predicted_ego_state = state.ego_state.clone_from_cartesian_state(
-                    timestamp_in_sec=prediction_timestamps[time_idx],
-                    cartesian_state=extended_sampled_action_trajectory[time_idx])
-            else:
-                # Note! This uses the same ego object without deep copying it. Since EgoState should be immutable this
-                # should be OK.
-                predicted_ego_state = state.ego_state
-
-            state = state.clone_with(ego_state=predicted_ego_state,
-                                     dynamic_objects=predicted_dynamic_objects)
-
-            future_states.append(state)
-
-        return future_states
-
     def predict_1d_frenet_states(self, objects_fstates: FrenetState1D, horizons: np.ndarray) -> FrenetTrajectories1D:
         """
         See base class
