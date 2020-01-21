155480df3048611819bcc45cd952d006de5a41cb
RoiReshef <roi.reshef@gm.com>
2020-01-21 14:44:38 +0200

    added @DMProfile.profile method decorator

--- src/utils/dm_profiler.py
+++ src/utils/dm_profiler.py
@@ -9,6 +9,8 @@ class DMProfiler:
      Using this profiler (with DMProfiler('label'):) creates a profiling log under ultracruise/logs/prof_log.log
      Use summarize_profiler and plot_profiler to plot a certain label
     """
+    logger = AV_Logger.get_logger()
+
     def __init__(self, label):
         self.logger = AV_Logger.get_logger()
         self.label = label.replace(' ', '_')
@@ -21,7 +23,14 @@ class DMProfiler:
         self.logger.debug("%s{'current_time': %s, 'label': '%s', 'running_time': %s}" %
                           (LOG_MSG_PROFILER_PREFIX, current_time, self.label, current_time - self.start_time))
 
-
-
-
-
+    @staticmethod
+    def profile(func):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            current_time = time.time()
+            DMProfiler.logger.debug("%s{'current_time': %s, 'label': '%s', 'running_time': %s}" %
+                              (LOG_MSG_PROFILER_PREFIX, current_time, str(func), current_time - start_time))
+            print(str(current_time - start_time))
+            return result
+        return wrapper
