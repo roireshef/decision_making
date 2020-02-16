e5170ae307b88baf80d471607effc6ef9eb7d52c
Vlad Goldner <mz8cj6@gm.com>
2020-02-16 13:27:34 +0200

    use safety_mask, fix _get_lon_safe_dist

--- src/planning/behavioral/filtering/action_spec_filter_bank.py
+++ src/planning/behavioral/filtering/action_spec_filter_bank.py
@@ -201,7 +201,7 @@ class FilterForSafetyTowardsTargetVehicle(ActionSpecFilter):
             for actor_cell, time_ranges in time_ranges_dict.items():
                 safety_dist = FilterForSafetyTowardsTargetVehicle._check_safety_for_actor(
                     behavioral_state, ego_ftrajectories, actor_cell, time_ranges, self._logger)
-                are_safe[indices_by_rel_lane[target_lane]] &= (safety_dist > 0)
+                are_safe[indices_by_rel_lane[target_lane]] &= (safety_dist >= 0)
                 if not are_safe[indices_by_rel_lane[target_lane]].any():
                     break
 
@@ -316,14 +316,21 @@ class FilterForSafetyTowardsTargetVehicle(ActionSpecFilter):
         # calculate safety distances for all trajectories points
         safety_dist = FilterForSafetyTowardsTargetVehicle._get_lon_safe_dist(
             ego_ftrajectories[:, min_from_time_idx:max_till_time_idx], SAFETY_HEADWAY,
-            obj_ftrajectory, SAFETY_HEADWAY, time_ranges[:, 0] - min_from_time_idx, time_ranges[:, 1] - min_from_time_idx,
-            margin, actor_cell[1] == RelativeLongitudinalPosition.FRONT, logger)
-        return np.min(safety_dist, axis=1)  # minimum on time
+            obj_ftrajectory, SAFETY_HEADWAY, time_ranges[:, 1] - min_from_time_idx, margin,
+            actor_cell[1] == RelativeLongitudinalPosition.FRONT, logger)
+
+        # zero all safety_dist beyond trajectory_lengths
+        max_len = max_till_time_idx - min_from_time_idx
+        aux_matrix = np.full((ego_ftrajectories.shape[0], max_len), np.arange(max_len))
+        safety_mask = (aux_matrix < time_ranges[:, 1, np.newaxis] - min_from_time_idx) & \
+                      (aux_matrix >= time_ranges[:, 0, np.newaxis] - min_from_time_idx)
+
+        return np.min(safety_dist * safety_mask, axis=1)  # minimum on time
 
     @staticmethod
     def _get_lon_safe_dist(ego_trajectories: FrenetTrajectories2D, ego_response_time: float,
                            obj_trajectory: FrenetTrajectory2D, obj_response_time: float,
-                           from_time_idx: np.array, till_time_idx: np.array, margin: float, front_actor: bool, logger: Logger,
+                           till_time_idx: np.array, margin: float, front_actor: bool, logger: Logger,
                            ego_behind_max_brake: float = -LON_ACC_LIMITS[0],
                            ego_ahead_max_brake: float = LON_SAFETY_BACK_ACTOR_MAX_DECEL) -> np.array:
         """
@@ -336,7 +343,6 @@ class FilterForSafetyTowardsTargetVehicle(ActionSpecFilter):
         :param ego_response_time: [sec] ego response time
         :param obj_trajectory: object's Frenet trajectory: 2D matrix: timestamps_num x 6
         :param obj_response_time: [sec] object's response time
-        :param from_time_idx: array of first relevant time indices for each ego_trajectory
         :param till_time_idx: array of last relevant time indices for each ego_trajectory
         :param margin: [m] cars' lengths half sum
         :param front_actor: True if the actor is in front of ego
@@ -346,21 +352,16 @@ class FilterForSafetyTowardsTargetVehicle(ActionSpecFilter):
         :return: normalized longitudinal safety distance per timestamp. 2D matrix shape: traj_num x timestamps_num
         """
         # extract the relevant longitudinal data from the trajectories
-        ego_lon, ego_vel, ego_lat = ego_trajectories[..., FS_SX], ego_trajectories[..., FS_SV], ego_trajectories[..., FS_DX]
+        ego_s, ego_v, ego_lat = ego_trajectories[..., FS_SX], ego_trajectories[..., FS_SV], ego_trajectories[..., FS_DX]
         ego_trajectories_s = ego_trajectories[..., :FS_DX]
         if ego_trajectories.ndim == 2:  # single ego trajectory
-            ego_lon = ego_lon[np.newaxis]
-            ego_vel = ego_vel[np.newaxis]
+            ego_s = ego_s[np.newaxis]
+            ego_v = ego_v[np.newaxis]
             ego_trajectories_s = ego_trajectories_s[np.newaxis]
 
-        obj_lon, obj_vel, obj_lat = obj_trajectory[:, FS_SX], obj_trajectory[:, FS_SV], obj_trajectory[:, FS_DX]
+        obj_s, obj_v, obj_lat = obj_trajectory[:, FS_SX], obj_trajectory[:, FS_SV], obj_trajectory[:, FS_DX]
 
         # set infinite s for irrelevant time indices
-        irrelevant_s = -np.inf if front_actor else np.inf
-        for ego_s, from_i, till_i in zip(ego_lon, from_time_idx, till_time_idx):
-            ego_s[till_i:] = irrelevant_s
-            ego_s[:from_i] = irrelevant_s
-
         if front_actor:
             # extrapolate ego trajectories ego_response_time seconds beyond their end state
             dt = TRAJECTORY_TIME_RESOLUTION
@@ -370,28 +371,30 @@ class FilterForSafetyTowardsTargetVehicle(ActionSpecFilter):
             ego_extrapolation = predictor.predict_1d_frenet_states(last_ego_states_s, extrapolated_times)
             delay_shift = ego_extrapolation.shape[1]
 
-            ext_ego_lon = np.concatenate((ego_lon, np.zeros_like(ego_extrapolation[..., FS_SX])), axis=1)
-            ext_ego_vel = np.concatenate((ego_vel, np.zeros_like(ego_extrapolation[..., FS_SV])), axis=1)
+            ext_ego_s = np.concatenate((ego_s, np.zeros_like(ego_extrapolation[..., FS_SX])), axis=1)
+            ext_ego_v = np.concatenate((ego_v, np.zeros_like(ego_extrapolation[..., FS_SV])), axis=1)
             for i in range(delay_shift):
-                ext_ego_lon[range(ext_ego_lon.shape[0]), till_time_idx + i] = ego_extrapolation[:, i, FS_SX]
-                ext_ego_vel[range(ext_ego_vel.shape[0]), till_time_idx + i] = ego_extrapolation[:, i, FS_SV]
+                ext_ego_s[range(ext_ego_s.shape[0]), till_time_idx + i] = ego_extrapolation[:, i, FS_SX]
+                ext_ego_v[range(ext_ego_v.shape[0]), till_time_idx + i] = ego_extrapolation[:, i, FS_SV]
 
             # we assume ego continues its trajectory during its reaction time, so we compute the difference between
             # object's braking distance from any moment and delayed braking distance of ego
-            braking_distances_diff = np.maximum(0, ext_ego_vel[:, delay_shift:] ** 2 - obj_vel ** 2) / (2 * ego_behind_max_brake)
-            marginal_safe_dist = obj_lon - ext_ego_lon[:, delay_shift:] - braking_distances_diff - margin
+            braking_distances_diff = (ext_ego_v[:, delay_shift:] ** 2 - obj_v ** 2) / (2 * ego_behind_max_brake)
+            min_safe_dist = margin + np.maximum(0, ext_ego_s[:, delay_shift:] - ego_s + braking_distances_diff)
+            marginal_safe_dist = obj_s - ego_s - min_safe_dist
 
         else:
             # The worst-case velocity of the rear object (either ego or another object) may increase during its reaction
             # time, since it may accelerate before it starts to brake.
-            obj_vel_after_reaction_time = obj_vel + obj_response_time * LON_SAFETY_ACCEL_DURING_RESPONSE
+            obj_vel_after_reaction_time = obj_v + obj_response_time * LON_SAFETY_ACCEL_DURING_RESPONSE
 
             # longitudinal RSS formula considers distance reduction during the reaction time and difference between
             # objects' braking distances
             obj_acceleration_dist = 0.5 * LON_SAFETY_ACCEL_DURING_RESPONSE * obj_response_time ** 2
-            min_safe_dist = np.maximum((obj_vel_after_reaction_time ** 2 - ego_vel ** 2) / (2 * ego_ahead_max_brake), 0) + \
-                            (obj_vel * obj_response_time + obj_acceleration_dist) + margin
-            marginal_safe_dist = ego_lon - obj_lon - min_safe_dist
+            min_safe_dist = margin + np.maximum(0,
+                                (obj_vel_after_reaction_time ** 2 - ego_v ** 2) / (2 * ego_ahead_max_brake) +
+                                obj_v * obj_response_time + obj_acceleration_dist)
+            marginal_safe_dist = ego_s - obj_s - min_safe_dist
 
         return marginal_safe_dist if ego_trajectories.ndim > 2 else marginal_safe_dist[0]
 
