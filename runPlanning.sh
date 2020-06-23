49523eacd23e1649d6732888c554e1dd2d583e19
wz4y58 <efrat.slesslin@gm.com>
2020-06-23 16:29:14 -0400

    add arguments

--- runPlanning.sh
+++ runPlanning.sh
@@ -20,8 +20,12 @@ export OMP_NUM_THREADS=1
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$INSTALL_DIR/lib:$LD_LIBRARY_PATH
 export PYTHONPATH=$INSTALL_DIR:$INSTALL_DIR/lib:$INSTALL_DIR/python
 
+arguments=""
+[[ $# -gt 0 ]] && arguments+="--vehicle_configuration $1"
+[[ $# -gt 1 ]] && arguments+=" --map_file $2"
+
 cd ${INSTALL_DIR}/python
-python decision_making/src/dm_main.py --pid $$ &
+python decision_making/src/dm_main.py ${arguments} --pid $$ &
 
 child=$!
 wait "$child"
